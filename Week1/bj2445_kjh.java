288d22e9e7e767c206587b11da3f8ccb9bdb6882
김진홍 <deepred.kim@gmail.com>
2020-12-29 07:55:50 +0900

    Update bj2445_kjh.java
    가독성 개선 및 BufferedReader 전역변수화

--- Week1/bj2445_kjh.java
+++ Week1/bj2445_kjh.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 class Main {
-  public static void main(String[] args) {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws Exception{
     final int N = inputInt();
     int height = 2 * N - 1; // 출력할 결과의 높이
     int width = 2 * N; // 출력할 결과의 너비
@@ -11,12 +13,14 @@ class Main {
     for (int i = 0; i < height; i++) {
       int stars = calcStars(i, height);
       int spaces = width - (stars * 2);
-      sb.append("*".repeat(stars));
-      sb.append(" ".repeat(spaces));
-      sb.append("*".repeat(stars));
-      sb.append("\n");
+      sb.append("*".repeat(stars))
+        .append(" ".repeat(spaces))
+        .append("*".repeat(stars))
+        .append("\n");
     }
     System.out.print(sb);
+
+    br.close();
   }
 
   private static int calcStars(int currentIndex, int length) {
@@ -29,10 +33,9 @@ class Main {
 
   private static int inputInt() {
     String strLine = null;
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+    try {
       strLine = br.readLine();
-    }
-    catch(Exception e) {}
+    } catch(Exception e) { }
 
     return Integer.parseInt(strLine);
   }
