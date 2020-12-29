aba3e50b71b02bd0c0169000ac82aea699f99067
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2020-12-29 23:43:10 +0900

    Update bj5565_wsb.java

--- Week1/Day1/bj5565_wsb.java
+++ Week1/Day1/bj5565_wsb.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//// 흠 잡을 데 없이 깔끔한 것 같습니다.
+//// 흠 잡을 데 없이 깔끔한 것 같습니다.:22 코딩 장인이신가요??
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
