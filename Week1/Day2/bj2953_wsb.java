0bc8e6a25d4677ee34bcf6690aa40d72f76ca73e
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2020-12-30 23:50:12 +0900

    Update bj2953_wsb.java

--- Week1/Day2/bj2953_wsb.java
+++ Week1/Day2/bj2953_wsb.java
@@ -7,7 +7,7 @@ class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        //// max로만 썼는데 high 네이밍 배워갑니다
+        //// max로만 썼는데 high 네이밍 배워갑니다 :22 네이밍을 잘하셨네요
         int highScore = 0, highNum = 0;
 
         for(int i = 1; i <= 5; i++){
