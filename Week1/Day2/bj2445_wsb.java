137ab4b890b1c04330d0a3610c9a38e0a04b02b9
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2020-12-30 23:44:44 +0900

    Update bj2445_wsb.java

--- Week1/Day2/bj2445_wsb.java
+++ Week1/Day2/bj2445_wsb.java
@@ -11,7 +11,7 @@ class Main{
         int m = 2 * n;
 
         for(int i = 1; i < m; i++){
-            //// 이 부분은 가독성이 조금 떨어지는 것 같습니다
+            //// 이 부분은 가독성이 조금 떨어지는 것 같습니다 :22 저도 조금 이해하기 어려웠어요
             sb.append("*".repeat(i <= n ? i : m - i));
             sb.append(" ".repeat(i <= n ? m - 2 * i : 2 * (i - n)));
             sb.append("*".repeat(i <= n ? i : m - i)).append("\n");
