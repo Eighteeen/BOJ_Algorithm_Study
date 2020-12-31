fd516714dab9a4c2600c7225a58d7cec6769ad84
김진홍 <deepred.kim@gmail.com>
2021-01-01 02:00:20 +0900

    Update bj10871_kjh.java

--- Week1/Day2/bj10871_kjh.java
+++ Week1/Day2/bj10871_kjh.java
@@ -13,6 +13,10 @@ class Main {
       int number = Input.nextInt();
       //// 숫자의 활용을 하는 문제라서 Input.nextInt()을 뜯어봤는데 활용성 있게 메소드를 작성하신 것 같으면서도 너무 세부적으로 나눠놓아서 한번에 파악하기에 어려웠습니다.
       //// 굳이 나눌 필요가 없는 부분은 하나의 메소드로 합치는 게 좋을 것 같아요.
+      //// => next, nextLine, nextInt 셋 모두 Scanner에서 따온 이름이에요!
+      //// => 더 효율적이면서 (BufferedReader 사용)
+      //// => 더 간편한 (static 메소드라서 인스턴스화 하지 않아도 사용할 수 있음)
+      //// => 클래스로 제가 따로 만들어서 항상 참조하는 클래스로 사용하고 있습니다
       if (number < X) {
         sb.append(number).append(' ');
       }
