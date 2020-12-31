6edcc1a19e3b6ee4fea3102aec180fbb9723e0ca
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-01 02:33:21 +0900

    Update bj2711_wsb.java

--- Week1/Day3/bj2711_wsb.java
+++ Week1/Day3/bj2711_wsb.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+////깔끔하게 잘 짠거 같아요
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
