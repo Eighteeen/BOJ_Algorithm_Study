146b29195e87f280e9202fef8d8f90fdb90db250
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-01 02:19:06 +0900

    Update bj10996_wsb.java

--- Week1/Day3/bj10996_wsb.java
+++ Week1/Day3/bj10996_wsb.java
@@ -9,6 +9,7 @@ class Main{
         int N = Integer.parseInt(br.readLine());
         int repeatNum = N / 2;
         //// 맨 뒤에 추가 공백이 붙어도 정답처리 된다는 걸 이용해서 굉장히 깔끔하게 짜여졌네요! 배워갑니다
+        //// 짝/홀수로 구분해서 깔끔하게 짠거 같슴다
         String str1 = (N % 2 == 0 ? "* ".repeat(repeatNum) : "* ".repeat(repeatNum + 1)) + "\n";
         String str2 = (" *".repeat(repeatNum)) + "\n";
 
