1ac54f4c499d4dfaca2efe9540e44f2a54df16e0
김진홍 <deepred.kim@gmail.com>
2021-01-01 22:11:57 +0900

    5일차: 카드 역배치 해결

---
+++ Week1/Day5/bj10804_kjh.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    int[] oneToTwenty = IntStream.range(1, 21).toArray();
+    
+    for (int i = 0; i < 10; i++) {
+      int startReverse = Input.nextInt() - 1;
+      int endReverse = Input.nextInt() - 1;
+      reverseArrayOrder(oneToTwenty, startReverse, endReverse);
+    }
+
+    printArray(oneToTwenty);
+  }
+
+  private static void reverseArrayOrder(int[] array, int startReverse, int endReverse) {
+    int[] copiedTarget = Arrays.copyOfRange(array, startReverse, endReverse + 1);
+
+    int reverseCount = endReverse - startReverse + 1;
+    for(int i = 0; i < reverseCount; i++) {
+      int reversedOrder = copiedTarget.length - 1 - i;
+      array[startReverse + i] = copiedTarget[reversedOrder];
+    }
+  }
+
+  private static void printArray(int[] array) {
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < array.length; i++) {
+      sb.append(array[i])
+        .append(' ');
+    }
+    System.out.println(sb);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
