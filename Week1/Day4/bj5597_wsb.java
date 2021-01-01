e6a642e9da52d3cc8f78795c09d4abf41059dbc3
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-01 23:04:12 +0900

    Update bj5597_wsb.java

--- Week1/Day4/bj5597_wsb.java
+++ Week1/Day4/bj5597_wsb.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+        ////깔끔하게 잘 짠거 
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
