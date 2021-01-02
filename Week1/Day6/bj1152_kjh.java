a72be077de8d139d10d651e5b53bc7032fa7628c
김진홍 <deepred.kim@gmail.com>
2021-01-02 10:01:24 +0900

    6일차: '단어의 개수' 해결

---
+++ Week1/Day6/bj1152_kjh.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    String sentence = Input.nextLine();
+    sentence = sentence.trim();
+
+    if (sentence.equals("")) {
+      System.out.print(0);
+      return;
+    }
+    
+    int wordCount = calcCharCountFromString(' ', sentence) + 1;
+    System.out.print(wordCount);
+  }
+
+  private static int calcCharCountFromString(char targetChar, String string) {
+    int charCount = 0;
+    for(char ch : string.toCharArray()) {
+      if (ch == targetChar) {
+        charCount++;
+      }
+    }
+    return charCount;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
