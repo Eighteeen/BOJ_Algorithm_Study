e0f48a41f62558a92099b783b0d079f5bd495050
김진홍 <deepred.kim@gmail.com>
2021-01-02 09:35:51 +0900

    '도비의 난독증 테스트' 중간커밋

---
+++ Week1/Day6/bj2204_kjh.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    int testCases;
+    StringBuilder sb = new StringBuilder();
+    while( (testCases = Input.nextInt()) != 0 ) {
+      String frontWord = Input.nextLine();
+      for(int i = 1; i < testCases; i++) {
+        String word = Input.nextLine();
+        
+        boolean isFrontWord = word.toLowerCase().compareTo(frontWord.toLowerCase()) == -1;
+        if (isFrontWord) {
+          frontWord = word;
+        }
+      }
+      sb.append(frontWord).append('\n');
+    }
+    System.out.print(sb);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
