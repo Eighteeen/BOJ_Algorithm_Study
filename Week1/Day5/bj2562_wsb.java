8a93d1303799127bcb430f1fe9390dab9723e13a
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-02 20:55:03 +0900

    Update bj2562_wsb.java

--- Week1/Day5/bj2562_wsb.java
+++ Week1/Day5/bj2562_wsb.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//// 무난하게 잘 짜신거ㅓ 같아요.
 //// 가독성이 좋고 문제도 쉬워서 10초만에 코드가 읽혔습니다.
 //// 효율성 면에서도 이 코드보다 더 효율적인건 생각나는게 없네요.
 class Main{
