008ee53272eb08c99109388662a5629b5ae94c22
김진홍 <deepred.kim@gmail.com>
2021-01-02 09:50:23 +0900

    6일차: '홀수' 해결

---
+++ Week1/Day6/bj2576_kjh.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    int sum = 0;
+    int min = 101;
+    for(int i = 0; i < 7; i++) {
+      int number = Input.nextInt();
+      if (number % 2 == 1) {
+        min = (number < min) ? (number) : (min);
+        sum += number;
+      }
+    }
+    if (sum == 0) {
+      System.out.print(-1);
+      return;
+    }
+    System.out.print(sum + "\n" + min);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
