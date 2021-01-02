49cc1c44deecd53ac22eae12d853abfe523e1b31
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-02 20:32:35 +0900

    Update bj10173_wsb.java

--- Week1/Day5/bj10173_wsb.java
+++ Week1/Day5/bj10173_wsb.java
@@ -9,8 +9,8 @@ class Main{
         String str;
 
         while(!(str = br.readLine()).equals("EOI")){
-            //// 이게 한줄로 될거란 생각을 못했네요. 와우
-            //// contains도 몰랐어요. Java에서 제공하는 기능 정말 잘 활용하시는 것 같습니다. 배워갑니다!
+            //// 이게 한줄로 될거란 생각을 못했네요. 와우:22
+            //// contains도 몰랐어요. Java에서 제공하는 기능 정말 잘 활용하시는 것 같습니다. 배워갑니다! :22
             sb.append(str.toUpperCase().contains("NEMO") ? "Found" : "Missing").append("\n");
         }
         System.out.print(sb);
