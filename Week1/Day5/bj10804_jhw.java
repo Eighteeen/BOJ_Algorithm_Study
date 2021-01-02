47b96abb00fc57faadbe6beeb11b9c32b07657d9
김진홍 <deepred.kim@gmail.com>
2021-01-02 22:47:23 +0900

    6일차: n-1일차 추가 피드백

--- Week1/Day5/bj10804_jhw.java
+++ Week1/Day5/bj10804_jhw.java
@@ -15,6 +15,8 @@ public class bj10804_jhw {
 			int  num1 = sc.nextInt();
 			int  num2 = sc.nextInt();
 				////Reverse기능을 활용하면 더 편리할거같아요!
+			//// 이 부분은 해석하는데 시간이 좀 걸렸어요. num1에서 1은 왜 뺸걸까, num2에서 1은 왜뺀걸까, 마지막에 num2는 왜 빼주는걸까?
+			//// 변수로 또는 함수로 이름을 붙여 의도를 표현하신다면 더 가독성이 좋아질 수 있을 것 같습니다
 			 for(int k = num1-1; k < num2; k++ ) {
 				 temp = arr[k];
 				 arr[k] = arr[num2-1];
