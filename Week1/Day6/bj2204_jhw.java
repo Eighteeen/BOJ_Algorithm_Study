c9234852f21e739f144bff3afe79d05f94f388b9
cousim46 <wjdghldns24@naver.com>
2021-01-04 04:20:52 +0900

    Update bj2204_jhw.java

--- Week1/Day6/bj2204_jhw.java
+++ Week1/Day6/bj2204_jhw.java
@@ -4,27 +4,25 @@ import java.util.Scanner;
 
 public class bj2204_jhw {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String firstWord;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstWord;
         String compareWord;
-        
-		while(true) {
-			int n = sc.nextInt();
-			if(n == 0) {
-				return;
+
+        while(true) {
+        int n = sc.nextInt();
+        if(n == 0) {
+            return;
+        }
+
+        firstWord = sc.next();	
+        for(int i = 1; i < n; i++) {
+            compareWord = sc.next();
+            if(compareWord.compareToIgnoreCase(firstWord) < 0) {
+                firstWord= compareWord;
             }
-            
-		    firstWord = sc.next();	
-		    for(int i = 1; i < n; i++) {
-			    compareWord = sc.next();
-			    if(compareWord.compareToIgnoreCase(firstWord)<0) {
-				    firstWord= compareWord;
-			    }
-		    }
-		    System.out.println(firstWord);
-		}
-
-	}
-
+        }
+        System.out.println(firstWord);
+        }
+    }
 }
