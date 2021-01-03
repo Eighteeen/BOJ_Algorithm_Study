a2ea14601e011a560b96acaa7260bb317c3edace
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-03 22:25:14 +0900

    Update bj14720_wsb.java

--- Week1/Day6/bj14720_wsb.java
+++ Week1/Day6/bj14720_wsb.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+    ////깔끔하게 잘짠거 같습니다
 class Main{
     //// enum 활용 배워가요!
     enum Milk {
