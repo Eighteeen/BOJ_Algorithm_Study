e5013be6bcd4e74186372e3865f06df4d9eb7b65
cousim46 <wjdghldns24@naver.com>
2021-01-04 04:17:02 +0900

    Create bj2204_jhw.java

---
+++ Week1/Day6/bj2204_jhw.java
@@ -0,0 +1,30 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class bj2204_jhw {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String firstWord;
+        String compareWord;
+        
+		while(true) {
+			int n = sc.nextInt();
+			if(n == 0) {
+				return;
+            }
+            
+		    firstWord = sc.next();	
+		    for(int i = 1; i < n; i++) {
+			    compareWord = sc.next();
+			    if(compareWord.compareToIgnoreCase(firstWord)<0) {
+				    firstWord= compareWord;
+			    }
+		    }
+		    System.out.println(firstWord);
+		}
+
+	}
+
+}
