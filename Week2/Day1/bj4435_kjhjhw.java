e4bd4f1bc0e187a69011037d40955e2eac1842bc
김진홍 <deepred.kim@gmail.com>
2021-01-04 22:02:08 +0900

    Day8: 페어프로그래밍 완료

---
+++ Week2/Day1/bj4435_kjhjhw.java
@@ -0,0 +1,102 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int[] gandalfUnitScores = { 1, 2, 3, 3, 4, 10 };
+    final int[] sauronUnitScores = { 1, 2, 2, 2, 3, 5, 10 };
+
+    int BATTLE = Input.nextInt();
+
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < BATTLE; i++) {
+      String[] gandalfCount = Input.nextLine().split(" ");
+      String[] sauronCount =  Input.nextLine().split(" ");
+
+      int gandalfGrade = calcGrade(gandalfUnitScores, gandalfCount);
+      int sauronGrade = calcGrade(sauronUnitScores, sauronCount);
+
+      if (gandalfGrade > sauronGrade) {
+        sb.append(makeBattleMessage(i + 1, "Good triumphs over Evil"));
+      }
+      else if (sauronGrade > gandalfGrade) {
+        sb.append(makeBattleMessage(i + 1, "Evil eradicates all trace of Good"));
+      }
+      else {
+        sb.append(makeBattleMessage(i + 1, "No victor on this battle field"));
+      }
+    }
+    System.out.print(sb);
+  }
+
+  private static StringBuilder makeBattleMessage(int battleNum, String message) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Battle ")
+      .append(battleNum)
+      .append(": ")
+      .append(message)
+      .append('\n');
+    return sb;
+  }
+
+  private static int calcGrade(int[] unitScores, String[] unitCounts) {
+    int sum = 0;
+    for(int i = 0; i < unitScores.length; i++) {
+      int unitScore = unitScores[i];
+      int unitCount = Integer.parseInt(unitCounts[i]);
+
+      sum += unitCount * unitScore;
+    }
+    
+    return sum;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
