dcb7a635fb4e81603b23f555d4f2e9bdc6ba6239
김진홍 <deepred.kim@gmail.com>
2021-01-05 21:32:16 +0900

    9일차: n-2일차 피드백 반영

--- Week1/Day7/bj1475_kjh.java
+++ Week1/Day7/bj1475_kjh.java
@@ -15,12 +15,16 @@ class Main {
       ////이것도 배워갑니다.
       int num = charToInt(N.charAt(i));
       //// 삼항연산자 하나하나마다 괄호를 넣은 이유가 뭔가요??
+      //// => 가독성을 위함입니다!
+      //// => 괄호로 묶음으로써 각 항이 명확히 구별하고
+      //// => ? : 양옆을 띄움으로써 삼항연산자가 눈에 띄게 했어요
       int needNumberIndex = (num == 9) ? (SIX_OR_NINE) : (num);
       needNumbers[needNumberIndex] += 1;
     }
 
     //// 구현 관점이 달라서 나타나는 문제지만 9일때도 6으로 들어가게 해서 나중에 2로 나누는 게 저는 좀 어색해보이네요 ㅜ
     //// 하지만 이런 방법도 있구나 하고 배워가기도 합니다!
+    //// => 요건 신기한 관점의 차이인걸로 ㅎㅎ
     needNumbers[SIX_OR_NINE] = (int) Math.ceil(needNumbers[SIX_OR_NINE] / 2.0);
 
     int needSet = calcMax(needNumbers);
