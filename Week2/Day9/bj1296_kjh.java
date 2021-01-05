6d9371eb62e58d892ff8341cc3b95899fb577f57
김진홍 <deepred.kim@gmail.com>
2021-01-05 21:04:54 +0900

    9일차: '데이트' 해결

---
+++ Week2/Day9/bj1296_kjh.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final String HIS_NAME = Input.nextLine();
+    final int N = Input.nextInt();
+    String lover = "";
+    int maxLove = -1;
+
+    for(int i = 0; i < N; i++) {
+      String herName = Input.nextLine();
+      int l = countCharFromString('L', HIS_NAME) + countCharFromString('L', herName);
+      int o = countCharFromString('O', HIS_NAME) + countCharFromString('O', herName);
+      int v = countCharFromString('V', HIS_NAME) + countCharFromString('V', herName);
+      int e = countCharFromString('E', HIS_NAME) + countCharFromString('E', herName);
+      int love = ((l+o)*(l+v)*(l+e)*(o+v)*(o+e)*(v+e)) % 100;
+
+      if (love > maxLove) {
+        maxLove = love;
+        lover = herName;
+      } else if (love == maxLove) {
+        lover = (lover.compareTo(herName) <= 0) ? (lover) : (herName);
+      }
+    }
+    
+    System.out.print(lover);
+  }
+  
+  private static int countCharFromString(char ch, String str) {
+    int count = 0;
+    for(char partOfStr : str.toCharArray()) {
+      if (partOfStr == ch) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
