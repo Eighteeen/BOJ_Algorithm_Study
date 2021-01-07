971800a408a1cfe390f7e7c1ae7abbf33a829ab9
cousim46 <wjdghldns24@naver.com>
2021-01-07 23:36:45 +0900

    Day11

---
+++ Week2/Day11/bj10103_jhw.java
@@ -0,0 +1,36 @@
+package Day11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class bj10103_jhw {
+
+	public static void main(String[] args) throws NumberFormatException, IOException  {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int changScore = 100;
+		int sangScore = 100;
+		String[] score;
+		int number = Integer.parseInt(bf.readLine());
+		
+		for(int i = 0; i < number; i++) {
+			score = bf.readLine().split(" ");
+			
+			int chang = Integer.parseInt(score[0]);
+            int sang = Integer.parseInt(score[1]);
+            
+			if(sang > chang) {
+				changScore -= sang;
+			}else if(sang < chang){
+				sangScore -= chang;
+			}else {
+				continue;
+			}
+		}
+		System.out.println(changScore);
+		System.out.println(sangScore);
+
+	}
+
+}
