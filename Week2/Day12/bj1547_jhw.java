b505e6c23134d321964eef4415497f85c7eba2c7
cousim46 <wjdghldns24@naver.com>
2021-01-08 23:57:48 +0900

    Day12

---
+++ Week2/Day12/bj1547_jhw.java
@@ -0,0 +1,33 @@
+package Day12;
+
+
+import java.util.Scanner;
+
+public class bj1547_jhw {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+	
+		int numArr[] = {1,0,0};
+		int temp;
+        
+		for(int i = 0; i < num; i++) {
+			int X = sc.nextInt(); 
+			int Y = sc.nextInt(); 
+			
+			temp = numArr[X-1];
+			numArr[X-1] = numArr[Y-1];
+			numArr[Y-1] = temp;
+		}
+		
+		for(int i  = 0; i < numArr.length; i++) {
+			if(numArr[i] == 1) {
+				System.out.println(i+1);
+			}
+		}
+
+	}
+
+}
