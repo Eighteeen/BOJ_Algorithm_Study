4c5126dda74411bc3a69dd16e6fd3c6240f3246c
wsb0722 <wsb0722@naver.com>
2021-01-08 17:29:27 +0900

    Solve Day12

---
+++ Week2/Day12/bj14647_wsb.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String bingoInfo[] = br.readLine().split(" ");
+        int line = Integer.parseInt(bingoInfo[0]), row = Integer.parseInt(bingoInfo[1]);
+        String nowLineInfo[];
+        int lineSum[] = new int[line], rowSum[] = new int[row];
+        int cntNow, cntAll = 0;
+
+        for(int i = 0; i < line; i++){
+            nowLineInfo = br.readLine().split(" ");
+            for(int j = 0; j < row; j++){
+                cntNow = cntChar(nowLineInfo[j], '9');
+                lineSum[i] += cntNow;
+                rowSum[j] += cntNow;
+                cntAll += cntNow;
+            }
+        }
+
+        List<Integer> lineSumList = IntStream.of(lineSum).boxed().collect(Collectors.toList());
+        List<Integer> rowSumList = IntStream.of(rowSum).boxed().collect(Collectors.toList());
+        int lineMax = Collections.max(lineSumList), rowMax = Collections.max(rowSumList);
+        cntAll -= Math.max(lineMax, rowMax);
+
+        System.out.print(cntAll);
+    }
+
+    static int cntChar(String str, char ch){
+        int cnt = 0;
+        int len = str.length();
+        for(int i = 0; i < len; i++){
+            if(str.charAt(i) == ch) cnt++;
+        }
+        return cnt;
+    }
+}
---
+++ Week2/Day12/bj1547_wsb.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        Integer cups[] = {1, 2, 3};
+        String cupPositionInfo[];
+        int X, Y;
+        int indexX, indexY;
+
+        for(int i = 0; i < M; i++){
+            cupPositionInfo = br.readLine().split(" ");
+            X = Integer.parseInt(cupPositionInfo[0]);
+            Y = Integer.parseInt(cupPositionInfo[1]);
+            indexX = Arrays.asList(cups).indexOf(X);
+            indexY = Arrays.asList(cups).indexOf(Y);
+
+            cups[indexX] = Y;
+            cups[indexY] = X;
+        }
+
+        System.out.print(cups[0]);
+    }
+}
