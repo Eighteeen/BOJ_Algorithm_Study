f8ce03b35da94e5d8f6fa2aff417cc827db01d98
김진홍 <deepred.kim@gmail.com>
2021-01-08 14:06:44 +0900

    12일차: 오늘의 문제 해결

---
+++ Week2/Day12/bj14647_kjh.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int N = Input.nextInt();
+    final int M = Input.nextInt();
+
+    int wholeNine = 0;
+    int[] nineInRow = new int[N];
+    int[] nineInColumn = new int[M];
+    
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        int nineInNumber = countCharFromString('9', Input.next());
+        wholeNine += nineInNumber;
+        nineInRow[i] += nineInNumber;
+        nineInColumn[j] += nineInNumber;
+      }
+    }
+
+    int maxNineInRow = Arrays.stream(nineInRow).max().getAsInt();
+    int maxNineInColumn = Arrays.stream(nineInColumn).max().getAsInt();
+    
+    int hitCount = wholeNine - ( maxNineInRow > maxNineInColumn ? maxNineInRow : maxNineInColumn);
+    System.out.print(hitCount);
+  }
+  
+  private static int countCharFromString(char ch, String str) {
+    int count = 0;
+    for(char partOfStr : str.toCharArray()) {
+      if (partOfStr == ch) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
---
+++ Week2/Day12/bj1547_kjh.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    boolean[] ballInCup = { true, false, false };
+    final int M = Input.nextInt();
+
+    for (int i = 0; i < M; i++) {
+      int cupXIndex = Input.nextInt() - 1;
+      int cupYIndex = Input.nextInt() - 1;
+
+      boolean ballInCupX = ballInCup[cupXIndex];
+      boolean ballInCupY = ballInCup[cupYIndex];
+
+      if (ballInCup[cupXIndex] != ballInCup[cupYIndex]) {
+        ballInCup[cupXIndex] = !ballInCup[cupXIndex];
+        ballInCup[cupYIndex] = !ballInCup[cupYIndex];
+      }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+      if (ballInCup[i] == true) {
+        System.out.print(i+1);
+        break;
+      }
+    }
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
