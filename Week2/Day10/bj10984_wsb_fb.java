10d870573a880614c4e2040a3e6f60a6169b0069
wsb0722 <wsb0722@naver.com>
2021-01-10 05:41:02 +0900

    Reflect Feedback Day10
    - bj9536_wsb 소스코드 바뀐 거 없음
    - bj1551_wsb 소스코드 바뀐 거 없음
    - bj1551_wsb 질문 카톡 답변

---
+++ Week2/Day10/bj10984_wsb_fb.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    ////깔끔하네요. : 22 완전 깔꼼해용
+    public static void main(String[] args) throws IOException {
+        int semester = Integer.parseInt(br.readLine());
+        int subjectNum;
+
+        for(int i = 0; i < semester; i++){
+            subjectNum = Integer.parseInt(br.readLine());
+            calcGPA(subjectNum);
+        }
+        System.out.print(sb);
+    }
+
+    //// 함수이름 보고 학점 계산해서 출력만 하는 줄 알았는데 코드보니 입력까지 받는다니까 배신당한 기부니에요
+    //// 좋은 지적 감사해욧 함수이름을 바꿔봤어요
+    static void calcGPA(int len) throws IOException {
+        String subjectInfo[];
+        int credit, creditSum = 0;
+        float grade, gradeSum = 0;
+        for(int i = 0; i < len; i++){
+            subjectInfo = br.readLine().split(" ");
+            credit = Integer.parseInt(subjectInfo[0]);
+            grade = Float.parseFloat(subjectInfo[1]);
+
+            creditSum += credit;
+            gradeSum += grade * credit;
+        }
+        sb.append(creditSum).append(" ")
+            .append(String.format("%.1f", gradeSum / creditSum)).append("\n");
+    }
+}
