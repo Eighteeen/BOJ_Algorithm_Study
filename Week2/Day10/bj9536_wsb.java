355ce0057fbfaf3bf52bdca468798941f417b172
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-10 04:04:37 +0900

    Update bj9536_wsb.java

--- Week2/Day10/bj9536_wsb.java
+++ Week2/Day10/bj9536_wsb.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+    ////전체적으로 깔끔하게 짜여진거 같습니다.
 class Main{
     static StringBuilder sb = new StringBuilder();
     static String nowSound;
@@ -17,7 +17,7 @@ class Main{
         for(int i = 0; i < T; i++){
             recordList = new ArrayList<>(Arrays.asList(br.readLine().split(" ")));
             while(!(nowSound = br.readLine()).equals("what does the fox say?")){
-                //// removeIf 배워가요.. 있는 줄도 몰랐네:22
+                //// removeIf 배워가요.. 있는 줄도 몰랐네:22 :33 별게 다있네요
                 recordList.removeIf(r -> r.equals(getSound(nowSound)));
             }
             printFoxSound(recordList);
@@ -32,7 +32,7 @@ class Main{
         }
         sb.append("\n");
     }
-
+        //// 이 함수는 뭘 해주는 과정인가요??
     static String getSound(String sound){
         return sound.substring(sound.indexOf(" goes") + 6);
     }
