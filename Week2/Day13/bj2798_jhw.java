8b53b569e6b09e229f7a7e39ba9b9e7363cf6b3a
cousim46 <wjdghldns24@naver.com>
2021-01-09 21:33:08 +0900

    Update bj2798_jhw.java

--- Week2/Day13/bj2798_jhw.java
+++ Week2/Day13/bj2798_jhw.java
@@ -16,11 +16,11 @@ public class bj2798_jhw {
 			int num = sc.nextInt();
 			mArr[i] = num;
 		}
-		for(int k = 0 ; k<N-2;k++) {
+		for(int k = 0 ; k < N-2; k++) {
 			for(int i  = 1; i < N-1; i++) {
-				for(int j = 2 ; j <N;j++ ) {
-					if(i != j &&k!=i && k!=j) {
-						sum = mArr[k]+mArr[i] + mArr[j];
+				for(int j = 2 ; j < N; j++ ) {
+					if(i != j && k != i && k != j) {
+						sum = mArr[k] + mArr[i] + mArr[j];
 						if(sum <= M && sum >= max) {
 							max = sum;
 						}
