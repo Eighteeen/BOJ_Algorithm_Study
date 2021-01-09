7ab042da03b801b22fd95d88829d018b7b6133b0
cousim46 <wjdghldns24@naver.com>
2021-01-09 19:41:13 +0900

    Day13

---
+++ Week2/Day13/bj2163_jhw.java
@@ -0,0 +1,17 @@
+package Day13;
+
+
+import java.util.Scanner;
+
+public class bj2163_jhw {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		System.out.println(N*M-1);
+
+	}
+
+}
---
+++ Week2/Day13/bj2798_jhw.java
@@ -0,0 +1,35 @@
+package Day13;
+
+import java.util.Scanner;
+
+public class bj2798_jhw {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int [] mArr = new int[N];
+		int max = 0;
+		int sum = 0;
+		
+		for(int i =0; i < N; i++) {
+			int num = sc.nextInt();
+			mArr[i] = num;
+		}
+		for(int k = 0 ; k<N-2;k++) {
+			for(int i  = 1; i < N-1; i++) {
+				for(int j = 2 ; j <N;j++ ) {
+					if(i != j &&k!=i && k!=j) {
+						sum = mArr[k]+mArr[i] + mArr[j];
+						if(sum <= M && sum >= max) {
+							max = sum;
+						}
+					}
+				}
+			}
+		
+		}
+		System.out.println(max);
+	}
+
+}
