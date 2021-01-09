c5d3cdaef6e7c17b1f20685b9f0dd52bf9bd8e4f
김진홍 <deepred.kim@gmail.com>
2021-01-09 11:02:46 +0900

    13일차: 오늘의 문제 해결

---
+++ Week2/Day13/bj2163_kjh.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int N = Input.nextInt();
+    final int M = Input.nextInt();
+
+    System.out.print(cutChocolate(N, M));
+  }
+
+  private static int cutChocolate(int N, int M) {
+    if (N + M == 2) return 0;
+    
+    if (N % 2 == 0) {
+      return 1 + (cutChocolate(N / 2, M) * 2);
+    } else if (M % 2 == 0) {
+      return 1 + (cutChocolate(N, M / 2) * 2);
+    } else if (N > 1) {
+      return (N - 1) + (cutChocolate(1, M) * N);
+    } else if (M > 1) {
+      return (M - 1) + (cutChocolate(N, 1) * N);
+    }
+    
+    return 0;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
---
+++ Week2/Day13/bj2798_kjh.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int CARDS = Input.nextInt();
+    final int GOAL = Input.nextInt();
+    int[] cards = new int[CARDS];
+
+    for(int i = 0; i < CARDS; i++) {
+      cards[i] = Input.nextInt();
+    }
+
+    int sumOfBestCombination = findSumOfBestCombination(cards, GOAL);
+    System.out.print(sumOfBestCombination);
+  }
+  
+  private static int findSumOfBestCombination(int[] cards, int GOAL) {
+    int bestSum = 0;
+    int bestDifference = GOAL;
+    
+    for (int i = 0; i < cards.length; i++) {
+      for (int j = i + 1; j < cards.length; j++) {
+        for (int k = j + 1; k < cards.length; k++) {
+          int sum = cards[i] + cards[j] + cards[k];
+          if (sum > GOAL) continue;
+
+          int difference = GOAL - sum;
+          if (difference < bestDifference) {
+            bestSum = sum;
+            bestDifference = difference;
+          }
+        }
+      }
+    }
+    return bestSum;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
