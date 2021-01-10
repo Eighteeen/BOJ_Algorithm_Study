79f48a9ed22973526b895bdda422bcd9654fb83e
김진홍 <deepred.kim@gmail.com>
2021-01-10 17:18:10 +0900

    14일차: 오늘의 문제

---
+++ Week2/Day14/bj10250_kjh.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int T = Input.nextInt();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < T; i++) {
+      int floors = Input.nextInt();
+      int rooms = Input.nextInt();
+      int nthGuest = Input.nextInt();
+
+      int guestFloor = ((nthGuest - 1) % floors) + 1;
+      int guestRoom = nthGuest / floors;
+      guestRoom += (nthGuest % floors > 0) ? (1) : (0);
+      
+      sb.append(guestFloor);
+      if (guestRoom < 10) sb.append("0");
+      sb.append(guestRoom)
+        .append('\n');
+    }
+    
+    System.out.print(sb);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    return readLine();
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(readLine(), " ");
+  }
+
+  private static String readLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+}
