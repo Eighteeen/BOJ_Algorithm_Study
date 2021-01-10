83cbd9da13c0ad91377e948ae516b73236a27a6e
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-10 20:32:22 +0900

    day13_feedback

---
+++ Week2/Day13/bj2798_lye_fb.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		int sum, gap, result = 0;
+		int[] info = Read_ints();
+		int len = info[0];
+		int goal = info[1];
+		int min = 100000;
+		int[] numArr = Read_ints();
+
+		for (int i = 0; i < len - 2; i++)
+		{
+			for (int j = i + 1; j < len - 1; j++)
+			{
+				for (int k = j + 1; k < len; k++)
+				{
+					sum = numArr[i] + numArr[j] + numArr[k];
+					gap = goal - sum;
+					if ((gap > 0 || gap == 0) && gap < min)
+					{
+						min = gap;
+						result = sum;
+					}
+
+
+				}
+			}
+		}
+		Console.Write(result);
+	}
+
+	static int[] Read_ints()
+	{
+		string input = Console.ReadLine();
+		string[] strArr = input.Split(' ');
+		int len = strArr.Length;
+		int[] nums = new int[len];
+		for (int i = 0; i < len; i++)
+			nums[i] = Convert.ToInt32(strArr[i]);
+		return nums;
+	}
+}
