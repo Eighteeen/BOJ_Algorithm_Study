b44b1f85365c2a8c322ba49babf0891b22c6cf14
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-10 12:56:40 +0900

    Update bj2798_kjh.java

--- Week2/Day13/bj2798_kjh.java
+++ Week2/Day13/bj2798_kjh.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//// 항상 함수를 잘 분리해서 메인을 되게 깔끔하게 짜주시는거 같아요!
 class Main {
   public static void main(String[] args) throws Exception {
     final int CARDS = Input.nextInt();
