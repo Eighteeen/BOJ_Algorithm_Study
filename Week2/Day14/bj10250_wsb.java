7d54beb4303616430555e540eb53a98c8c1f5ef7
wsb0722 <wsb0722@naver.com>
2021-01-10 13:29:50 +0900

    Solve Day14

---
+++ Week2/Day14/bj10250_wsb.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        String assignInfo[];
+        int H, guestN;
+        int floorNum, distanceNum;
+
+        for(int i = 0; i < T; i++){
+            assignInfo = br.readLine().split(" ");
+            H = Integer.parseInt(assignInfo[0]);
+            guestN = Integer.parseInt(assignInfo[2]);
+            floorNum = guestN % H;
+            distanceNum = guestN / H;
+
+            if(floorNum == 0){
+                sb.append(H);
+            }else{
+                distanceNum += 1;
+                sb.append(floorNum);
+            }
+            sb.append(distanceNum < 10 ? "0" + distanceNum : distanceNum).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
---
+++ Week2/Day14/bj10797_wsb.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int day = Integer.parseInt(br.readLine());
+        String carInfo[] = br.readLine().split(" ");
+        int cnt = 0;
+
+        for(int i = 0; i < 5; i++){
+            if(Integer.parseInt(carInfo[i]) == day) cnt++; 
+        }
+
+        System.out.print(cnt);
+    }
+}
