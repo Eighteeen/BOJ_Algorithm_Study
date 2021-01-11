e8db6edf3af334cff58821267cf4babc9dac1284
cousim46 <wjdghldns24@naver.com>
2021-01-12 00:54:57 +0900

    Day13피드백 반영

--- Week2/Day13/bj2798_jhw.java
+++ Week2/Day13/bj2798_jhw.java
@@ -10,27 +10,26 @@ public class bj2798_jhw {
 		int M = sc.nextInt();
 		//// 어떤 의미에서 mArr라고 변수 지정을 하신지 모르겠어요
 		//// 위의 피드백에 동감해요. 명확한 약어가 아니라면 줄여쓰는건 오히려 독인거같아요
-		int [] mArr = new int[N];
+		//// 수정했습니다.
+		int [] blackJackScore = new int[N];
 		int max = 0;
 		int sum = 0;
 		
 		for(int i =0; i < N; i++) {
 			int num = sc.nextInt();
-			mArr[i] = num;
+			blackJackScore[i] = num;
 		}
 		//// for문의 i는 k+1로, j는 i+1로 해주면 if문으로 j,k,i끼리 같아질때 처리를 안해줄 수 있을거 같아요! : 22 :33
+		////->수정했습니다
 		for(int k = 0 ; k < N-2; k++) {
-			for(int i  = 1; i < N-1; i++) {
-				for(int j = 2 ; j < N; j++ ) {
-					if(i != j && k != i && k != j) {
-						sum = mArr[k] + mArr[i] + mArr[j];
-						if(sum <= M && sum >= max) {
-							max = sum;
-						}
+			for(int i  = k+1; i < N-1; i++) {
+				for(int j = i+1 ; j < N; j++ ) {
+					sum = blackJackScore[k] + blackJackScore[i] + blackJackScore[j];
+					if(sum <= M && sum >= max) {
+						max = sum;
 					}
 				}
 			}
-		
 		}
 		System.out.println(max);
 	}
