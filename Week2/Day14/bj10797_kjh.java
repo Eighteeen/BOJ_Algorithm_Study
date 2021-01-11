b28895cec58a8ee42c08e7a431d050363e75fdce
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-11 23:38:22 +0900

    feedback14

--- Week2/Day14/bj10797_kjh.java
+++ Week2/Day14/bj10797_kjh.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//// 깔끔하네요!
+//// 깔끔하네요! : 22 깔끔해요 굳굳
 class Main {
   public static void main(String[] args) throws Exception {
     final int FORBIDDEN = Input.nextInt();
--- Week2/Day14/bj10797_wsb.java
+++ Week2/Day14/bj10797_wsb.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+        ////깔끔해요 따보오옹!
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
