1a0e4a3ea5d6b2bc6f9614c222df97bcfecd6256
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-13 21:20:48 +0900

    Update bj10708_wsb.java

--- Week3/Day16/bj10708_wsb.java
+++ Week3/Day16/bj10708_wsb.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//// 효율적이고 코드도 깔꼼쓰~
+//// 효율적이고 코드도 깔꼼쓰~ :22
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +12,7 @@ class Main{
         int gameLen = Integer.parseInt(br.readLine());
         int targets[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int predictTargets[], friendScores[] = new int[friendNum];
-
+        //// 오 for문 이렇게 간단히 쓰는거 신기하네요
         for(int target : targets){
             predictTargets = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             for(int j = 0; j < friendNum; j++){
