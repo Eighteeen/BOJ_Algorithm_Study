368c4d1b38ca2fe90816f1085a34b1b772e37306
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-16 02:43:00 +0900

    Add files via upload

---
+++ Week3/Day19/bj1181_lye.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Test
+{
+	public static void Main()
+	{
+		int wordCnt = Convert.ToInt32(Console.ReadLine());
+		string[] words = read_string(wordCnt);
+		int[] wordLengths = get_lengths(words);
+		int min = wordLengths.Min();
+		List<string> sortedWords = new List<string>();
+		StringBuilder sb = new StringBuilder();
+
+		while (min != 51)
+		{
+			List<string> minWords = new List<string>();
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (wordLengths[i] == min)
+				{
+					minWords.Add(words[i]);
+					wordLengths[i] = 51;
+				}
+			}
+			minWords.Sort();
+			sortedWords.AddRange(minWords);
+			min = wordLengths.Min();
+		}
+
+		foreach (string word in sortedWords.Distinct())
+			sb.Append(word + "\n");
+		Console.Write(sb);
+	}
+
+	static string[] read_string(int n)
+	{
+		string[] strings = new string[n];
+		for (int i = 0; i < n; i++)
+			strings[i] = Console.ReadLine();
+		return strings;
+	}
+	static int[] get_lengths(string[] strs)
+	{
+		int len = strs.Length;
+		int[] lengths = new int[len];
+		for (int i = 0; i < len; i++)
+			lengths[i] = strs[i].Length;
+		return lengths;
+	}
+}
