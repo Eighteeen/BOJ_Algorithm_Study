99333fffcd1cdc02608dee9448aee4cdfcc6ec03
wsb0722 <wsb0722@naver.com>
2021-01-16 00:18:34 +0900

    Solve Day19

---
+++ Week3/Day19/bj1181_wsb.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        String nowStr;
+        int strLen;
+        Set<String> strSet[] = new Set[51];
+
+        for(int i = 0; i < N; i++){
+            nowStr = br.readLine();
+            strLen = nowStr.length();
+            if(strSet[strLen] == null) strSet[strLen] = new TreeSet<>();
+            strSet[strLen].add(nowStr);
+        }
+
+        for(int i = 1; i < 51; i++){
+            if(strSet[i] != null) sb.append(strSet[i].stream().collect(Collectors.joining("\n")) + "\n");
+        }
+        System.out.print(sb);
+    }
+}
---
+++ Week3/Day19/bj17202_wsb.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ANum = br.readLine();
+        String BNum = br.readLine();
+        int compatiNum[] = new int[16];
+        for(int i = 0; i < 8; i++){
+            int index = i * 2;
+            compatiNum[index] = ANum.charAt(i) - '0';
+            compatiNum[index + 1] = BNum.charAt(i) - '0';
+        }
+
+        for(int i = 15; i > 1; i--){
+            for(int j = 0; j < i; j++){
+                compatiNum[j] = (compatiNum[j] + compatiNum[j + 1]) % 10;
+            }
+        }
+        System.out.print(compatiNum[0] + "" + compatiNum[1]);
+    }
+}
