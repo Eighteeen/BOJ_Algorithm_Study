9096aa12d9f8b985890c82e3876876a3f8f7a9c4
김진홍 <deepred.kim@gmail.com>
2021-01-15 12:09:45 +0900

    19일차: n+1일차 문제

--- Week3/Day15/bj19945_kjhjhw.java
+++ Week3/Day15/bj19945_kjhjhw.java
@@ -3,24 +3,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-////이렇게 구현해도 간단하게 풀릴 수 있네요 멋집니다! : 22 구현을 어느정도 하셨는데도 짧고 꽤 효율적으로 잘 하신 거 같아요! 멋져요!
-public class bj19945_kjhjhw {
-
+public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
         
-		if(N  < 0 ) {
-			System.out.println(32);
+		if (N < 0) {
+			System.out.print(32);
 			return;
 		}
+
 		int cnt = 0;
 		do {
-			N = N / 2 ;
+			N = N / 2;
 			cnt++;
-		}while(N != 0 );
-		System.out.println(cnt);
-		
-	}
+		} while(N != 0 );
 
+		System.out.print(cnt);
+	}
 }
