c82c27faba5398dcc1d9b6a2be4f635e82a79435
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-16 16:05:56 +0900

    Update bj10874_kjh.java

--- Week3/Day18/bj10874_kjh.java
+++ Week3/Day18/bj10874_kjh.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
-
+//// 깔끔합니다 ~~~
 class Main {
   public static void main(String[] args) throws Exception {
     StringBuilder sb = new StringBuilder();
