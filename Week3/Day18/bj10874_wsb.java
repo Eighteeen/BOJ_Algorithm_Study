3dede7066717f50471252dfe9659d1995b5736cd
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-16 16:06:28 +0900

    Update bj10874_wsb.java

--- Week3/Day18/bj10874_wsb.java
+++ Week3/Day18/bj10874_wsb.java
@@ -8,7 +8,7 @@ class Main{
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        //// 아..! 굳이 int[]로 바꿔줄 필요 없이 문자열로 비교해도 되구나.. 배워갑니다
+        //// 아..! 굳이 int[]로 바꿔줄 필요 없이 문자열로 비교해도 되구나.. 배워갑니다 :22 와우 이건 진짜 깔끔하게 풀 수 있는거였네요
         for(int i = 1; i <= N; i++){
             if(br.readLine().equals("1 2 3 4 5 1 2 3 4 5")) sb.append(i + "\n");
         }
