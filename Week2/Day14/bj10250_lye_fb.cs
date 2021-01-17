0608099a411bb1d2ed39d383d17c13f1d3822612
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-17 16:01:03 +0900

    Update bj10250_lye_fb.cs

--- Week2/Day14/bj10250_lye_fb.cs
+++ Week2/Day14/bj10250_lye_fb.cs
@@ -6,30 +6,26 @@ public class Test
 	public static void Main()
 	{
 		int repeat = Read_int();
-		int H, W, customerNum, cnt;
+		int H, W, customerNum, roomFloor, roomNum;
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < repeat; i++)
 		{
 			int[] toFindInfo = Read_ints();
-			H = toFindInfo[0] + 1;
-			W = toFindInfo[1] + 1;
+			H = toFindInfo[0];
+			W = toFindInfo[1];
 			customerNum = toFindInfo[2];
-			cnt = 0;
 
-			for (int j = 1; j < W; j++)
-			{
-				for (int k = 1; k < H; k++)
-				{
-					cnt++;
-					if (cnt == customerNum)
-					{
-						sb.Append(k.ToString());
-						sb.Append(j < 10 ? "0" : "");
-						sb.Append(j.ToString() + "\n");
-					}
-				}
-			}
+			roomFloor = customerNum % H;
+			roomNum = customerNum / H;
+
+			if (roomFloor == 0) roomFloor = H;
+			else roomNum++;
+
+			sb.Append(roomFloor)
+				.Append(roomNum < 10 ? "0" : "")
+				.Append(roomNum)
+				.Append("\n");
 		}
 		Console.Write(sb);
 	}
@@ -51,3 +47,4 @@ public class Test
 		return numArr;
 	}
 }
+
