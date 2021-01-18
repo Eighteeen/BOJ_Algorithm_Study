93347e6b93656a3f229774ba0d5c1c604cbba413
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-19 01:21:22 +0900

    Update bj1920_lye.cs

--- Week3/Day20/bj1920_lye.cs
+++ Week3/Day20/bj1920_lye.cs
@@ -15,6 +15,7 @@ public class Test
 		StringBuilder sb = new StringBuilder();
 
 		//// 어 저도 처음에 이렇게 했더니 시간초과 나던데.. 채점기록 보니까 여러번 시간초과 후에 성공하신거 같은데 시간을 단축한 비결이 있나요? : 22 저도 궁금합니다
+		//// 처음에는 string으로 해서 비교했다가 검색해보니 string으로 찾는게 더 시간이 오래 걸린다 해서 int로 바꿔줬더니 성공했어요!
 		for (int i = 0; i < inputCnt; i++)
 		{
 			if (compare.Contains(input[i]))
