d8750e5ceb1ce9095dab922a7bc189c1944f3f28
김진홍 <deepred.kim@gmail.com>
2021-01-25 23:31:13 +0900

    Solve Day29 with wsb, lye
    - 페어프로그래밍 풀이
    - 드라이버 김진홍
    - 네비게이터 위승빈, 이예은

---
+++ Week5/bj1874_kjh.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    Stack<Integer> stack = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+    final int N = Input.nextInt();
+
+    int toAdd = 1;
+    for (int i = 0; i < N; i++) {
+      int now = Input.nextInt();
+      while (toAdd <= now) {
+        stack.push(toAdd++);
+        sb.append("+\n");
+      }
+
+      if (stack.empty()) continue;
+
+      int peek = stack.peek();
+      if (peek == now) {
+        stack.pop();
+        sb.append("-\n");
+      }
+      if (peek > now) {
+        System.out.print("NO");
+        return;
+      }
+    }
+
+    System.out.print(sb);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
