27b98dd39db81dba9785fd13174ba46b3159d846
wsb0722 <wsb0722@naver.com>
2021-01-27 07:41:19 +0900

    Solve Day30

---
+++ Week5/Day30/bj12605_wsb.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Stack strStack = new Stack<>();
+
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            while(st.hasMoreTokens()){
+                strStack.push(st.nextToken());
+            }
+
+            bw.write("Case #" + i + ": ");
+            while(!strStack.empty()){
+                bw.write(strStack.pop() + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ Week5/Day30/bj20301_wsb.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int orders[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int entry = orders[0], removeRule = orders[1], converseRule = orders[2];
+        Deque<Integer> personDeque = new ArrayDeque<>();
+        int removeNum = 0;
+        boolean isAdvance = true;
+
+        fillOneToNumInDeque(personDeque, entry);
+
+        while(!personDeque.isEmpty()){
+            if(removeNum == converseRule) isAdvance = !isAdvance;
+            removeNum %= converseRule;
+            if(isAdvance) bw.write(progressAdvance(personDeque, removeRule) + "\n");
+            else bw.write(progressRevere(personDeque, removeRule) + "\n");
+            removeNum++;
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    
+    static void fillOneToNumInDeque(Deque<Integer> deque, int num){
+        for(int i = 1; i <= num; i++){
+            deque.addLast(i);
+        }
+    }
+
+    static int progressAdvance(Deque<Integer> deque, int ruleNum){
+        for(int i = 1; i < ruleNum; i++){
+            deque.addLast(deque.removeFirst());
+        }
+        return deque.removeFirst();
+    }
+
+    static int progressRevere(Deque<Integer> deque, int ruleNum){
+        for(int i = 1; i < ruleNum; i++){
+            deque.addFirst(deque.removeLast());
+        }
+        return deque.removeLast();
+    }
+}
