d9300fc08538e8c22cbeafac4c61af859a60b58e
김진홍 <deepred.kim@gmail.com>
2021-01-26 20:29:38 +0900

    30일차: 오늘의 문제

---
+++ Week5/Day30/bj12605_kjh.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    StringBuilder sb = new StringBuilder();
+    Stack<String> words = new Stack<>();
+    final int N = Input.nextInt();
+    
+    for (int i = 1; i <= N; i++) {
+      sb.append("Case #").append(i).append(": ");
+
+      for (String word : Input.nextLine().split(" ")) {
+        words.push(word);
+      }
+
+      while (!words.empty()) {
+        sb.append(words.pop())
+          .append(' ');
+      }
+      sb.append('\n');
+    }
+    
+    System.out.print(sb);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
---
+++ Week5/Day30/bj20301_kjh.java
@@ -0,0 +1,125 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.stream.IntStream;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    StringBuilder sb = new StringBuilder();
+
+    final int NUMBER_OF_PEOPLE = Input.nextInt();
+    final int REMOVE_NTH_PERSON = Input.nextInt();
+    final int REVERSE_EVERY_NTH = Input.nextInt();
+    
+    Josephus josephus = new Josephus(NUMBER_OF_PEOPLE, REVERSE_EVERY_NTH);
+
+    for (int i = 1; i <= NUMBER_OF_PEOPLE; i++) {
+      sb.append(josephus.popNth(REMOVE_NTH_PERSON))
+        .append('\n');
+      
+      boolean needReverse = (i % REVERSE_EVERY_NTH) == 0;
+      if (needReverse) {
+        josephus.reverse();
+      }
+    }
+
+    System.out.print(sb);
+  }
+}
+
+class Josephus {
+  private Deque<Integer> sequence = new ArrayDeque<>();
+  
+  private boolean isNaturalOrder = true;
+  private int numberOfPeople;
+  private int reverseEveryNth;
+
+  public Josephus(int numberOfPeople, int reverseEveryNth) {
+    IntStream.range(1, numberOfPeople + 1)
+      .boxed()
+      .forEach(item -> sequence.add(item));
+    
+    this.numberOfPeople = numberOfPeople;
+    this.reverseEveryNth = reverseEveryNth;
+  }
+
+  public int popNth(int n) {
+    for (int i = 0; i < (n - 1); i++) {
+      popAndPush();
+    }
+
+    return pop();
+  }
+  
+  public void reverse() {
+    isNaturalOrder = !isNaturalOrder;
+  }
+
+  private void popAndPush() {
+    if (isNaturalOrder) {
+      sequence.offerLast(sequence.pollFirst());
+      return;
+    }
+
+    sequence.offerFirst(sequence.pollLast()); 
+  }
+
+  private int pop() {
+    if (isNaturalOrder) {
+      return sequence.pollFirst();
+    }
+    
+    return sequence.pollLast();
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
