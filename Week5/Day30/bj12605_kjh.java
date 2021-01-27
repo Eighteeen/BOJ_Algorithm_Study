b0ad3380d031f92a21301180d66f6e198a94ea30
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-27 23:17:10 +0900

    Update bj12605_kjh.java

--- Week5/Day30/bj12605_kjh.java
+++ Week5/Day30/bj12605_kjh.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Stack;
-
+//// 깔꼼합니당 
 class Main {
   public static void main(String[] args) throws Exception {
     StringBuilder sb = new StringBuilder();
