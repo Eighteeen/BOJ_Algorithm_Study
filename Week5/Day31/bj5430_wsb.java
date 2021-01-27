329c56fae68e306a3fa3802b21c9bb394787edef
wsb0722 <wsb0722@naver.com>
2021-01-28 08:19:30 +0900

    Solve Day31

---
+++ Week5/Day31/bj5430_wsb.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int ERROR = -1, ADVANCE = 0, REVERSE = 1;
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        String command;
+        int arrLen;
+        Deque<Integer> arrDeque;
+
+        for(int i = 0; i < T; i++){
+            command = br.readLine();
+            arrLen = Integer.parseInt(br.readLine());
+            String arrInfo = br.readLine();
+            if(arrLen == 0){
+                if(command.contains("D")) sb.append("error\n");
+                else sb.append("[]\n");
+                continue;
+            }
+
+            arrDeque = new ArrayDeque<>(Arrays.stream(arrInfo.substring(1, arrInfo.length() - 1).split(","))
+                            .map(Integer::valueOf).collect(Collectors.toList()));
+
+            int result = getResultAfterCommand(command, arrDeque);
+            if(result == ERROR) sb.append("error\n");
+            else printArr(arrDeque, result);
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    static int getResultAfterCommand(String command, Deque<Integer> arrDeque){
+        int result = 0;
+
+        for(char method : command.toCharArray()){    
+            if(method == 'R'){
+                result = (result + 1) % 2;
+                continue;
+            }
+
+            if(method == 'D' && arrDeque.isEmpty()) return ERROR;
+
+            if(result == ADVANCE) arrDeque.removeFirst();
+            else arrDeque.removeLast();
+        }
+
+        return result;
+    }
+
+    static void printArr(Deque<Integer> arrDeque, int direction){
+        if(direction == ERROR) return;
+
+        sb.append("[");
+        if(direction == ADVANCE) printAdvance(arrDeque);
+        else printReverse(arrDeque);
+        sb.append("]\n");
+    }
+
+    static void printAdvance(Deque<Integer> arrDeque){
+        while(!arrDeque.isEmpty()){
+            sb.append(arrDeque.removeFirst());
+            if(!arrDeque.isEmpty()) sb.append(",");
+        }
+    }
+
+    static void printReverse(Deque<Integer> arrDeque){
+        while(!arrDeque.isEmpty()){
+            sb.append(arrDeque.removeLast());
+            if(!arrDeque.isEmpty()) sb.append(",");
+        }
+    }
+}
