0af115094513ab236da4ca1fa06141cc9c417a59
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-01-29 01:45:02 +0900

    Update bj12605_lye.cs

--- Week5/Day30/bj12605_lye.cs
+++ Week5/Day30/bj12605_lye.cs
@@ -15,12 +15,12 @@ public class Test
 			string[] input = ReadString();
 			int len = input.Length;
 			Stack stack = new Stack();
-			sb.Append("Case #" + i + ":");
 
 			//// 여기 for문은 출력과 상관없이 스택을 구성하는 코드라서 위에 Case문이 있는 게 좀 어색하게 느껴져요. 출력과 함께 쓰여있다면 걸리는 거 없이 쭉 읽힐 것 같아요!
 			for (int j = 0; j < len; j++)
 				stack.Push(input[j]);
-
+			
+			sb.Append("Case #" + i + ":");
 			for (int j = 0; j < len; j++)
 				sb.Append(" " + stack.Pop());
 
