3e3a0f8b8f3dd820143065c071e6f445501f6d78
김진홍 <deepred.kim@gmail.com>
2021-01-30 14:42:34 +0900

    34일차: 오늘의 문제

---
+++ Week5/Day34/bj1966_kjh.java
@@ -0,0 +1,127 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    StringBuilder sb = new StringBuilder();
+    final int TEST_CASES = Input.nextInt();
+
+    for (int i = 0; i < TEST_CASES; i++) {
+      int numberOfDocs = Input.nextInt();
+      int targetDoc = Input.nextInt();
+      String priority = Input.nextLine();
+      PrinterQueue printerQueue = new PrinterQueue(numberOfDocs, priority);
+
+      for (int nth = 1; nth <= numberOfDocs; nth++) {
+        int printedDoc = printerQueue.print();
+        if (printedDoc == targetDoc) {
+          sb.append(nth)
+            .append('\n');
+          break;
+        }
+      }
+    }
+    
+    System.out.print(sb);
+  }
+}
+
+class PrinterQueue {
+  private Queue<Integer> queue;
+  private Queue<Integer> priorities;
+
+  public PrinterQueue(int numberOfDocs, String strPriorities) {
+    queue = new LinkedList<>();
+    IntStream.range(0, numberOfDocs)
+      .boxed()
+      .forEach(docNumber -> queue.add(docNumber));
+
+    priorities = new LinkedList<>();
+    Arrays.stream(strPriorities.split(" "))
+      .mapToInt(Integer::parseInt)
+      .forEach(priority -> priorities.add(priority));
+  }
+
+  public int print() {
+    while (queue.size() > 0) {
+      int highestPriority = getHighestPriority();
+      
+      int dequeued = queue.remove();
+      int priority = priorities.remove();
+
+      if (priority == highestPriority) {
+        return dequeued;
+      }
+      
+      queue.add(dequeued);
+      priorities.add(priority);
+    }
+
+    return -1;
+  }
+
+  public int getHighestPriority() {
+    return priorities.stream().max(Integer::compareTo).orElse(-1);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
