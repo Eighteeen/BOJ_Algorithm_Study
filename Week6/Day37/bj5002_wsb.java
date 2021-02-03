3de10542dc945d28b523ff3e38ee394118058518
김진홍 <deepred.kim@gmail.com>
2021-02-03 23:27:08 +0900

    38일차: n-1일차 피드백

--- Week6/Day37/bj5002_wsb.java
+++ Week6/Day37/bj5002_wsb.java
@@ -25,6 +25,7 @@ class Main{
 
             if(peopleStack.size() > possibleSize) break;
         }
+        //// 함수의 이름을 함수 내용을 봐도 어떤 역할을 하는 코드인지 잘 모르겠습니다 ㅠㅠ 어떤 예외처리를 하는거 같긴한데..
         cntPeople += cntOnlyEnterClub(peopleStack, X, possibleSize);
 
         System.out.print(cntPeople);
@@ -41,6 +42,7 @@ class Main{
         return 0;
     }
 
+    //// possible이라는 변수가 사실상 remember+1이니, remember만 받아서 안쪽에서 int possible = remember = 1; 하면 매개변수가 줄어서 더 깔끔하지 않을까유?
     static int cntOnlyEnterClub(Stack<Character> peopleStack, int remember, int possible){
         if(peopleStack.size() < possible) return  peopleStack.size();
         return remember;
