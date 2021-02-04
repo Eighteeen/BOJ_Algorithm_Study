67b960c97c7b6cbf13786f1cdcf1118b87abc024
김진홍 <deepred.kim@gmail.com>
2021-02-04 20:17:20 +0900

    39일차: 어제 피드백

--- Week6/Day38/bj17298_wsb.java
+++ Week6/Day38/bj17298_wsb.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Stack;
 
+//// 배워갑니다 센세이
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
