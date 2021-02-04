eeae0ab9d56530b7ea45519b30d23ea4edb95d93
wsb0722 <wsb0722@naver.com>
2021-02-04 23:32:07 +0900

    Reflect Feedback Day37

--- Week6/Day37/bj5002_wsb.java
+++ Week6/Day37/bj5002_wsb.java
@@ -41,7 +41,10 @@ class Main{
         return 0;
     }
 
-    //// possible이라는 변수가 사실상 remember+1이니, remember만 받아서 안쪽에서 int possible = remember = 1; 하면 매개변수가 줄어서 더 깔끔하지 않을까유?
+    //// possible이라는 변수가 사실상 remember+1이니, remember만 받아서 안쪽에서 int possible = remember + 1; 하면 매개변수가 줄어서 더 깔끔하지 않을까유?
+    //// -> 함수는 어떤 문제를 추상화하니 remember과 possible을 따로 두어야 활용성에서 좀 더 좋다고 생각합니다.
+        //// 예를 들어 클럽 지침이 바뀌거나 도어맨이 바뀌었을 때 remember과 possible의 차이가 더이상 1이 아닐 수도 있다고 생각해서 이 부분은 유지하도록 하겠습니다!
+        //// 하지만 문제 자체로만 보았을 때는 코드의 간결함을 위해 좋은 피드백이라고 생각합니다. 피드백 감사해요!
     static int cntOnlyEnterClub(Stack<Character> peopleStack, int remember, int possible){
         if(peopleStack.size() < possible) return  peopleStack.size();
         return remember;
