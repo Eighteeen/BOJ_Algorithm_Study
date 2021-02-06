b43647a811259b628067c0906fa5570aa6c380e8
wsb0722 <wsb0722@naver.com>
2021-02-07 02:56:25 +0900

    Solve Day41

---
+++ Week6/Day41/bj17225_wsb.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String todayShopInfo[] = br.readLine().split(" ");
+        int A = Integer.parseInt(todayShopInfo[0]);
+        int B = Integer.parseInt(todayShopInfo[1]);
+        int N = Integer.parseInt(todayShopInfo[2]);
+        Deque<Integer> BPackStart = new ArrayDeque();
+        Deque<Integer> RPackStart = new ArrayDeque();
+
+        for(int i = 0; i < N; i++){
+            String customerInfo[] = br.readLine().split(" ");
+            int t = Integer.parseInt(customerInfo[0]);
+            char c = customerInfo[1].charAt(0);
+            int m = Integer.parseInt(customerInfo[2]);
+
+            if(c == 'B') offerPackStart(BPackStart, A, t, m);
+            else offerPackStart(RPackStart, B, t, m);
+        }
+
+        List<Integer> BPackList = new ArrayList<>();
+        List<Integer> RPackList = new ArrayList<>();
+        int cntPack = 0;
+        while(!BPackStart.isEmpty() || !RPackStart.isEmpty()){
+            while(!BPackStart.isEmpty() && (RPackStart.isEmpty() || RPackStart.peek() >= BPackStart.peek())){
+                BPackStart.poll();
+                BPackList.add(++cntPack);
+            }
+            
+            while(!RPackStart.isEmpty() && (BPackStart.isEmpty() || BPackStart.peek() > RPackStart.peek())){
+                RPackStart.poll();
+                RPackList.add(++cntPack);
+            }
+        }
+
+        System.out.print(getResult(BPackList, RPackList));
+        br.close();
+    }
+
+    static void offerPackStart(Deque<Integer> pack, int takeTime, int orderTime, int cnt){
+        if(!pack.isEmpty() && (pack.peekLast() + takeTime) > orderTime) orderTime = pack.peekLast() + takeTime;
+        orderTime -= takeTime;
+        for(int i = 0; i < cnt; i++){
+            pack.offer(orderTime += takeTime);
+        }
+    }
+
+    static String getResult(List<Integer> List1, List<Integer> List2){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(List1.size()).append("\n");
+        List1.forEach(l -> sb.append(l).append(" "));
+        sb.append("\n");
+        sb.append(List2.size()).append("\n");
+        List2.forEach(l -> sb.append(l).append(" "));
+
+        return sb.toString();
+    }
+}
