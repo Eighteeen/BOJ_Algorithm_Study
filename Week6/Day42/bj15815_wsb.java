35e32a3974449dfe02457fd7e3ec90773c433203
jjklunicc <55953594+jjklunicc@users.noreply.github.com>
2021-02-10 22:20:06 +0900

    Update bj15815_wsb.java

--- Week6/Day42/bj15815_wsb.java
+++ Week6/Day42/bj15815_wsb.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-//// 깔꼼쓰효율쓰
+//// 깔꼼쓰효율쓰 :22 굿이에요!
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
