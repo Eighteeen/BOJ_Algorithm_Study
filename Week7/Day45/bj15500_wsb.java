eafe6b6139950cc7336f39ead6f465dc1bb65ff9
김진홍 <deepred.kim@gmail.com>
2021-02-11 17:23:01 +0900

    46일차: 어제 피드백

--- Week7/Day45/bj15500_wsb.java
+++ Week7/Day45/bj15500_wsb.java
@@ -10,10 +10,14 @@ import java.util.stream.Collectors;
 class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    //// 문제의 각 요소간의 의존 관계가 복잡하게 얽혀있어서, 함수나 클래스로 분리하기 정말 까다로운 문제였던 것 같아요..
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
         Stack<Integer>[] rods = new Stack[3];
         for(int i = 1; i < 3; i++) rods[i] = new Stack<>();
+        //// Arrays.stream로 만든 stream을 이용하여 바로 스택에 추가할 수 있어요!
+        //// Arrays.stream(...).forEach(item -> rods[1].push(item)); 이러면 됩니다
         rods[1].addAll(Arrays.stream(br.readLine().split(" ")).map(Integer::valueOf).collect(Collectors.toList()));
 
         final int SHOULD_TO_ROD = 3;
@@ -35,6 +39,7 @@ class Main{
                 nextMove--;
             }
 
+            //// 오.. from과 to를 바꿔주는 방식이 더 괜찮네요!
             if(rods[from].empty()){
                 int tmp = from;
                 from = to;
