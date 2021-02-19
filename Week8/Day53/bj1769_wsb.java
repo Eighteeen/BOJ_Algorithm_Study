0583b991098d6955ca9966a3784e294422130634
김진홍 <deepred.kim@gmail.com>
2021-02-19 21:37:23 +0900

    54일차: 어제 피드백

--- Week8/Day53/bj1769_wsb.java
+++ Week8/Day53/bj1769_wsb.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+//// 깔꼼~
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,14 +14,17 @@ class Main{
     }
 
     static StringBuilder answerOfQuestionTwo(String numStr, int cnt){
+        //// 만일을 위한 AND 조건인가요?? (그냥 궁금한 것)
         if(numStr.length() == 1 && Integer.parseInt(numStr) < 10){
             StringBuilder sb = new StringBuilder();
+            //// 결과를 반환하게 할 수도 있구나.. 발상 배워갑니다
             if(isMultiple(Integer.parseInt(numStr), 3)) return sb.append(cnt).append("\nYES");
             else return sb.append(cnt).append("\nNO");
         }
 
         int sum = 0;
         for(char c : numStr.toCharArray()){
+            //// 의미전달도 되면서 간략한 charToInt 어떠쉰?
             sum += convertCharToInt(c);
         }
 
