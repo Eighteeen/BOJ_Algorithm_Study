b3ab3390c0a080bc8b3be6d560d6117c70b6c9e2
wsb0722 <wsb0722@naver.com>
2021-02-21 00:40:37 +0900

    Update bj30701_wsb.java

--- Week8/Day54/bj30701_wsb.java
+++ Week8/Day54/bj30701_wsb.java
@@ -8,12 +8,12 @@ class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print(printFlagStar(Integer.parseInt(br.readLine()), null));
+        System.out.print(printStarFlag(Integer.parseInt(br.readLine()), null));
 
         br.close();
     }
 
-    static StringBuilder printFlagStar(int n, List<StringBuilder> untilFlag){ // When calling this method, use null for List parameter 
+    static StringBuilder printStarFlag(int n, List<StringBuilder> untilFlag){ // When calling this method, use null for List parameter 
         if(n < 0){
             StringBuilder resultFlag = new StringBuilder();
             untilFlag.forEach(sb -> resultFlag.append(sb).append("\n"));
@@ -31,6 +31,6 @@ class Main{
             currentFlag.addAll(untilFlag);
         }
 
-        return printFlagStar(--n, currentFlag);
+        return printStarFlag(--n, currentFlag);
     }
 }
