a83b7041fed90734aa277c24d333266fbafdcb4e
김진홍 <deepred.kim@gmail.com>
2021-02-21 17:52:57 +0900

    56일차: n-1일차 피드백

--- Week8/Day55/bj5568_wsb.java
+++ Week8/Day55/bj5568_wsb.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//// 와우 효율성 굳! 제 코드보다 두배 효율적이에요
 class Main{
     static Set<String> possibleSet;
     public static void main(String[] args) throws IOException {
@@ -21,6 +22,8 @@ class Main{
         br.close();
     }
 
+    //// 이 재귀함수는 더욱 for문 쓰는 것 처럼 작성하신 것 같아 아쉽습니당.. for문 위에 선언할 지역변수를 매개변수로 옮긴 것 같아요
+    //// 효율성은 조금 떨어질 수 있지만, 재귀함수를 공부하는 만큼 재귀함수를 여러 방법으로 활용해보시면 좋을 것 같아요!
     static Set possibleSetOfCanMake(List<String> objectList, int len, int useNum, int cntUse){
         boolean isUseAll = cntUse == (useNum - 1);
         if(isUseAll || len == 0){
