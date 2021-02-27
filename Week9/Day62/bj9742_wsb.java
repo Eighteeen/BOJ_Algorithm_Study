a68ae9d085606f009c7a43301acc93ba803b4f8d
wsb0722 <wsb0722@naver.com>
2021-02-28 04:17:42 +0900

    Update bj9742_wsb.java

--- Week9/Day62/bj9742_wsb.java
+++ Week9/Day62/bj9742_wsb.java
@@ -15,8 +15,7 @@ class Main{
             List<Character> charList = stringToCharacterList(TC[0]);
             int wonder = Integer.parseInt(TC[1]);
 
-            sb.append(TC[0]).append(" ")
-                .append(TC[1]).append(" = ")
+            sb.append(input).append(" = ")
                 .append(getPermutation(charList, wonder - 1)).append("\n");
         }
 
