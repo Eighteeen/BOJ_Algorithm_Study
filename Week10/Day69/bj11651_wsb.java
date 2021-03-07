89b9fa42383a5c5f0a8b999e934b7a0cfa7b7c32
김진홍 <deepred.kim@gmail.com>
2021-03-07 14:15:20 +0900

    70일차: n-1일차 피드백

--- Week10/Day69/bj11651_wsb.java
+++ Week10/Day69/bj11651_wsb.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+//// 깔꼼합니다!
+//// compareTo를 구현하면 객체 배열 정렬할때 기준이 돼주는군요
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
