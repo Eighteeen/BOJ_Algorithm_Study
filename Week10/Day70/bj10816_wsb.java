19c3efc4e08c5eaeb80a01e32b4286bb41e4e3ab
김진홍 <deepred.kim@gmail.com>
2021-03-08 10:28:58 +0900

    Update bj10816_wsb.java

--- Week10/Day70/bj10816_wsb.java
+++ Week10/Day70/bj10816_wsb.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+//// 굳굳!
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
