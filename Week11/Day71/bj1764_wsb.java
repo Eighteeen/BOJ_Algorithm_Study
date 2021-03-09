ce06a2dd840e97b37166ec86e590eb208645a1f3
김진홍 <deepred.kim@gmail.com>
2021-03-09 18:04:49 +0900

    72일차: 어제자 피드백

--- Week11/Day71/bj1764_wsb.java
+++ Week11/Day71/bj1764_wsb.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//// 효율깔꼼깔꼼합니다
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
