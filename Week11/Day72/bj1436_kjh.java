df3ac6b41aa53650fb29e8815ef301508addd735
김진홍 <deepred.kim@gmail.com>
2021-03-09 18:03:47 +0900

    72일차: 오늘의 문제

---
+++ Week11/Day72/bj1436_kjh.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) {
+    final int N = Input.nextInt();
+    System.out.print(nthSixSixSix(N));
+  }
+
+  static int nthSixSixSix(int nth) {
+    int cnt = 0;
+    int number = 665;
+
+    do {
+      number++;
+      if (isSixSixSix(number)) {
+        cnt++;
+      }
+    } while (cnt != nth);
+
+    return number;
+  }
+  
+  static boolean isSixSixSix(int number) {
+    int strike = 0;
+    while (number > 0) {
+      if (number % 10 == 6) { 
+        strike++;
+      } else {
+        strike = 0;
+      }
+
+      if (strike == 3) return true;
+      number = number / 10;
+    }
+    return false;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
