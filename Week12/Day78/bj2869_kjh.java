d2ff0984a98694568e7954e24c384ed49243833c
Seungbeen Wee <wsb0722@naver.com>
2021-03-16 11:33:44 +0900

    Feedback Day78

--- Week12/Day78/bj2869_kjh.java
+++ Week12/Day78/bj2869_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// 깔끔합니다.
 class Main {
   public static void main(String[] args) throws Exception {
     int climb = Input.nextInt();
