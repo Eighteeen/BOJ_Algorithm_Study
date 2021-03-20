ed91dcad4f9f8bc9dabd45f2a99c6cc4aee1a07e
Seungbeen Wee <wsb0722@naver.com>
2021-03-20 20:42:22 +0900

    Solve Day83 with kjh
    - 페어 프로그래밍 풀이
    - 드라이버 위승빈
    - 네비게이터 김진홍

---
+++ Week12/Day83/bj6603_kjhwsb.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    static List<Lotto> lottoList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] input;
+        while ((input = br.readLine().split(" "))[0].charAt(0) != '0') {
+            int k = Integer.parseInt(input[0]);
+            List<Integer> numList = new ArrayList<>();
+            for (int i = 1; i <= k; i++) {
+                numList.add(Integer.parseInt(input[i]));
+            }
+
+            lottoList = new ArrayList<>();
+            selectedPossibleBalls(new Lotto(), numList, 6);
+            lottoList.forEach(l -> sb.append(l.toString()).append("\n"));
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void selectedPossibleBalls(Lotto lotto, List<Integer> balls, int toSelect) {
+        if (toSelect == 0) {
+            lottoList.add(lotto);
+            return;
+        }
+
+        List<Integer> copyBalls = new ArrayList<>(balls);
+        for (Integer ball : balls) {
+            Lotto currentLotto = new Lotto(lotto);
+            currentLotto.addBall(ball);
+            copyBalls.remove(ball);
+            selectedPossibleBalls(currentLotto, copyBalls, toSelect - 1);
+        }
+    }
+}
+
+class Lotto {
+    private final int MAX_SELECT = 6;
+    private int[] balls = new int[MAX_SELECT];
+    private int pointer = 0;
+
+    public Lotto() {}
+
+    public Lotto(Lotto lotto) {
+        this.balls = lotto.getBalls().clone();
+        this.pointer = lotto.getPointer();
+    }
+
+    public int[] getBalls() {
+        return balls;
+    }
+
+    public int getPointer() {
+        return pointer;
+    }
+
+    public void addBall(int ball) {
+        balls[pointer++] = ball;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int b : balls) {
+            sb.append(b).append(" ");
+        }
+        return sb.toString();
+    }
+}
