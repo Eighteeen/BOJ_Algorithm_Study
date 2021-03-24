c6fc67f6f2bdf7ec44b9c76c2b2fca0223bc2963
Seungbeen Wee <wsb0722@naver.com>
2021-03-24 22:22:07 +0900

    Update bj1631_wsb.java

--- Week13/Day85/bj1631_wsb.java
+++ Week13/Day85/bj1631_wsb.java
@@ -8,7 +8,6 @@ import java.util.List;
 class Main {
     static int N, M;
     static char[] discBars, wantBars;
-    static int cntMove;
     static int checkDiscIdx;
 
     public static void main(String[] args) throws IOException {
@@ -24,7 +23,7 @@ class Main {
         checkDiscIdx = N - 1;
         removeDontNeedDisc();
 
-        moveTowerOfHanoi(N, 'A', getTempBar('A', wantBars[checkDiscIdx]), wantBars[checkDiscIdx]);
+        if (N > 0) moveTowerOfHanoi(N, 'A', getTempBar('A', wantBars[checkDiscIdx]), wantBars[checkDiscIdx]);
         printDiscBars();
 
         br.close();
@@ -38,16 +37,11 @@ class Main {
         }
     }
 
-    static boolean isWantBar(int wantIdx) {
-        removeDontNeedDisc();
-        return checkDiscIdx == wantIdx;
-    }
-
-    static void moveDiscOnce(char toOnce, char toLastDisc, int lastDiscIdx) {
-        for (int i = 0; i < lastDiscIdx; i++) {
-            discBars[i] = toOnce;
+    static void moveDiscOnce(int discToTmp, char tmp, char to) {
+        for (int i = 0; i < discToTmp; i++) {
+            discBars[i] = tmp; 
         }
-        discBars[lastDiscIdx] = toLastDisc;
+        discBars[discToTmp] = to;
     }
 
     static void moveTowerOfHanoi(int disc, char from, char tmp, char to) {
@@ -55,31 +49,28 @@ class Main {
 
         int aboveDisc = disc - 1;
         if (Math.pow(2, aboveDisc) <= M) {
-            moveDiscOnce(tmp, to, aboveDisc);
+            moveDiscOnce(aboveDisc, tmp, to);
             M -= Math.pow(2, aboveDisc);
-
-            int checkIdx = (aboveDisc == 0 ? 0 : aboveDisc - 1);
-            if (to != wantBars[checkIdx] && isWantBar(checkIdx)) {
-                from = discBars[checkIdx];
-                to = wantBars[checkIdx];
+            if (M == 0) return;
+            
+            removeDontNeedDisc();
+            if (N <= aboveDisc) {
+                from = tmp;
+                to = wantBars[checkDiscIdx];
                 tmp = getTempBar(from, to);
-                moveTowerOfHanoi(disc, from, tmp, to);
+                moveTowerOfHanoi(N, from, tmp, to);
             } else {
                 moveTowerOfHanoi(aboveDisc, tmp, from, to);
             }
-        }
-        // else {
+        } else {
             moveTowerOfHanoi(aboveDisc, from, to, tmp);
-        // }
+        }
     }
     
-    static char getTempBar(char from, char to) {
-        // if (from == 'A') return (to == 'B' ? 'C' : 'B');
-        // if (from == 'B') return (to == 'A' ? 'C' : 'A');
-        // return (to == 'A' ? 'B' : 'A');
+    static char getTempBar(char bar1, char bar2) {
         List<Character> bars = new ArrayList<>(Arrays.asList('A', 'B', 'C'));
-        bars.remove(Character.valueOf(from));
-        bars.remove(Character.valueOf(to));
+        bars.remove(Character.valueOf(bar1));
+        bars.remove(Character.valueOf(bar2));
         return bars.get(0);
     }
 
