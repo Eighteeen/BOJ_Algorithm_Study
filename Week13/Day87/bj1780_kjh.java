4111311b198a47eac843155ae2cfb299629351cd
Deepred <deepred.kim@gmail.com>
2021-03-24 02:10:22 +0000

    87일차: 오늘의 문제

---
+++ Week13/Day87/bj1780_kjh.java
@@ -0,0 +1,143 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int N = Input.nextInt();
+    int[][] papers = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        papers[i][j] = Input.nextInt();
+      }
+    }
+
+    PaperCutter paperCutter = new PaperCutter(papers);
+    
+    paperCutter.cut();
+    paperCutter.printPaperAmounts();
+  }
+}
+
+class PaperCutter {
+  private int[][] papers;
+  private int[] amount;
+
+  public PaperCutter(int[][] papers) {
+    this.papers = papers;
+
+    amount = new int[3];
+    amount[0] = amount[1] = amount[2] = 0;
+  }
+
+  public void cut() {
+    cut(papers.length, 0, 0);
+  }
+
+  public void cut(int size, int y, int x) {
+    if (areAllEqual(size, y, x)) {
+      amount[papers[y][x] + 1] += 1;
+      return;
+    }
+
+    int nextSize = size / 3;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        cut(nextSize, ( y + (nextSize * i) ), ( x + (nextSize * j) ));
+      }
+    }
+  }
+
+  private boolean areAllEqual(int size, int y, int x) {
+    if (size == 1) return true;
+
+    int head = papers[y][x];
+
+    for (int i = y; i < y + size; i++) {
+      for (int j = x; j < x + size; j++) {
+        if (head != papers[i][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public void printPaperAmounts() {
+    for (int i = 0; i < 3; i++) {
+      System.out.println(amount[i]);
+    }
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+  
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
