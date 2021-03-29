140d933d69d18fb2cf2afbf93da0b3a8b98d3739
Deepred <deepred.kim@gmail.com>
2021-03-29 09:42:31 +0900

    92일차: n-1일차 피드백

--- Week13/Day91/bj5904_wsb.java
+++ Week13/Day91/bj5904_wsb.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 class Main {
     static int N;
 
+    //// 굳굳 여러모로 더 효율적이네요
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,6 +15,7 @@ class Main {
         br.close();
     }
 
+    //// NthLetter를 객체처럼 대문자 시작으로 이름 지은 이유가 있나요?
     static boolean isMOfMooGame(int seqOrder, int sideLen, int NthLetter) {
         if (NthLetter < sideLen) return isMOfMooGame(seqOrder - 1, getPrevSideLen(seqOrder, sideLen, 2), NthLetter);
 
@@ -31,6 +33,7 @@ class Main {
         return sideLen * 2 + middleLen;
     }
 
+    //// orderDifference는 항상 2로 쓰여서 매개변수로 받지 않아도 될 것 같아요
     static int getPrevSideLen(int order, int sideLen, int orderDifference) {
         return (sideLen - order - orderDifference) / 2;
     }
