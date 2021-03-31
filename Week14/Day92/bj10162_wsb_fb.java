efbc38c401747202b66b748650df3a76b332ae25
Seungbeen Wee <wsb0722@naver.com>
2021-04-01 06:14:04 +0900

    Reflect Feedback Day92

---
+++ Week14/Day92/bj10162_wsb_fb.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        if (T % 10 != 0) {
+            System.out.println(-1);
+            return;
+            //// 여기다가 return 넣어주고 else문 없애서 더 깔끔하게 하는건 어때요
+            //// -> 반영했어요! 피드백 감사해요.
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int pressNum : minPressButtons(T)) sb.append(pressNum).append(" ");
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    //// 훨씬 깔끔하네요 ㄷㄷ 굳
+    static int[] minPressButtons(int cookingTime) {
+        int[] pressNums = new int[3];
+        
+        pressNums[0] = cookingTime / 300;
+        cookingTime -= pressNums[0] * 300;
+
+        pressNums[1] = cookingTime / 60;
+        cookingTime -= pressNums[1] * 60;
+
+        pressNums[2] = cookingTime / 10;
+
+        return pressNums;
+    }
+}
