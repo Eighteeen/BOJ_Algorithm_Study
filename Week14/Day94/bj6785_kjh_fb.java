5f1a98f670a56824b30a6512b110ff7b2b6db115
김진홍 <deepred.kim@gmail.com>
2021-04-02 23:09:49 +0900

    96일차: n-2일차 피드백 반영

---
+++ Week14/Day94/bj6785_kjh_fb.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//// 전체적으로 깔끔합니다.
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int TEST_CASE = Input.nextInt();
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < TEST_CASE; i++) {
+      int level = Input.nextInt();
+
+      int targetCol = Input.nextInt();
+      int targetRow = Input.nextInt();
+      sb.append(isCrystal(level, targetRow, targetCol) ? "crystal" : "empty")
+        .append('\n');
+    }
+
+    System.out.print(sb);
+  }
+
+  static boolean isCrystal(int level, int row, int col) {
+    if (row < 0 || col < 0) return false;
+
+    int prevSize = (int) Math.pow(5, level - 1);
+
+    //// row >= prevSize 조건은 체크할 필요가 없네요.
+    //// => 아아 그르네요 아래 row여도 true일거니깐
+    //// => 앞으로 요런 디테일도 여유되는대로 좀 더 신경써봐야겠어요. 이런 부류의 피드백을 꽤 받은듯
+    //// => 세심한 피드백 고마워요!
+    boolean isInLargerSquare = (col >= prevSize && col < prevSize * 4 && row < prevSize)
+      || (col >= prevSize * 2 && col < prevSize * 3 && row < prevSize * 2);
+    
+    if (isInLargerSquare) return true;
+    if (level == 1) return false;
+
+    //// 사소하지만 첫번째 호출문에서는 level - 1 로 작성하시고 두번째, 세번째 호출문에서는 level -1 이렇게 작성하셨네요. (불-편 ㅋㅋ)
+    //// => 개-운
+    boolean isInPrevRepeat = isCrystal(level - 1, row - prevSize, col - prevSize) ||
+      isCrystal(level - 1, row - prevSize * 2, col - prevSize * 2) ||
+      isCrystal(level - 1, row - prevSize, col - prevSize * 3);
+
+    return isInPrevRepeat;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+  
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
