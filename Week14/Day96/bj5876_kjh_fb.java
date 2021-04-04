9d467f1dfd85f5119799edf8c6befe126d26730e
김진홍 <deepred.kim@gmail.com>
2021-04-04 23:18:33 +0900

    98일차: n-2일차 피드백 반영

---
+++ Week14/Day96/bj5876_kjh_fb.java
@@ -0,0 +1,151 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//// 깔끔합니다.
+class Main {
+  public static void main(String[] args) throws Exception {
+    final int SHOES_BOARD_SIZE = Input.nextInt();
+    ShoesBoard shoesBoard = new ShoesBoard(SHOES_BOARD_SIZE);
+
+    for (int i = 0; i < SHOES_BOARD_SIZE; i++) {
+      String shoesRow = Input.nextLine();
+      shoesBoard.addShoesInRow(i, shoesRow);
+    }
+
+    System.out.print(shoesBoard.getMaxBalanced());
+  }
+}
+
+class ShoesBoard {
+  private final boolean OPEN = true;
+  private final boolean CLOSE = false;
+
+  //// isOpening, isClosing과 함께 할 때에 뜻을 알아보긴 하지만 shoes 자체 변수명을 보고 무엇을 저장하는지 한번에 알기 어려웠어요.
+  //// open을 기준으로 true값을 저장하니 단순히 shoes보다는 openShoes, isLeftShoes 등으로 정의하면 알아보기 더 편할 것 같습니다.
+  //// => 위의 상수로 커버하려했던 의도였는데
+  //// => isOpening, isClosing 함수를 봐야만 알 수 있어서 가독성이 떨어졌네요
+  //// => 좋은 의견 고마워요!
+  private boolean[][] isLeftShoes;
+  private int size;
+
+  public ShoesBoard(int size) {
+    isLeftShoes = new boolean[size][size];
+    this.size = size;
+  }
+
+  public void addShoesInRow(int row, String shoesRow) {
+    for (int i = 0; i < shoesRow.length(); i++) {
+      isLeftShoes[row][i] = shoesRow.charAt(i) == '(';
+    }
+  }
+
+  public int getMaxBalanced() {
+    if (isClosing(isLeftShoes[0][0])) return 0;
+    return getMaxBalanced(0, 0, new boolean[size][size], 0, 0);
+  }
+
+  private int getMaxBalanced(int row, int col, boolean[][] visited, int opening, int closing) {
+    if (outOfShoesBoard(row, col)) return 0;
+    if (visited[row][col]) return 0;
+    if (closing > 0 && isOpening(isLeftShoes[row][col])) return 0;
+
+    visited[row][col] = true; 
+    opening += isOpening(isLeftShoes[row][col]) ? 1 : 0;
+    closing += isClosing(isLeftShoes[row][col]) ? 1 : 0;
+    
+    if (opening == closing) {
+      visited[row][col] = false;
+      return opening + closing;
+    }
+    
+    int maxBalanced = getMaxBalanced(row + 1, col, visited, opening, closing);
+    maxBalanced = Math.max(maxBalanced, getMaxBalanced(row - 1, col, visited, opening, closing));
+    maxBalanced = Math.max(maxBalanced, getMaxBalanced(row, col + 1, visited, opening, closing));
+    maxBalanced = Math.max(maxBalanced, getMaxBalanced(row, col - 1, visited, opening, closing));
+
+    visited[row][col] = false;
+    return maxBalanced;
+  }
+  
+  private boolean outOfShoesBoard(int row, int col) {
+    return row < 0 || col < 0 || row >= size || col >= size;
+  }
+
+  private boolean isOpening(boolean shoe) {
+    return shoe == OPEN;
+  }
+
+  private boolean isClosing(boolean shoe) {
+    return shoe == CLOSE;
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+  
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
--- Week14/README.md
+++ Week14/README.md
@@ -38,7 +38,7 @@
 
 | 사이트 | 번호 | 문제                 | 답안                | 피드백 반영 답안    |
 | ------ | ---- | -------------------- | ------------------- | ------------------- |
-| 백준   | 5876 | [Horseshoes](https://www.acmicpc.net/problem/5876) | [진홍](Day96/bj5876_kjh.java) [승빈](Day96/bj5876_wsb.java) | 진홍 [승빈](Day96/bj5876_wsb_fb.java) |
+| 백준   | 5876 | [Horseshoes](https://www.acmicpc.net/problem/5876) | [진홍](Day96/bj5876_kjh.java) [승빈](Day96/bj5876_wsb.java) | [진홍](Day96/bj5876_kjh_fb.java) [승빈](Day96/bj5876_wsb_fb.java) |
 
 ## [97일차](Day97) : Pairing Day
 
