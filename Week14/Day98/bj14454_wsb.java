b6e13851ee8085ed152643825e4726549bd72ac1
Seungbeen Wee <wsb0722@naver.com>
2021-04-05 04:53:17 +0900

    Solve Day98

---
+++ Week14/Day98/bj14454_wsb.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static float baseLen;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        String[] input = br.readLine().split(" ");
+        String baseCode = input[0];
+        baseLen = baseCode.length();
+        long wantNth = Long.parseLong(input[1]);
+
+        System.out.println(baseCode.charAt(nthLetterInCode(wantNth) - 1));
+        br.close();
+    }
+
+    static int nthLetterInCode(long nthLetter) {
+        if (nthLetter <= baseLen) return (int) nthLetter;
+        
+        long nthRotated = nthRotated(nthLetter);
+        if (nthLetter == nthRotated) return nthLetterInCode(nthLetter - 1);
+        else return nthLetterInCode(nthLetter - nthRotated);
+    }
+
+    static long nthRotated(long nthLetter) {
+        return (long) (Math.pow(2, repetition(nthLetter) - 1) * baseLen) + 1;
+    }
+
+    static int repetition(long nthLetter) {
+        return (int) Math.ceil(Math.log(nthLetter / baseLen) / Math.log(2));
+    }
+}
