ed04eafa04a59b4055e53404500190d8909fcff6
Deepred <deepred.kim@gmail.com>
2021-04-05 10:27:33 +0900

    99일차: n-1일차 피드백

--- Week14/Day98/bj14454_wsb.java
+++ Week14/Day98/bj14454_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// 전체적으로 코드가 깔끔해서 좋았습니다
 class Main {
     static float baseLen;
 
@@ -19,16 +20,19 @@ class Main {
 
     static int nthLetterInCode(long nthLetter) {
         if (nthLetter <= baseLen) return (int) nthLetter;
-        
+    
         long nthRotated = nthRotated(nthLetter);
         if (nthLetter == nthRotated) return nthLetterInCode(nthLetter - 1);
         return nthLetterInCode(nthLetter - nthRotated);
     }
 
+    //// 1조번째의 글자(nthLetter)를 1조번 돌린 것?(nthRotated)
+    //// 어떤걸 의도하는 내용인지 알기 어려워요 ㅠㅠ
     static long nthRotated(long nthLetter) {
-        return (long) (Math.pow(2, repetition(nthLetter) - 1) * baseLen) + 1;
+        return (long) (Math.pow(2, repetition(nthLetter) - 1) * baseLen) + 1; //// 누구보다 수학을 사랑하시는 승빈씨..
     }
 
+    //// nthLetter를 어떻게 반복하는거고 왜 int로 반환되는지 이름만으로 알기 어려운 것 같아요
     static int repetition(long nthLetter) {
         return (int) Math.ceil(Math.log(nthLetter / baseLen) / Math.log(2));
     }
