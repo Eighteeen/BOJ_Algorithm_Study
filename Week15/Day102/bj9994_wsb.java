2c0726bde3ccea54c9c9d36ed275f59a328e5512
Deepred <deepred.kim@gmail.com>
2021-04-09 11:57:58 +0900

    103일차: n-1일차 피드백

--- Week15/Day102/bj9994_wsb.java
+++ Week15/Day102/bj9994_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// for문으로 반복되는 부분을 처리해줘서 깔끔하네요!
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,6 +20,7 @@ class Main {
         int overlapLen = (len - 1) /2;
         int sumProduced = 0;
 
+        //// for문 와우..
         for (int i = 0; i < len; i += overlapLen + 1) {
             String overlapStr = encryptedStr.substring(i, i + overlapLen);
             String beforeEncryptedStr;
