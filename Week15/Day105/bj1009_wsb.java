fcc548e3a9928cb9dc48b33d1d70f28feb887bf1
Deepred <deepred.kim@gmail.com>
2021-04-12 10:19:06 +0900

    106일차: n-1일차 피드백

--- Week15/Day105/bj1009_wsb.java
+++ Week15/Day105/bj1009_wsb.java
@@ -4,6 +4,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+//// 알고리즘이 효율적으로 되게 잘 짜여진 것 같아요!
+//// 연산결과를 저장하는게 꽤 시간단축을 이룬 것 같고
+//// 밑이 아무리 커도 결국엔 2~8의 밑으로 변환된다는 점을 이용한점이 인상깊었습니다
+
+//// 근데 개인적으로 좀 읽기 어려웠어요 ㅠㅠ
+//// List는 왜 쓰는건지, 왜 size 8 배열을 만드는지, [base-2]로 접근하는 이유는 뭔지, 오버로딩된 함수(List<>, int)가 하는 역할이 원 함수와 조금 다르다는 사실을 알기까지 꽤 걸린 것 같습니다..
 class Main {
     static List<Integer>[] lastProcessListArr;
 
@@ -42,6 +48,7 @@ class Main {
         return lastProcessComputer(lastProcessList, exponent);
     }
 
+    //// getStoredLastProcess는 어때요
     static int lastProcessComputer(List<Integer> lastProcessList, int processLoop) {
         int loopSize = lastProcessList.size();
         processLoop %= loopSize;
