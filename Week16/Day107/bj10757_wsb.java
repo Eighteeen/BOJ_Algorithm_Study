ad9968636e3bf2928b51d87d9f1e936b87690caa
Seungbeen Wee <wsb0722@naver.com>
2021-04-14 00:03:01 +0900

    Solve Day107

---
+++ Week16/Day107/bj10757_wsb.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String numInfo[] = br.readLine().split(" ");
+        BigInteger A = new BigInteger(numInfo[0]);
+        BigInteger B = new BigInteger(numInfo[1]);
+        System.out.print(A.add(B));
+
+        br.close();
+    }
+}
