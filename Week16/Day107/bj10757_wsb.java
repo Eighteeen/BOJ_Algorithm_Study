69f892fe021ef5a8bc7361661af7a10d7f5c7242
Deepred <deepred.kim@gmail.com>
2021-04-14 09:08:16 +0900

    108일차: n-1일차 피드백

--- Week16/Day107/bj10757_wsb.java
+++ Week16/Day107/bj10757_wsb.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 
+//// kjh wsb 틀린그림찾기
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
