589aec7ee29670739a4a1d604c921c9e55b5fb0a
Seungbeen Wee <wsb0722@naver.com>
2021-04-15 17:59:51 +0900

    Feedback Day108

--- Week16/Day108/bj3046_kjh.java
+++ Week16/Day108/bj3046_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// clean
 class Main {
   public static void main(String[] args) throws Exception {
     final int R1 = Input.nextInt();
