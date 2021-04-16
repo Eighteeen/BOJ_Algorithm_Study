d01bedb173ca9a364f85f7dd0d1ea50429a35fa5
Seungbeen Wee <wsb0722@naver.com>
2021-04-16 18:05:51 +0900

    Solve Day110

---
+++ Week16/Day110/bj13866_wsb.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numArr);
+
+        int team1 = numArr[0] + numArr[3];
+        int team2 = numArr[1] + numArr[2];
+
+        System.out.println(Math.abs(team1 - team2));
+        br.close();
+    }
+}
