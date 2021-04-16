c3e746461e7908f05beac91f21912c31fb7b8b8c
Seungbeen Wee <wsb0722@naver.com>
2021-04-16 17:51:10 +0900

    Feedback Day109

--- Week16/Day109/bj18108_kjh.java
+++ Week16/Day109/bj18108_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// クリーン
 class Main {
   public static void main(String[] args) throws Exception {
     final int THAI_YEAR = Input.nextInt();
