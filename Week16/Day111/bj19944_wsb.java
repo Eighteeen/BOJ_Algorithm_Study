ff8a69eb345967ea06cbfb36b49cab45220a0852
Seungbeen Wee <wsb0722@naver.com>
2021-04-17 22:09:56 +0900

    Solve Day111

---
+++ Week16/Day111/bj19944_wsb.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] gradeInfo = br.readLine().split(" ");
+        int N = Integer.parseInt(gradeInfo[0]);
+        int M = Integer.parseInt(gradeInfo[1]);
+
+        String output = "NEWBIE!";
+        if (M > N) output = "TLE!";
+        else if (M > 2) output = "OLDBIE!";
+
+        System.out.println(output);
+        br.close();
+    }
+}
