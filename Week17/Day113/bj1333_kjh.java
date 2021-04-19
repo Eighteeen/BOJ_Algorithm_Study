6e678976486fe33ca59c00a92d6ed35b1ed39166
Deepred <deepred.kim@gmail.com>
2021-04-19 09:52:10 +0900

    113일차: 오늘의 문제

---
+++ Week17/Day113/bj1333_kjh.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    int numberOfSongs = Input.nextInt();
+    final int SONG_DURATION = Input.nextInt();
+    final int BELL_CYCLE = Input.nextInt();
+
+    int seconds = 0;
+    while (numberOfSongs --> 0) {
+      seconds += SONG_DURATION;
+      for (int i = 0; i < 5; i++) {
+        if (seconds % BELL_CYCLE == 0) {
+          System.out.print(seconds);
+          return;
+        }
+        seconds++;
+      }
+    }
+
+    while (true) {
+      if (seconds % BELL_CYCLE == 0) break;
+      seconds++;
+    }
+    
+    System.out.print(seconds);
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      return br.readLine();
+    } catch(Exception e) { }
+    
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+  
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+  
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (tokenizer.hasMoreTokens() == false) {
+      tokenizer = makeTokens();
+    }
+  }
+  
+  private static StringTokenizer makeTokens() {
+    return new StringTokenizer(nextLine(), " ");
+  }
+}
