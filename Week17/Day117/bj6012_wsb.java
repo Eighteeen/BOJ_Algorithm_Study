6e9068ea2943ddd393232af430d4c7bffd068a12
김진홍 <deepred.kim@gmail.com>
2021-04-24 23:38:13 +0900

    Day118

--- Week17/Day117/bj6012_wsb.java
+++ Week17/Day117/bj6012_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// 깔끔하다 그죠?
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
