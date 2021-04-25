eab7007309c423f8e2e9e377ba14b81b54e2a39f
Seungbeen Wee <wsb0722@naver.com>
2021-04-26 03:30:31 +0900

    Solve Day119

---
+++ Week17/Day119/bj1094_wsb.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        System.out.println(Integer.bitCount(X));
+        
+        br.close();
+    }
+}
