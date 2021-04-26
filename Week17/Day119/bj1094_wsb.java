88b079b978a0907117d95fbc4d87a8b7ae22a907
Deepred <deepred.kim@gmail.com>
2021-04-26 09:07:45 +0900

    120일차: n-1일차 피드백

--- Week17/Day119/bj1094_wsb.java
+++ Week17/Day119/bj1094_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// 아 하긴 라이브러리 함수 있을만한 기능이네요
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
