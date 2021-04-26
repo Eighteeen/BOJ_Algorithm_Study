d3b167a3a2c5bc75f95bfaee87f7e81829f3b94e
Seungbeen Wee <wsb0722@naver.com>
2021-04-27 02:56:26 +0900

    Feedback Day119

--- Week17/Day119/bj1094_kjh.java
+++ Week17/Day119/bj1094_kjh.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// 깔끔하고 착실하게 구현하신 것 같습니다.
 class Main {
   public static void main(String[] args) throws Exception {
     final int X = Input.nextInt();
