8e2f125df22606371672e570924274ff5dd19d8b
Seungbeen Wee <wsb0722@naver.com>
2021-05-01 00:18:07 +0900

    Solve Day124

---
+++ Week18/Day124/bj13985_wsb.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] quizInfo = br.readLine().split(" ");
+        int a = Integer.parseInt(quizInfo[0]);
+        int b = Integer.parseInt(quizInfo[2]);
+        int c = Integer.parseInt(quizInfo[4]);
+
+        System.out.println((a + b == c) ? "YES" : "NO");
+        br.close();
+    }
+}
