ed7929a37fa444d7237d5bacc2b9e799799bf0e1
Seungbeen Wee <wsb0722@naver.com>
2021-05-02 02:15:47 +0900

    Solve Day125

---
+++ Week18/Day125/bj1271_wsb.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] divideInfo = br.readLine().split(" ");
+        BigInteger n = new BigInteger(divideInfo[0]), m = new BigInteger(divideInfo[1]);
+        sb.append(n.divide(m)).append("\n");
+        sb.append(n.mod(m)).append("\n");
+
+        System.out.print(sb);
+        br.close();
+    }
+}
