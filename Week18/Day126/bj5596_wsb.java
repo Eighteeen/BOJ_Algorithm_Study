fdf889e0f61ac44556d1901ffb0e761fb71889a7
Seungbeen Wee <wsb0722@naver.com>
2021-05-02 19:01:26 +0900

    Solve Day126

---
+++ Week18/Day126/bj5596_wsb.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] scoresOfStudent1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] scoresOfStudent2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sumSocre1 = IntStream.of(scoresOfStudent1).sum();
+        int sumSocre2 = IntStream.of(scoresOfStudent2).sum();
+        
+        System.out.println(sumSocre1 >= sumSocre2 ? sumSocre1 : sumSocre2);
+        br.close();
+    }
+}
