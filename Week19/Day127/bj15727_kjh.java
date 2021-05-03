d7f4c2c983d209e5aa588caa0116c7eff280bfe0
Deepred <deepred.kim@gmail.com>
2021-05-03 09:56:14 +0900

    127일차: 오늘의 문제

---
+++ Week19/Day127/bj15727_kjh.java
@@ -0,0 +1,82 @@
+  import java.io.BufferedReader;
+  import java.io.InputStreamReader;
+  import java.util.StringTokenizer;
+
+  class Main {
+    public static void main(String[] args) throws Exception {
+      final int DISTANCE = Input.nextInt();
+      int minTime = DISTANCE / 5;
+      minTime += (DISTANCE % 5 > 0) ? 1 : 0;
+
+      System.out.print(minTime);
+    }
+  }
+
+  class Input {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer tokenizer;
+
+    public static String nextLine() {
+      try {
+        return br.readLine();
+      } catch(Exception e) { }
+      
+      return null;
+    }
+
+    public static int nextInt() {
+      String nextString = next();
+      return Integer.parseInt(nextString);
+    }
+
+    public static double nextDouble() {
+      String nextString = next();
+      return Double.parseDouble(nextString);
+    }
+
+    public static long nextLong() {
+      String nextString = next();
+      return Long.parseLong(nextString);
+    }
+
+    public static char nextChar() {
+      String nextString = next();
+      return nextString.charAt(0);
+    }
+    
+    public static String next() {
+      makeTokensIfNeed();
+      return tokenizer.nextToken();
+    }
+
+    public static void skipLine() {
+      nextLine();
+    }
+
+    public static void skipLine(int n) {
+      for (int i = 0; i < n; i++) {
+        nextLine();
+      }
+    }
+    
+    private static void makeTokensIfNeed() {
+      makeTokensIfNotReadedYet();
+      makeTokensIfHasNoToken();
+    }
+
+    private static void makeTokensIfNotReadedYet() {
+      if (tokenizer == null) {
+        tokenizer = makeTokens();
+      }
+    }
+
+    private static void makeTokensIfHasNoToken() {
+      if (tokenizer.hasMoreTokens() == false) {
+        tokenizer = makeTokens();
+      }
+    }
+    
+    private static StringTokenizer makeTokens() {
+      return new StringTokenizer(nextLine(), " ");
+    }
+  }
