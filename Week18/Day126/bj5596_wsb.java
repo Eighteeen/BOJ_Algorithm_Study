7b93d5438e446d9aedd40c5b76057061980fd76e
Deepred <deepred.kim@gmail.com>
2021-05-03 09:50:48 +0900

    127일차: n-1일차 피드백

--- Week18/Day126/bj5596_wsb.java
+++ Week18/Day126/bj5596_wsb.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+//// Clean
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,6 +12,7 @@ class Main {
         int[] scoresOfStudent1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] scoresOfStudent2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        //// Socrates
         int sumSocre1 = IntStream.of(scoresOfStudent1).sum();
         int sumSocre2 = IntStream.of(scoresOfStudent2).sum();
         
