04e0012c3246426e9ce3f94eead6afb8ae45b569
Seungbeen Wee <wsb0722@naver.com>
2021-05-05 23:36:34 +0900

    Solve Day129

---
+++ Week19/Day129/bj14929_wsb.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] numArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int prefixSum = 0;
+        long resultSum = 0;
+        for (int num : numArr) {
+            resultSum += num * prefixSum;
+            prefixSum += num;
+        }
+
+        System.out.println(resultSum);
+        br.close();
+    }
+}
