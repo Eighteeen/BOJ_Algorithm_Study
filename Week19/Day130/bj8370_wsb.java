064e9fdae521df24597a36e10658684df946874f
Deepred <deepred.kim@gmail.com>
2021-05-07 09:07:35 +0900

    131일차: 어제 피드백

--- Week19/Day130/bj8370_wsb.java
+++ Week19/Day130/bj8370_wsb.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+//// 굿
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
