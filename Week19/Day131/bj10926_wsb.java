d23377b7c4e70435aca6a835e10cfc8042338c4e
김진홍 <deepred.kim@gmail.com>
2021-05-08 22:50:41 +0900

    132일차: 어제 피드백

--- Week19/Day131/bj10926_wsb.java
+++ Week19/Day131/bj10926_wsb.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// ㅏ ㅡ
+//// ㄹ ㅁ
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
