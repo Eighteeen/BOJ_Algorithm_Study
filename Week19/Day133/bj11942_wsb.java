49d37ce9da71b3eaa33d01671a911812d56e19c2
Seungbeen Wee <wsb0722@naver.com>
2021-05-09 23:56:55 +0900

    Solve Day133

---
+++ Week19/Day133/bj11942_wsb.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("고려대학교");
+
+        br.close();
+    }
+}
