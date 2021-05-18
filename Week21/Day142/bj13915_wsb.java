0e12e378584d62ce3078bf46c515fda9f2bd5544
Seungbeen Wee <wsb0722@naver.com>
2021-05-19 00:05:19 +0900

    Solve Day142

---
+++ Week21/Day142/bj13915_wsb.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input;
+        while ((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input);
+            int[] levels = new int[N];
+            for (int i = 0; i < N; i++) {
+                int[] hotAirBalloons = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+                levels[i] = bitmaskOfNumArrAllAdd(hotAirBalloons);
+            }
+            sb.append(Arrays.stream(levels).distinct().count()).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    static int bitmaskOfNumArrAllAdd(int[] numArr) {
+        int bitmask = 0;
+        for (int num : numArr) {
+            bitmask |= (1 << num);
+        }
+        return bitmask;
+    }
+}
