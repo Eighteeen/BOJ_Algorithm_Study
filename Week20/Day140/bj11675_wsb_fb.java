0d48f7623671c4c7bc91ea4536dfa49073e64c9f
Seungbeen Wee <wsb0722@naver.com>
2021-05-18 23:32:12 +0900

    Reflect Feedback Day140

---
+++ Week20/Day140/bj11675_wsb_fb.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] byteMessages = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int byteMessage : byteMessages) {
+            sb.append(unscrambleByteMessage(byteMessage)).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    //// 형용사 형태(복호화된 바이트 메시지)보다는 동사 형태(바이트 메시지를 복호화한다)가 더 좋을 것 같습니다!
+    //// -> 반영했습니다.
+    static int unscrambleByteMessage(int byteMessage) {
+        int unscrambledNum = 0;
+
+        while (scrambleByteMessage(unscrambledNum) != byteMessage) {
+            unscrambledNum++;
+        }
+
+        return unscrambledNum;
+    }
+
+    static int scrambleByteMessage(int num) {
+        int shiftNum = num << 1;
+        if (shiftNum >= 256) shiftNum -= 256;
+        return num ^ shiftNum;
+    }
+}
