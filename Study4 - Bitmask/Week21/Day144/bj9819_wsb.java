4816cb2e4e791a8a851dd5595ff333d8d68ed8ce
SeungBeen Wee <wsb0722@naver.com>
2021-05-23 18:45:44 +0900

    Solve Day144

---
+++ Study4 - Bitmask/Week21/Day144/bj9819_wsb.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("-1")) break;
+
+            String[] gamePieceInfo = input.split(",");
+            int n = Integer.parseInt(gamePieceInfo[0]);
+            int m = Integer.parseInt(gamePieceInfo[1]);
+            int k = Integer.parseInt(gamePieceInfo[2]);
+            
+            sb.append(input + ": ")
+            	.append(numOfWhitePieceAfterPlace(n, m, k))
+            	.append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+    
+    static int numOfWhitePieceAfterPlace(int totalPiece, int whitePiece, int placeLoop) {
+    	BigInteger bitmask = BigInteger.ZERO;
+    	for (int i = 0; i < whitePiece; i++) {
+    		bitmask = bitmask.or(BigInteger.ONE.shiftLeft(i));
+    	}
+    	
+    	for (int i = 0; i < placeLoop; i++) {
+    		BigInteger lastPiece = bitmask.and(BigInteger.ONE).shiftLeft(totalPiece - 1);
+    		bitmask = bitmask.xor(bitmask.shiftRight(1).or(lastPiece));
+    	}
+    	
+    	return bitmask.bitCount();
+    }
+}
