4339e6c7e3b452a85379d4a8a4637e77d828e278
Seungbeen Wee <wsb0722@naver.com>
2021-05-24 04:31:43 +0900

    Solve Day148

---
+++ Study4 - Bitmask/Week22/Day148/bj3009_wsb.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        List<Integer> xPossible = new ArrayList<>();
+        List<Integer> yPossible = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            String[] numInfo = br.readLine().split(" ");
+            Integer x = Integer.valueOf(numInfo[0]);
+            Integer y = Integer.valueOf(numInfo[1]);
+
+            if (xPossible.contains(x)) xPossible.remove(x);
+            else xPossible.add(x);
+            if (yPossible.contains(y)) yPossible.remove(y);
+            else yPossible.add(y);
+        }
+
+        sb.append(xPossible.get(0)).append(" ").append(yPossible.get(0));
+        System.out.println(sb);
+        br.close();
+    }
+}
