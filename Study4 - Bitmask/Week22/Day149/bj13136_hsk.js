23580c4ef7b986471a7668643d10f11ca5254326
Seungbeen Wee <wsb0722@naver.com>
2021-05-26 01:43:33 +0900

    Feedback Day149

--- Study4 - Bitmask/Week22/Day149/bj13136_hsk.js
+++ Study4 - Bitmask/Week22/Day149/bj13136_hsk.js
@@ -3,4 +3,5 @@ const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toSt
 
 const [row, col, N] = input.map((value) => Number(value));
 
+//// 오호 Number의 특징을 잘 활용하신 거 같아요. 깔끔!
 console.log(Math.ceil(row / N) * Math.ceil(col / N));
