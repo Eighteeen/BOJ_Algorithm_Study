7b1907c025570563801b222624819c3e5506c42c
Seungbeen Wee <wsb0722@naver.com>
2021-05-26 01:30:25 +0900

    Solve Day149

---
+++ Study4 - Bitmask/Week22/Day149/bj13136_wsb.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] competitionPlaceInfo = br.readLine().split(" ");
+        int R = Integer.parseInt(competitionPlaceInfo[0]);
+        int C = Integer.parseInt(competitionPlaceInfo[1]);
+        int N = Integer.parseInt(competitionPlaceInfo[2]);
+
+        int verticalCCTV = R / N;
+        if (R % N != 0) verticalCCTV++;
+
+        int horizontalCCTV = C / N;
+        if (C % N != 0) horizontalCCTV++;
+
+        System.out.println((long) verticalCCTV * horizontalCCTV);
+        br.close();
+    }
+}
