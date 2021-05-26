fb507f62f50e599c409918783858629c233724c3
sukyeongh <hsk10271@gmail.com>
2021-05-26 09:41:42 +0900

    Day150 : Day150 문제 (사분면 고르기)

---
+++ Study4 - Bitmask/Week22/Day150/bj14681_hsk.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const x = parseInt(input[0]);
+  const y = parseInt(input[1]);
+
+  let quaerant = 1;
+
+  if (x < 0) {
+    if (y > 0) quaerant = 2;
+    else quaerant = 3;
+  } else {
+    if (y < 0) quaerant = 4;
+  }
+
+  console.log(quaerant);
+
+  process.exit();
+});
