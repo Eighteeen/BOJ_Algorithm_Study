b2c01049d9e820a31f2d90ca0fa433fdefeafa56
Deepred <deepred.kim@gmail.com>
2021-05-27 11:03:25 +0900

    Update bj20546_kjh.java

--- Study4 - Bitmask/Week22/Day151/bj20546_kjh.java
+++ Study4 - Bitmask/Week22/Day151/bj20546_kjh.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) throws Exception {
