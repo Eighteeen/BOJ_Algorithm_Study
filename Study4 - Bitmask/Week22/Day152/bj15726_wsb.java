0da9a1b488bd9e69c592fbf8a20162053a68f54a
Seungbeen Wee <wsb0722@naver.com>
2021-05-28 23:16:15 +0900

    Solve Day152

---
+++ Study4 - Bitmask/Week22/Day152/bj15726_wsb.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numStrArr = br.readLine().split(" ");
+        double A = Double.parseDouble(numStrArr[0]), B = Double.parseDouble(numStrArr[1]), C = Double.parseDouble(numStrArr[2]);
+
+        System.out.println((int) Math.max((A / B * C), (A * B / C)));                                                                                                                                                                                                                                                                                                                                             
+
+        br.close();
+    }
+}
