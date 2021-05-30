7093668f12f3cc3fa9e61a10f582f33f2f398e73
sukyeongh <hsk10271@gmail.com>
2021-05-31 00:33:40 +0900

    Day155 : Day153 피드백 반영

---
+++ Study4 - Bitmask/Week22/Day153/bj1712_hsk_fb.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `3 2 1`).split(' ');
+
+const fixed_cost = parseInt(input[0]);
+const variable_cost = parseInt(input[1]);
+const laptop_cost = parseInt(input[2]);
+
+//// 무난쓰합니다
+//// laptop_cost <= variable_cost 일때는 break_even_point를 계산할 필요가 없으니 else 부분에 계산 부분을 넣었으면 더 깔끔할 것 같습니다. : 22
+//// -> 반영했습니다.
+if (laptop_cost <= variable_cost) console.log('-1');
+else {
+  let break_even_point = parseInt(fixed_cost / (laptop_cost - variable_cost) + 1);
+  console.log(break_even_point);
+}
