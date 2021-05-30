c45ae577dfb8d696bb3ec10b69853f43b195b3a3
Seungbeen Wee <wsb0722@naver.com>
2021-05-30 16:22:17 +0900

    Feedback Day153

--- Study4 - Bitmask/Week22/Day153/bj1712_hsk.js
+++ Study4 - Bitmask/Week22/Day153/bj1712_hsk.js
@@ -5,6 +5,7 @@ const fixed_cost = parseInt(input[0]);
 const variable_cost = parseInt(input[1]);
 const laptop_cost = parseInt(input[2]);
 
+//// laptop_cost <= variable_cost 일때는 break_even_point를 계산할 필요가 없으니 else 부분에 계산 부분을 넣었으면 더 깔끔할 것 같습니다.
 let break_even_point = parseInt(fixed_cost / (laptop_cost - variable_cost) + 1);
 if (laptop_cost <= variable_cost) console.log('-1');
 else console.log(break_even_point);
