07f82b2a3e053c254e412c509200458bc020735b
김진홍 <deepred.kim@gmail.com>
2021-06-01 08:23:59 +0900

    156일차: 어제 피드백

--- Study4 - Bitmask/Week23/Day155/bj5532_hsk.js
+++ Study4 - Bitmask/Week23/Day155/bj5532_hsk.js
@@ -11,6 +11,8 @@ const input = (
 
 const [L, A, B, C, D] = input.map((value) => Number(value));
 
+//// js는 나눠떨어지지 않으면 소수점이 붙어서 나오니까
+//// 고걸 이용해서 소수점 첫자리에서 올림했으면 더 깔끔했을 것 같아요!
 const solve_korean = parseInt(A / C) + (A % C !== 0 ? 1 : 0);
 const solve_math = parseInt(B / D) + (B % D !== 0 ? 1 : 0);
 
