101e36f0193b42552866afbc36af02a439d976e9
Seungbeen Wee <wsb0722@naver.com>
2021-05-31 22:20:06 +0900

    Solve Day155

---
+++ Study4 - Bitmask/Week23/Day155/bj5532_wsb.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vacationDays = Integer.parseInt(br.readLine());
+        int mathTotalPage = Integer.parseInt(br.readLine());
+        int languageTotalPage = Integer.parseInt(br.readLine());
+        int mathPagePerDay = Integer.parseInt(br.readLine());
+        int languagePagePerDay = Integer.parseInt(br.readLine());
+
+        int mathTakeDays = getTakeDaysForTask(mathTotalPage, mathPagePerDay);
+        int languageTakeDays = getTakeDaysForTask(languageTotalPage, languagePagePerDay);
+
+        System.out.println(vacationDays - Math.max(mathTakeDays, languageTakeDays));
+        br.close();
+    }
+
+    static int getTakeDaysForTask(int totalTask, int possibleTaskPerDay) {
+        int takeDays = totalTask / possibleTaskPerDay;
+        if (totalTask % possibleTaskPerDay != 0) takeDays++;
+        return takeDays;
+    }
+}
