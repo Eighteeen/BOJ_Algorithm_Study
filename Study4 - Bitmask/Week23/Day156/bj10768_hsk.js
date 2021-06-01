86fb23443e57ba52ffafb79f830ee86667358cb8
sukyeongh <hsk10271@gmail.com>
2021-06-01 15:08:56 +0900

    Day156 : Day156(특별한 날) 풀이

---
+++ Study4 - Bitmask/Week23/Day156/bj10768_hsk.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+18`
+).split('\n');
+
+const month = parseInt(input[0]);
+const day = parseInt(input[1]);
+
+if (month === 1 || (month === 2 && day < 18)) {
+  console.log('Before');
+} else if (month === 2 && day === 18) {
+  console.log('Special');
+} else console.log('After');
