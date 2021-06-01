674951a0ac24f5917f24aa1110322741703af5b5
sukyeongh <hsk10271@gmail.com>
2021-06-01 14:59:14 +0900

    Day156 : Day155 피드백

--- Study4 - Bitmask/Week23/Day155/bj5532_kjh.java
+++ Study4 - Bitmask/Week23/Day155/bj5532_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+////깔끔해요
 class Main {
   public static void main(String[] args) throws Exception {
     final int VACATION_DAYS = Input.nextInt();
