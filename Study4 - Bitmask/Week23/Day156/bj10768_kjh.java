4714eda5cdc81fb21e22f8aa35189e7cdfc3f7a6
sukyeongh <hsk10271@gmail.com>
2021-06-02 11:50:28 +0900

    Day157 : Day156 피드백

--- Study4 - Bitmask/Week23/Day156/bj10768_kjh.java
+++ Study4 - Bitmask/Week23/Day156/bj10768_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// good~
 class Main {
   public static void main(String[] args) throws Exception {
     int month = Input.nextInt();
