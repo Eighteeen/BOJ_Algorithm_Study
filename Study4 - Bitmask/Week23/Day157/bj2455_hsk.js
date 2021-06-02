fa0d0ecf2ddf04ce9bf6743c0ffc2cb699d015d1
sukyeongh <hsk10271@gmail.com>
2021-06-02 11:50:56 +0900

    Day157 : Day157(지능형 기차) 풀이

---
+++ Study4 - Bitmask/Week23/Day157/bj2455_hsk.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `0 32
+3 13
+28 25
+39 0`
+).split('\n');
+
+let currentPassenger = 0;
+let maxPassenger = 0;
+
+for (let i = 0; i < 4; i++) {
+  const [getOffPassenger, getOnPassenger] = input[i].split(' ').map((item) => parseInt(item));
+  currentPassenger += getOnPassenger;
+  currentPassenger -= getOffPassenger;
+  maxPassenger = Math.max(currentPassenger, maxPassenger);
+}
+console.log(maxPassenger);
