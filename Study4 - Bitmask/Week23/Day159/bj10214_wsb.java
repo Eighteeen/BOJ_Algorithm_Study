8e3ba9f62e36544fe38072bbc60be656cfc62eb7
Seungbeen Wee <wsb0722@naver.com>
2021-06-04 23:59:35 +0900

    Solve Day159

---
+++ Study4 - Bitmask/Week23/Day159/bj10214_wsb.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int YonseiScore = 0, KoreaScore = 0;
+            for (int j = 0; j < 9; j++) {
+                String[] scoreStringArr = br.readLine().split(" ");
+                YonseiScore += Integer.parseInt(scoreStringArr[0]);
+                KoreaScore += Integer.parseInt(scoreStringArr[1]);
+            }
+            
+            String result = "Draw";
+            if (YonseiScore > KoreaScore) result = "Yonsei";
+            else if (YonseiScore < KoreaScore) result = "Korea";
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
