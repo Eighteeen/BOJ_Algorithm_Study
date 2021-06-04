ad98e0c9daebf12fb5f8919affbb0cd3eb4750f4
sukyeongh <hsk10271@gmail.com>
2021-06-04 13:19:11 +0900

    Revert "Day159 : Day157 피드백"
    This reverts commit e4160a5fc941000133a7b0a5013b535d7ee8dee4.

--- Study4 - Bitmask/Week23/Day158/bj3058_kjh.java
+++ Study4 - Bitmask/Week23/Day158/bj3058_kjh.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//// 깔끔해요~
 class Main {
   public static void main(String[] args) throws Exception {
     final int TEST_CASE = Input.nextInt();
