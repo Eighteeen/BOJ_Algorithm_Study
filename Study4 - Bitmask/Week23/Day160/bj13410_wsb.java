07bd19780e5ff66288f8b6e92f22597f6231b645
Seungbeen Wee <wsb0722@naver.com>
2021-06-06 00:03:20 +0900

    Solve Day160

---
+++ Study4 - Bitmask/Week23/Day160/bj13410_wsb.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numStrArr = br.readLine().split(" ");
+        int N = Integer.parseInt(numStrArr[0]), K = Integer.parseInt(numStrArr[1]);
+
+        int maxNum = 0;
+        for (int i = 1; i <= K; i++) {
+            int multiplyNum = N * i;
+            maxNum = Math.max(maxNum, getReverseNum(multiplyNum));
+        }
+
+        System.out.println(maxNum);
+        br.close();
+    }
+
+    static int getReverseNum(int num) {
+        String numStr = Integer.toString(num);
+        String reverseNumStr = new StringBuilder(numStr).reverse().toString();
+        return Integer.parseInt(reverseNumStr);
+    }
+}
