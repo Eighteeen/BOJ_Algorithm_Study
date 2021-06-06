159418317c80dbafc59604873ba1d55a85b937d4
sukyeongh <hsk10271@gmail.com>
2021-06-06 20:19:18 +0900

    Day161 : Day159 피드백 재재반영

--- Study4 - Bitmask/Week23/Day159/bj10214_hsk.js
+++ Study4 - Bitmask/Week23/Day159/bj10214_hsk.js
@@ -21,10 +21,9 @@ const input = (() => {
 })();
 
 //// 함수로 뺐다면 team1, team2 등 추상화하면 더 좋을 것 같아요!
-//// ~~> 반영했습니다
-const getGameResult = (team1, team2, team1Name, team2Name) => {
-  if (team1 > team2) return team1Name;
-  else if (team2 > team1) return team2Name;
+const getGameResult = (yonsei, korea) => {
+  if (yonsei > korea) return 'Yonsei';
+  else if (korea > yonsei) return 'Korea';
   else return 'Draw';
 };
 
@@ -40,5 +39,5 @@ for (let i = 0; i < T; i++) {
     yonseiPoint += yonsei;
     koreaPoint += korea;
   }
-  console.log(getGameResult(yonseiPoint, koreaPoint, 'Yonsei', 'Korea'));
+  console.log(getGameResult(yonseiPoint, koreaPoint));
 }
--- Study4 - Bitmask/Week23/Day159/bj10214_hsk_fb.js
+++ Study4 - Bitmask/Week23/Day159/bj10214_hsk_fb.js
@@ -21,10 +21,10 @@ const input = (() => {
 })();
 
 //// 함수로 뺐다면 team1, team2 등 추상화하면 더 좋을 것 같아요!
-//// => 다음부터는 고려해서 문제를 풀어보겠습니다~
-const getGameResult = (yonsei, korea) => {
-  if (yonsei > korea) return 'Yonsei';
-  else if (korea > yonsei) return 'Korea';
+//// ~~> 반영했습니다
+const getGameResult = (team1, team2, team1Name, team2Name) => {
+  if (team1 > team2) return team1Name;
+  else if (team2 > team1) return team2Name;
   else return 'Draw';
 };
 
@@ -40,5 +40,5 @@ for (let i = 0; i < T; i++) {
     yonseiPoint += yonsei;
     koreaPoint += korea;
   }
-  console.log(getGameResult(yonseiPoint, koreaPoint));
+  console.log(getGameResult(yonseiPoint, koreaPoint, 'Yonsei', 'Korea'));
 }
