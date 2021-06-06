93470d48bfdc883dc78380f635ff1448ed04a61d
Seungbeen Wee <wsb0722@naver.com>
2021-06-06 17:46:48 +0900

    Solve Day161

---
+++ Study4 - Bitmask/Week23/Day161/bj15873_wsb.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numStrArr = br.readLine().split("");
+        int arrLen = numStrArr.length;
+
+        int result = 20;
+        int num1, num2;
+        if (arrLen == 3) {
+            num1 = Integer.parseInt(numStrArr[0]);
+            num2 = Integer.parseInt(numStrArr[2]);
+            result = 10;
+            result += (num2 == 0 ? num1 : num2);
+        } else if (arrLen < 3) {
+            num1 = Integer.parseInt(numStrArr[0]);
+            num2 = Integer.parseInt(numStrArr[1]);
+            result = num1 + num2;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
