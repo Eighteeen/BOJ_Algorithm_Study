7ff4800189600dd67f68d3a5d1d774b909de3b00
Seungbeen Wee <wsb0722@naver.com>
2021-06-08 00:46:00 +0900

    Solve Day162

---
+++ Study4 - Bitmask/Week24/Day162/bj2355_wsb.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] numStrArr = br.readLine().split(" ");
+        long A = Long.parseLong(numStrArr[0]);
+        long B = Long.parseLong(numStrArr[1]);
+
+        System.out.println(getAllSumBetweenTwoNums(A, B));
+        br.close();
+    }
+
+    static long getAllSumBetweenTwoNums(long num1, long num2) {
+        return (num1 + num2) * (Math.abs(num1 - num2) + 1) / 2;
+    }
+}
