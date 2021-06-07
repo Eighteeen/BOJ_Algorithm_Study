131ae01ac0f00d378f033e022d9ef4fe19fe8bb1
Seungbeen Wee <wsb0722@naver.com>
2021-06-08 00:24:15 +0900

    Feedback Day161

--- Study4 - Bitmask/Week23/Day161/bj15873_kjh.java
+++ Study4 - Bitmask/Week23/Day161/bj15873_kjh.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//// 무난해요
+//// 무난해요 : 22
 class Main {
   public static void main(String[] args) throws Exception {
     String EXPRESSION = Input.nextLine();
