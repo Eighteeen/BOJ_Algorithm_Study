06dc2f0c3747822055377120d491e36858af81df
Seungbeen Wee <wsb0722@naver.com>
2021-06-10 00:18:01 +0900

    Feedback Day164

--- Study4 - Bitmask/Week24/Day164/bj15963_hsk.js
+++ Study4 - Bitmask/Week24/Day164/bj15963_hsk.js
@@ -3,5 +3,6 @@ let [N, M] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toStr
   .split(' ')
   .map((item) => parseInt(item));
 
+//// js === 으로 왕 깔끔
 if (N === M) console.log(1);
 else console.log(0);
