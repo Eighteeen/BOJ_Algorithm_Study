f1583eae070bf81cb7513c192b3e084c8ac29e32
sukyeongh <hsk10271@gmail.com>
2021-06-10 00:23:34 +0900

    Day165 : Day165(수 뒤집기) 풀이

---
+++ Study4 - Bitmask/Week24/Day165/bj3062_hsk.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+let stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `4
+13
+58
+120
+5056`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+
+const isPalindrome = (num) => {
+  for (let i = 0; i < num.length / 2; i++) {
+    if (num[i] !== num[num.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+for (let i = 0; i < N; i++) {
+  const originNum = input();
+  const reverseNum = originNum.split('').reverse().join('');
+  const sum = parseInt(originNum) + parseInt(reverseNum);
+
+  console.log(isPalindrome(sum.toString().split('')) ? 'YES' : 'NO');
+}
