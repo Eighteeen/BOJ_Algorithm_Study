615a5409a054225110c46cc4bc352b18a064db0b
sukyeongh <hsk10271@gmail.com>
2021-06-10 00:06:08 +0900

    Day165 : Day164 피드백

--- Study4 - Bitmask/Week24/Day164/bj15963_kjh.java
+++ Study4 - Bitmask/Week24/Day164/bj15963_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+////깔끔~
 class Main {
   public static void main(String[] args) throws Exception {
     System.out.print(
