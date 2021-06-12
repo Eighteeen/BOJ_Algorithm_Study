64fe76a901880753e154f8130e37d4f295473d75
sukyeongh <hsk10271@gmail.com>
2021-06-12 17:32:22 +0900

    Day167 : Day167(Covid-19) 풀이

---
+++ Study4 - Bitmask/Week24/Day167/bj20673_hsk.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let [p, q] = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `15
+12`
+)
+  .split('\n')
+  .map((item) => parseInt(item));
+
+if (q <= 10) console.log('White');
+else if (q <= 30) console.log('Yellow');
+else console.log('Red');
