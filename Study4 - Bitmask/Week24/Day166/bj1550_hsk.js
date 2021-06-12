d993a56fed0bc015253e3b4e7711ea7cc3060e0e
김진홍 <deepred.kim@gmail.com>
2021-06-12 23:02:12 +0900

    167일차: 어제 피드백

--- Study4 - Bitmask/Week24/Day166/bj1550_hsk.js
+++ Study4 - Bitmask/Week24/Day166/bj1550_hsk.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const strN = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `A`;
 
-//// 깔끔
+//// 깔끔 :22
 console.log(parseInt(strN, 16));
