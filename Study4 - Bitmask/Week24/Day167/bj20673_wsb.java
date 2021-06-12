91e4334102cbbbf4bfcc5182de4cc1d3936d1aef
Seungbeen Wee <wsb0722@naver.com>
2021-06-12 23:21:56 +0900

    Solve Day167

---
+++ Study4 - Bitmask/Week24/Day167/bj20673_wsb.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int p = Integer.parseInt(br.readLine());
+        int q = Integer.parseInt(br.readLine());
+
+        String result = "Yellow";
+        if (p <= 50 && q <= 10) result = "White";
+        else if (q >= 30) result = "Red";
+
+        System.out.println(result);
+        br.close();
+    }
+}
