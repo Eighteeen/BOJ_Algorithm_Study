d7a486caa8967fcb25741695f6e110df52197b17
Seungbeen Wee <wsb0722@naver.com>
2021-06-13 18:14:37 +0900

    Solve Day168

---
+++ Study4 - Bitmask/Week24/Day168/bj11720_wsb.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char numChr[] = br.readLine().toCharArray();
+        
+        int sum = 0;
+        for(char c : numChr) sum += (c - '0');
+        
+        System.out.println(sum);
+        br.close();
+    }
+}
