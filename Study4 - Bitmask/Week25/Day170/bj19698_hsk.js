5237ef279d6c37dd70d6418883ceb31c49c31ca4
sukyeongh <hsk10271@gmail.com>
2021-06-15 13:37:58 +0900

    Day170 : Day170(헛간 청약) 풀이

---
+++ Study4 - Bitmask/Week25/Day170/bj19698_hsk.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let [N, W, H, L] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `7 17 11 5`)
+  .split(' ')
+  .map((item) => parseInt(item));
+
+const cowCount = parseInt(W / L) * parseInt(H / L);
+const maxCowCount = Math.min(N, cowCount);
+console.log(maxCowCount);
