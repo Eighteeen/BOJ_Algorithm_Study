0542afbb798d64dd170172869fc62827c1800053
Seungbeen Wee <wsb0722@naver.com>
2021-06-15 22:35:15 +0900

    Solve Day170

---
+++ Study4 - Bitmask/Week25/Day170/bj19698_wsb.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] barnInfo = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = barnInfo[0], W = barnInfo[1], H = barnInfo[2], L = barnInfo[3];
+
+        int wCow = W / L, hCow = H / L;
+        int maxCow = wCow * hCow;
+        if (maxCow > N) maxCow = N;
+
+        System.out.println(maxCow);
+        br.close();
+    }
+}
