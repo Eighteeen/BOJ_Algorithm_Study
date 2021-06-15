f41e4b18f97a5f58238c9a6b8052764151455166
sukyeongh <hsk10271@gmail.com>
2021-06-15 13:29:23 +0900

    Day170 : Day169 피드백

--- Study4 - Bitmask/Week25/Day169/bj15733_kjh.java
+++ Study4 - Bitmask/Week25/Day169/bj15733_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//// ㄲㅏㄹㄲㅡㅁ
 class Main {
   public static void main(String[] args) throws Exception {
     System.out.print("I'm Sexy");
--- Study4 - Bitmask/Week25/Day169/bj15733_wsb.java
+++ Study4 - Bitmask/Week25/Day169/bj15733_wsb.java
@@ -1,5 +1,5 @@
 class Main {
-    //// 코드가 섹시하네요
+    //// 코드가 섹시하네요 : 22
     public static void main(String[] args) throws Exception {
         System.out.println("I'm Sexy");
     }
