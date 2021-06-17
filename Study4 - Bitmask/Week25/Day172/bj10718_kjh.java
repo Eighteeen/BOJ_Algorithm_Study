dfc6405895120d000f9ef6d1ec21fcce5dd2fa99
Deepred <deepred.kim@gmail.com>
2021-06-17 10:10:05 +0900

    172일차: 오늘의 문제

---
+++ Study4 - Bitmask/Week25/Day172/bj10718_kjh.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    System.out.print("강한친구 대한육군\n".repeat(2));
+  }
+}
