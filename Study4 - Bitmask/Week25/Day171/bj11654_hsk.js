c700cb84a3ccc001e625e9d9d1119cc32479b13d
Seungbeen Wee <wsb0722@naver.com>
2021-06-17 23:14:59 +0900

    Feedback Day171

--- Study4 - Bitmask/Week25/Day171/bj11654_hsk.js
+++ Study4 - Bitmask/Week25/Day171/bj11654_hsk.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const input = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `9`;
 
-//// 깔
+//// 깔 : 22 끔
 console.log(input.charCodeAt(0))
