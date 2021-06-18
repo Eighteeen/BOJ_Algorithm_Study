75ff849a257fd25e54c38f6d1369f74f8a981d82
sukyeongh <hsk10271@gmail.com>
2021-06-18 13:25:38 +0900

    Day173 : Day172 피드백

--- Study4 - Bitmask/Week25/Day172/bj10718_kjh.java
+++ Study4 - Bitmask/Week25/Day172/bj10718_kjh.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+////깔끔
 class Main {
   public static void main(String[] args) throws Exception {
     System.out.print("강한친구 대한육군\n".repeat(2));
--- Study4 - Bitmask/Week25/Day172/bj10718_wsb.java
+++ Study4 - Bitmask/Week25/Day172/bj10718_wsb.java
@@ -1,5 +1,5 @@
 class Main {
-    //// 끔깔
+    //// 끔깔 : 22
     public static void main(String[] args) {
         System.out.println("강한친구 대한육군\n강한친구 대한육군");
     }
