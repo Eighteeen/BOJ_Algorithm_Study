765c87cccfbd888bf6885512667478eedffb439d
Seungbeen Wee <wsb0722@naver.com>
2021-06-19 03:12:18 +0900

    Solve Day173

---
+++ Study4 - Bitmask/Week25/Day173/bj2754_wsb.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] grades = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"};
+        float[] ratings = {4.3f, 4.0f, 3.7f, 3.3f, 3.0f, 2.7f, 2.3f, 2.0f, 1.7f, 1.3f, 1.0f, 0.7f, 0.0f};
+
+        String ClangGrade = br.readLine();
+
+        System.out.println(ratings[Arrays.asList(grades).indexOf(ClangGrade)]);
+        br.close();
+    }
+}
