f8739d36670597595722270cf5c8980104ec9055
sukyeongh <hsk10271@gmail.com>
2021-06-21 13:20:52 +0900

    Day176 : Day176(무한이진트리) 풀이

---
+++ Study5 - Tree/Week26/Day176/bj2078_hsk.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let [A, B] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `3 4`)
+  .split(' ')
+  .map((item) => parseInt(item));
+
+let leftCnt = 0;
+let rightCnt = 0;
+while (1) {
+  if (A === 1) {
+    rightCnt += B - 1;
+    break;
+  } else if (B === 1) {
+    leftCnt += A - 1;
+    break;
+  }
+  if (A > B) {
+    leftCnt += parseInt(A / B);
+    A %= B;
+    continue;
+  }
+  rightCnt += parseInt(B / A);
+  B %= A;
+}
+console.log(`${leftCnt} ${rightCnt}`);
