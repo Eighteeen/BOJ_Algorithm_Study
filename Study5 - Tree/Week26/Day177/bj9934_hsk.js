d2d3c3b9725e0f9fe0467e8d33f87b32f169e9fd
sukyeongh <hsk10271@gmail.com>
2021-06-22 12:39:57 +0900

    Day177 : Day177(완전 이진 트리) 문제

---
+++ Study5 - Tree/Week26/Day177/bj9934_hsk.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+let stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+2 1 3`
+).split('\n');
+
+const K = parseInt(stdin[0]);
+const buildingsByInOrder = stdin[1].split(' ').map((item) => parseInt(item));
+const buildingsTree = Array.from(Array(K), () => Array().fill());
+
+const makeBuildingTree = (tree, idx) => {
+  const rootNodeIndex = parseInt(tree.length / 2);
+  buildingsTree[idx].push(tree[rootNodeIndex]);
+
+  if (tree.length === 1) return;
+  makeBuildingTree(tree.slice(0, rootNodeIndex), idx + 1);
+  makeBuildingTree(tree.slice(rootNodeIndex + 1), idx + 1);
+};
+
+makeBuildingTree(buildingsByInOrder, 0);
+
+for (let node of buildingsTree) {
+  console.log(node.join(' '));
+}
