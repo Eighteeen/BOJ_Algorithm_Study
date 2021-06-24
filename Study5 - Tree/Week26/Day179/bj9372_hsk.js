b2104945a6204b6472821695068e3037d30145f0
sukyeongh <hsk10271@gmail.com>
2021-06-24 16:43:16 +0900

    Day179 : Day179(상근이의 여행) 풀이

---
+++ Study5 - Tree/Week26/Day179/bj9372_hsk.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+let stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+3 3
+1 2
+2 3
+1 3
+5 4
+2 1
+2 3
+4 3
+4 5`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const T = parseInt(input());
+for (let i = 0; i < T; i++) {
+  const [N, M] = input()
+    .split(' ')
+    .map((item) => parseInt(item));
+  for (let i = 0; i < M; i++) {
+    input();
+  }
+  console.log(N - 1);
+}
