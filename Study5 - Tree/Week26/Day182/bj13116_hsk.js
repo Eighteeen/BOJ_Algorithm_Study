4c30163fde7c1698aa63471e8d9223ed7f444436
sukyeongh <hsk10271@gmail.com>
2021-06-27 11:07:46 +0900

    Day182 : Day182(30번) 풀이

---
+++ Study5 - Tree/Week26/Day182/bj13116_hsk.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+let stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `3
+33 79
+9 15
+1022 1023`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const T = parseInt(input());
+
+const findParentNode = (node) => {
+  return parseInt(node / 2);
+};
+
+for (let i = 0; i < T; i++) {
+  let [A, B] = input()
+    .split(' ')
+    .map((value) => parseInt(value));
+
+  while (A !== B) {
+    if (A > B) A = findParentNode(A);
+    else B = findParentNode(B);
+  }
+  console.log(10 * A);
+}
