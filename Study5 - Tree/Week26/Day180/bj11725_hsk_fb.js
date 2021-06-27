f140b9535353cc82b8895b76fa469bc351e49c2c
sukyeongh <hsk10271@gmail.com>
2021-06-27 11:07:34 +0900

    Day182 : Day180 피드백 반영

---
+++ Study5 - Tree/Week26/Day180/bj11725_hsk_fb.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+let stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `7
+1 6
+6 3
+3 5
+4 1
+2 4
+4 7`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+const connectedNodeList = Array.from(Array(N + 1), () => Array());
+const parentsNodeList = new Array(N + 1).fill(false);
+
+//// 함수가 하는 일을 함수 이름이 좀 더 설명해줬으면 좋겠어요!
+//// 지금은 함수 이름이 dfs인데 반해 깊이 우선 탐색'만'하고있지는 않은 것 같아요: 22
+//// : 넹~ 수정하였습니닷
+//// 깔끔해요
+const getParentNode = (root, connectedNodeList, parentsNodeList) => {
+  let nodeList = [root];
+  while (nodeList.length > 0) {
+    let node = nodeList.pop();
+    for (let idx of connectedNodeList[node]) {
+      if (parentsNodeList[idx]) continue;
+      parentsNodeList[idx] = node;
+      nodeList.push(idx);
+    }
+  }
+};
+
+for (let i = 1; i < N; i++) {
+  const [node1, node2] = input()
+    .split(' ')
+    .map((value) => parseInt(value));
+  connectedNodeList[node1].push(node2);
+  connectedNodeList[node2].push(node1);
+}
+
+getParentNode(1, connectedNodeList, parentsNodeList);
+console.log(parentsNodeList.slice(2).join('\n'));
