6c32d885d6b8341d3b313635aae7c8348c31fd20
Seungbeen Wee <wsb0722@naver.com>
2021-06-30 04:20:15 +0900

    Solve Day184

---
+++ Study5 - Tree/Week27/Day184/bj5639_wsb.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+        List<Integer> nodeList = new ArrayList<>();
+
+        while ((input = br.readLine()) != null) {
+            int data = Integer.parseInt(input);
+            nodeList.add(data);
+        }
+
+        System.out.print(convertTraversalPreorderToPostorder(nodeList));
+        br.close();
+    }
+
+    static StringBuilder convertTraversalPreorderToPostorder(List<Integer> nodeList) {
+        StringBuilder sb = new StringBuilder();
+
+        int len = nodeList.size();
+        if (len == 0) return sb;
+
+        int rootData = nodeList.get(0);
+
+        int rightIdx = len;
+        int idx = 0;
+        for (Integer nodeData : nodeList) {
+            if (rootData < nodeData) {
+                rightIdx = idx;
+                break;
+            }
+            idx++;
+        }
+        
+        sb.append(convertTraversalPreorderToPostorder(nodeList.subList(1, rightIdx)))
+            .append(convertTraversalPreorderToPostorder(nodeList.subList(rightIdx, len)))
+            .append(rootData).append("\n");
+        return sb;
+    }
+}
