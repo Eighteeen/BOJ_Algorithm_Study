912863c1c1588bb78582d7972c53cdb5dbdda691
Deepred <deepred.kim@gmail.com>
2021-07-02 10:40:18 +0900

    187일차: 어제 피드백

--- Study5 - Tree/Week27/Day186/bj17024_wsb.java
+++ Study5 - Tree/Week27/Day186/bj17024_wsb.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//// 깔끔해요
+//// 깔끔해요 :22
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +16,7 @@ class Main {
             int firstNum = Integer.parseInt(nodeInfo[0]);
             int secondNum = Integer.parseInt(nodeInfo[1]);
 
+            //// 바로바로 구해두는 거 괜찮네요
             maxAdjacentLenght = Math.max(maxAdjacentLenght,
                     Math.max(++adjacentLengths[firstNum],
                         ++adjacentLengths[secondNum])
