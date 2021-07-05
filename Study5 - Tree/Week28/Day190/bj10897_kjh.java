b47f9ff26764c1ae219a953c07c3b0cad129ef61
김진홍 <deepred.kim@gmail.com>
2021-07-05 18:54:40 +0900

    Update bj10897_kjh.java

--- Study5 - Tree/Week28/Day190/bj10897_kjh.java
+++ Study5 - Tree/Week28/Day190/bj10897_kjh.java
@@ -3,8 +3,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.math.BigInteger;
 
-// 최대한 이름 직관적으로 지어서 최대한 로직 이해 쉽게 신경써봤는데
-// 이 부분에 대해서 솔직한 피드백 주시면 감사하겠습니다
+// 최대한 이름 직관적으로 지어서 최대한 로직 이해 쉽도록 신경써봤는데
+// 이 부분에 대해서 솔직한 피드백 주시면 감사하겠습니다 (__)
 class Main {
   public static void main(String[] args) throws Exception {
     final BigInteger GEN = BigInteger.valueOf(Input.nextInt());
