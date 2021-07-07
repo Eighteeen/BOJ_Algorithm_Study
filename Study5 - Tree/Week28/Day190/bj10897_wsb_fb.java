98d8e24f010dc4e114a268f79bee36630141b6fe
Seungbeen Wee <wsb0722@naver.com>
2021-07-08 04:33:52 +0900

    Reflect Feedback Day190

---
+++ Study5 - Tree/Week28/Day190/bj10897_wsb_fb.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//// 깔끔해요~ 풀이 배워가요!
+class Main {
+    static final int MOD_NUM = 1000000007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int D = Integer.parseInt(br.readLine());
+        int[] checkChildren = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.print(getCheckListOfJMPABU(checkChildren));
+        br.close();
+    }
+
+    //// 저도 BigInteger 사용않는 로직 시도해보려고, 반복문 한번 돌 때마다 나머지 연산 한번씩 하게했더니 값이 꼬이길래 포기했는데..
+    //// 연산되는 모든 것에 나머지 연산을 먼저 때리고 집어넣으면 제대로 구해지는군요
+    //// 진짜 수학 자신없다 하시지만 누구보다 수학에 진심이신 것 같습니다. 배워갑니다
+    
+    //// 궁금한게 하나 있는데, 나머지를 10억으로 때리니 int형 안에서 충분히 값이 나오지 않나요?
+    //// 혹시 어떤 부분이 int 범위를 초과하는건지 아시나요??
+    //// -> % MOD_NUM 연산을 하기 전의 연산 단계에서 int 범위를 초과할 수 있습니다.
+        //// -> 해당 질문을 보고 다시 확인해서 int로 가능한 부분은 int로 다시 설정했습니다.
+    static StringBuilder getCheckListOfJMPABU(int[] checkChildren) {
+        StringBuilder sb = new StringBuilder();
+
+        int nodeNum = 0;
+        int generation = 0;
+
+        long nthNode = 1;
+        long prevSiblingNode = 1;
+        int prevLastNodeNum = 0;
+
+        for (int checkChild : checkChildren) {
+            nthNode = ((nthNode - 1) * ++generation + checkChild) % MOD_NUM;
+            nodeNum = (int) (prevLastNodeNum + nthNode) % MOD_NUM;
+            sb.append(nodeNum).append("\n");
+
+            prevSiblingNode *= generation;
+            prevSiblingNode %= MOD_NUM;
+            prevLastNodeNum += prevSiblingNode;
+            prevLastNodeNum %= MOD_NUM;
+        }
+
+        return sb;
+    }
+}
