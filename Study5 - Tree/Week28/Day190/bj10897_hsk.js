68d1f69c7020ac96e68ce23f4c0f7430af369391
Seungbeen Wee <wsb0722@naver.com>
2021-07-08 04:23:38 +0900

    Feedback Day190

--- Study5 - Tree/Week28/Day190/bj10897_hsk.js
+++ Study5 - Tree/Week28/Day190/bj10897_hsk.js
@@ -3,9 +3,14 @@ let stdin = (
   process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString().trim()
     : `4
-1 1 1 2`
+1 2 1 2`
 ).split('\n');
 
+//// 전반적으로 변수명에 index 용어가 들어갔는데,
+  //// index라고 하기엔 1부터 시작하기 때문에 nth, sequence 정도의 변수명이 적합해보입니다.
+
+//// D의 경우 100 이하인데 BigInt를 사용하는 점이 아쉽습니다.
+  //// (아래에서 D를 사용하는 i도 BigInt로 변경해주기 때문에 필요성이 없어보입니다.)
 const D = BigInt(stdin[0]);
 const childIdxArray = stdin[1].split(' ').map((value) => BigInt(value));
 const mod = BigInt(1000000007);
@@ -20,9 +25,12 @@ let findNodeNumResult = [];
 //// 짧은 영어단어의 나열로도 충분히 의미를 전달할 수 있는 것 같네요. 영문법을 꽤 많이 신경써왔었던 저로서 느끼는 점이 있었습니다
 for (let i = 1; i <= D; i++) {
   let generationAllNodeCnt = prevGenerationNodeCnt * BigInt(i);
+  //// generation = i 인데 필요없는 연산과 변수같습니다.
   let generation = generationAllNodeCnt / prevGenerationNodeCnt;
 
   let childIdx = childIdxArray[i - 1];
+  //// prev1stChildNodeIndex가 prevChildNodeIdx에 저장되지만
+    //// 연산되는 시점은 현재 세대에 대한 정보를 연산하므로 prev를 current로 변경하거나 prev가 빠지는 게 가독에 오해가 없을 것 같습니다.
   let prev1stChildNodeIndex = generation * (prevChildNodeIdx - BigInt(1)) + childIdx;
 
   remainderAccumulateNode = (prevNodeNum + prev1stChildNodeIndex) % mod;
--- Study5 - Tree/Week28/Day190/bj10897_kjh.java
+++ Study5 - Tree/Week28/Day190/bj10897_kjh.java
@@ -7,11 +7,17 @@ import java.math.BigInteger;
 // 이 부분에 대해서 솔직한 피드백 주시면 감사하겠습니다 (__)
 
 //// 저는 저랑 비슷한 풀이여서 이름보고 이해가 잘 됐어요~ 깔끔해요!
+//// 변수명 이해는 잘 되지만 축약 가능한 단어는 축약하는 게 오히려 가독하는 데 더 빠를 것 같습니다.
+
+//// 몇몇 변수명에 index 용어가 들어갔는데,
+  //// index라고 하기엔 1부터 시작하기 때문에 nth, sequence 정도의 변수명이 적합해보입니다.
+
 class Main {
   public static void main(String[] args) throws Exception {
     final BigInteger GEN = BigInteger.valueOf(Input.nextInt());
     final BigInteger HASH = BigInteger.valueOf(1000000007);
     
+    //// number 변수명은 여러가지로 해석할 수 있는 여지가 있어서 좀 더 구체적인 변수명을 지으면 좋을 것 같습니다.
     BigInteger number = BigInteger.ZERO;
 
     BigInteger lastNumberOfPreviousGen = BigInteger.ZERO;
@@ -19,10 +25,13 @@ class Main {
     BigInteger lastGenNodeCount = BigInteger.ONE;
 
     StringBuilder sb = new StringBuilder();
+    //// for문의 i가 BigInteger가 되면서 가독성이 확 내려간 느낌이라서,
+      //// GEN과 i는 int로 설정하고 for문 내에서 i를 BigInteger로 설정하여 연산하는 건 어떠신가요?
     for (BigInteger i = BigInteger.ONE; i.compareTo(GEN) <= 0; i = i.add(BigInteger.ONE)) {
       BigInteger childIndex = BigInteger.valueOf(Input.nextInt());
 
       BigInteger genNodeCount = lastGenNodeCount.multiply(i);
+      //// genNodeCount.divide(lastGenNodeCount) = i 인데 필요없는 연산을 더 하고 있는 것 같습니다.
       BigInteger childIndexInGen = genNodeCount
         .divide(lastGenNodeCount)
         .multiply(
