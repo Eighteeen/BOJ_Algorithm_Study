3a98659255041b186705fd0a64fad5472918f8fe
Seungbeen Wee <wsb0722@naver.com>
2021-07-09 04:36:35 +0900

    Solve Day194

---
+++ Study5 - Tree/Week28/Day194/bj18005_wsb.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        if (n % 2 == 0) {
+            n /= 2;
+            if (n % 2 == 0) result = 2;
+            else result = 1;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
