1e7ef98e9258b572853f67efc8da375b8d161f27
sukyeongh <hsk10271@gmail.com>
2021-07-08 11:30:34 +0900

    Day193 : Day193(별 찍기 - 9) 풀이

---
+++ Study5 - Tree/Week28/Day193/bj2446_hsk.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `5`).split('\n');
+
+const N = parseInt(stdin);
+
+const drawStar = (spaceCnt, starCnt) => {
+  return ' '.repeat(spaceCnt) + '*'.repeat(starCnt);
+};
+
+for (let i = N; i > 0; i--) {
+  console.log(drawStar(N - i, 2 * i - 1));
+}
+for (let i = 2; i <= N; i++) {
+  console.log(drawStar(N - i, 2 * i - 1));
+}
