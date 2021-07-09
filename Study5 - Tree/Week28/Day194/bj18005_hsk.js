68fba990daa59112594dedac969661f62722f8fd
sukyeongh <hsk10271@gmail.com>
2021-07-09 13:05:06 +0900

    Day194 : Day194(	Even or Odd?) 풀이

---
+++ Study5 - Tree/Week28/Day194/bj18005_hsk.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `12`).split('\n');
+
+const N = parseInt(stdin[0]);
+let result = 1;
+
+if (N % 2 === 1) result = 0;
+else if (parseInt(N / 2) % 2 === 0) result = 2;
+
+console.log(result);
