64e12b148b1d5127c2eda9f08f407daa20006672
Seungbeen Wee <wsb0722@naver.com>
2021-07-11 01:01:38 +0900

    Solve Day195

---
+++ Study5 - Tree/Week28/Day195/bj11021_wsb.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            String[] numStrArr = br.readLine().split(" ");
+            int A = Integer.parseInt(numStrArr[0]);
+            int B = Integer.parseInt(numStrArr[1]);
+
+            sb.append("Case #")
+                .append(i)
+                .append(": ")
+                .append(A + B)
+                .append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
