a2e070847545b624a3f11694cde5928230eb3c34
Seungbeen Wee <wsb0722@naver.com>
2021-07-12 21:08:00 +0900

    Solve Day197

---
+++ Study5 - Tree/Week29/Day197/bj19602_wsb.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int S = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
+
+        int happinessScore = S + 2 * M + 3 * L;
+
+        System.out.println( happinessScore < 10 ? "sad" : "happy" );
+        br.close();
+    }
+}
