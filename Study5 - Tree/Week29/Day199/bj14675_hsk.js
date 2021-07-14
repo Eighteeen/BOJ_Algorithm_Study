683beecc8e1889d56c66fb8c469e0f305bf3b07f
sukyeongh <hsk10271@gmail.com>
2021-07-14 19:34:25 +0900

    Day199 : Day199(단절점과 단절선) 풀이

---
+++ Study5 - Tree/Week29/Day199/bj14675_hsk.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `5
+1 2
+2 3
+3 4
+4 5
+4
+1 1
+1 2
+2 1
+2 2`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+const nodeInfoInTree = Array.from(Array(N + 1), () => Array());
+for (let i = 1; i < N; i++) {
+  const [nodeA, nodeB] = input()
+    .split(' ')
+    .map((node) => parseInt(node));
+
+  nodeInfoInTree[nodeA].push(nodeB);
+  nodeInfoInTree[nodeB].push(nodeA);
+}
+
+const questionNum = parseInt(input());
+const isRightQuestion = [];
+for (let q = 1; q <= questionNum; q++) {
+  const [t, k] = input()
+    .split(' ')
+    .map((value) => parseInt(value));
+
+  if (t === 1) {
+    const isCutVertex = nodeInfoInTree[k].length === 1 ? 'no' : 'yes';
+    isRightQuestion.push(isCutVertex);
+  }
+  if (t === 2) isRightQuestion.push('yes');
+}
+
+console.log(isRightQuestion.join('\n'));
