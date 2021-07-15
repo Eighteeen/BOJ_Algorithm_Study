2a00926103d7cc53c28ddb5a31e9879118ede4b5
sukyeongh <hsk10271@gmail.com>
2021-07-16 00:13:35 +0900

    Day200 : Day200(Binary tree) 풀이

---
+++ Study5 - Tree/Week29/Day200/bj13237_hsk.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `7
+-1
+1
+1
+2
+2
+3
+3`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getHeightFromBFS = () => {
+  while (queueNodeList.length > 0) {
+    let size = queueNodeList.length;
+    for (let i = 0; i < size; i++) {
+      let currNode = queueNodeList.shift();
+
+      tree[currNode].map((node) => {
+        if (node) {
+          heightList[node] = height + 1;
+          queueNodeList.push(node);
+        }
+      });
+    }
+    height++;
+  }
+};
+
+const N = parseInt(input());
+const tree = Array.from(Array(N + 1), () => Array(2).fill(null));
+const heightList = [];
+const queueNodeList = [];
+let height = 0;
+
+for (let nodeNum = 1; nodeNum <= N; nodeNum++) {
+  const parentNodeNum = parseInt(input());
+
+  if (parentNodeNum === -1) {
+    const rootNodeNum = nodeNum;
+
+    heightList[rootNodeNum] = height;
+    queueNodeList.push(rootNodeNum);
+    continue;
+  }
+
+  if (!tree[parentNodeNum][0]) tree[parentNodeNum][0] = nodeNum;
+  else tree[parentNodeNum][1] = nodeNum;
+}
+
+getHeightFromBFS();
+console.log(heightList.splice(1).join('\n'));
