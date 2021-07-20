a97cad160f8921b59e8aeda04c00a8300823ba0e
sukyeongh <hsk10271@gmail.com>
2021-07-21 08:21:57 +0900

    Day206 : Day206(합) 풀이

---
+++ Study5 - Tree/Week30/Day206/boj8389_hsk.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const N = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `3`).split('\n');
+
+const sum = (n) => {
+  return (n * (n + 1)) / 2;
+};
+console.log(sum(parseInt(N)));
