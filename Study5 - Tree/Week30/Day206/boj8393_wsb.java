5247e2886e1455f4587b4a9a20f8cd559a0698f8
Seungbeen Wee <wsb0722@naver.com>
2021-07-21 23:15:48 +0900

    Solve Day206

---
+++ Study5 - Tree/Week30/Day206/boj8393_wsb.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((int) (Math.pow(n, 2) + n) / 2);
+
+        br.close();
+    }
+}
