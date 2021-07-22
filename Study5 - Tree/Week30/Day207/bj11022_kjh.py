fa020515f2c4b933ba5753e8a33a7e552be06207
Deepred <deepred.kim@gmail.com>
2021-07-22 09:02:50 +0900

    207일차: 오늘의 문제

---
+++ Study5 - Tree/Week30/Day207/bj11022_kjh.py
@@ -0,0 +1,6 @@
+quantity=input()
+for i in range(1, int(quantity)+1):
+    plus=input()
+    x=int(plus.split(' ')[0])
+    y=int(plus.split(' ')[1])
+    print('Case #%d: %d + %d = %d'%(i, x, y, x+y))
