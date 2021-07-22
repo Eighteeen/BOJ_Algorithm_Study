fbf92d399f180680590bd4e782e75b804d90cd7c
Seungbeen Wee <wsb0722@naver.com>
2021-07-22 22:59:35 +0900

    Solve Day207

---
+++ Study5 - Tree/Week30/Day207/boj11022_wsb.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            String[] numStrArr = br.readLine().split(" ");
+            int A = Integer.parseInt(numStrArr[0]);
+            int B = Integer.parseInt(numStrArr[1]);
+            
+            sb.append(String.format("Case #%d: %d + %d = %d", i, A, B, A + B)).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
