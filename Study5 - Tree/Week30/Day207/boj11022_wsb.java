09a5bb1144a52315026c8d5d33738c8fbb4df723
Deepred <deepred.kim@gmail.com>
2021-07-23 13:52:39 +0900

    208일차: 어제 피드백

--- Study5 - Tree/Week30/Day207/boj11022_wsb.java
+++ Study5 - Tree/Week30/Day207/boj11022_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// ㄲㄲㅆ
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
