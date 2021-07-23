43f09a5c4d9cf719ab2871f4f54f579fb0b82377
sukyeongh <hsk10271@gmail.com>
2021-07-23 19:38:21 +0900

    Day208 : Day207 피드백

--- Study5 - Tree/Week30/Day207/boj11022_wsb.java
+++ Study5 - Tree/Week30/Day207/boj11022_wsb.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//// ㄲㄲㅆ
+//// ㄲㄲㅆ : 22
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
