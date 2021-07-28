7696bae9a245e59d1a55a49b653b7ee5b24cb19b
Seungbeen Wee <wsb0722@naver.com>
2021-07-29 03:25:52 +0900

    Update boj2263_wsb.java

--- Study5 - Tree/Week31/Day213/boj2263_wsb.java
+++ Study5 - Tree/Week31/Day213/boj2263_wsb.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Main {
     static int[] inOrderIdxArr, postOrderArr, preOrderArr;
