b549fb7555b9166751680f2b361e5713773243cf
Deepred <deepred.kim@gmail.com>
2021-07-30 16:50:33 +0900

    216일차: 그제 피드백 반영

--- Study5 - Tree/Week31/Day213/boj2263_kjh.java
+++ Study5 - Tree/Week31/Day213/boj2263_kjh.java
@@ -13,7 +13,8 @@ class Main {
 
   static Map<Integer, Integer> inorderIdxMap;
   //// sb는 너무 공통적인 변수명으로 느껴져서 프리오더를 저장한다는 걸 알 수 있도록 지정하면 좋을 것 같습니다.
-  static StringBuilder sb;
+  //// => 타성에 젖어서 썼네요. 피드백 고마워요!
+  static StringBuilder resultInPreorder;
 
   public static void main(String[] args) throws Exception {
     final int NODE_AMOUNT = Input.nextInt();
@@ -31,27 +32,29 @@ class Main {
       postorder.add(Input.nextInt());
     }
 
-    sb = new StringBuilder();
-    traversePreorder(0, NODE_AMOUNT - 1, 0, NODE_AMOUNT - 1);
+    resultInPreorder = new StringBuilder();
+    getResultOfTraversingInPreorder(0, NODE_AMOUNT - 1, 0, NODE_AMOUNT - 1);
 
-    System.out.print(sb);
+    System.out.print(resultInPreorder);
   }
 
   //// 정말 프리오더 방식으로 노드를 방문한다기 보단 다른 오더 방식으로부터 프리오더를 추출하는 메서드라서
   //// traverse 이라는 용어가 조금 어색하게 느껴졌습니다. set, add, infer 등등 대체할 수 있는 단어는 많을 것 같습니다.
-  static void traversePreorder(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx) {
+  //// => 반영했습니다!
+  static void getResultOfTraversingInPreorder(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx) {
     if (inStartIdx > inEndIdx) {
       return;
     }
 
     int root = postorder.get(postEndIdx);
     //// StringBuilder를 사용하는데 굳이 +를 사용할 필요는 없을 것 같습니다.
-    sb.append(root + " ");
+    //// => 삽질의 흔적을 다 못 지운 ㅋㅋ
+    resultInPreorder.append(root).append(' ');
 
     int inRootIdx = inorderIdxMap.get(root);
     int leftTreeSize = inRootIdx - inStartIdx;
-    traversePreorder(inStartIdx, inRootIdx - 1, postStartIdx, postStartIdx + leftTreeSize - 1);
-    traversePreorder(inRootIdx + 1, inEndIdx, postStartIdx + leftTreeSize, postEndIdx - 1);
+    getResultOfTraversingInPreorder(inStartIdx, inRootIdx - 1, postStartIdx, postStartIdx + leftTreeSize - 1);
+    getResultOfTraversingInPreorder(inRootIdx + 1, inEndIdx, postStartIdx + leftTreeSize, postEndIdx - 1);
   }
 }
 
