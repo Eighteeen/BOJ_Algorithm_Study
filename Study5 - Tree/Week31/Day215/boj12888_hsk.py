e2880d2291681804d3614725fa6c2c2230be9491
sukyeongh <hsk10271@gmail.com>
2021-07-30 22:49:04 +0900

    nodejs 실패 이유

--- Study5 - Tree/Week31/Day215/boj12888_hsk.py
+++ Study5 - Tree/Week31/Day215/boj12888_hsk.py
@@ -1,4 +1,6 @@
 #같은 로직으로 nodejs에서는 실패가 떠서 python으로 작성하였습니다.
+#js 실패 이유 : js에서는  Number.MAX_SAFE_INTEGER ( = 9007199254740991 ) 보다 큰 정수는
+#오차가 있을 수 있기 때문에 정확하지 않은 값이 산출됩니다.
 import sys
 input = sys.stdin.readline
 
