0fa22176e5664852693f2aab364e5b63acfffc3d
Seungbeen Wee <wsb0722@naver.com>
2021-07-31 10:32:36 +0900

    Solve Day215

---
+++ Study5 - Tree/Week31/Day215/boj12888_wsb.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static long[] minNumOfVisitedCarArr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int H = Integer.parseInt(br.readLine());
+        minNumOfVisitedCarArr = new long[H + 1];
+        minNumOfVisitedCarArr[0] = 1;
+
+        System.out.println(minNumOfVisitedCar(H));
+        br.close();
+    }
+
+    static long minNumOfVisitedCar(int treeHeight) {
+        if (minNumOfVisitedCarArr[treeHeight] != 0) return minNumOfVisitedCarArr[treeHeight];
+
+        for (int i = treeHeight - 2; i >= 0; i--) {
+            minNumOfVisitedCarArr[treeHeight] += minNumOfVisitedCar(i);
+        }
+
+        minNumOfVisitedCarArr[treeHeight] *= 2;
+        return ++minNumOfVisitedCarArr[treeHeight];
+    }
+}
