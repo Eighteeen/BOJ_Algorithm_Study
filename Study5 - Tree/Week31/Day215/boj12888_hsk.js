34e21a3770b8e0c1d79c0f5628914389f6993c12
sukyeongh <hsk10271@gmail.com>
2021-08-01 16:40:10 +0900

    Day215(완전 이진 트리 도로 네트워크) nodejs 성공한 풀이

--- Study5 - Tree/Week31/Day215/boj12888_hsk.js
+++ Study5 - Tree/Week31/Day215/boj12888_hsk.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `60`).split('\n');
-const H = Number(input[0]);
-const numOfCarNeeded = [1, 1];
+const H = BigInt(input[0]);
+const numOfCarNeeded = [BigInt(1), BigInt(1)];
 
 for (let i = 2; i <= H; i++) {
   if (i % 2 === 0) {
-    numOfCarNeeded[i] = 2 * numOfCarNeeded[i - 1] + 1;
+    numOfCarNeeded[i] = BigInt(2) * numOfCarNeeded[i - 1] + BigInt(1);
   } else {
-    numOfCarNeeded[i] = 2 * numOfCarNeeded[i - 1] - 1;
+    numOfCarNeeded[i] = BigInt(2) * numOfCarNeeded[i - 1] - BigInt(1);
   }
 }
 
-console.log(Number(numOfCarNeeded[H]));
+console.log(numOfCarNeeded[H].toString());
+// MAX_SAFE_INTEGER보다 큰 값들은 BigInt로 계산을 하고 숫자를 문자열로 바꿔서 제출하여 성공하였습니다.
