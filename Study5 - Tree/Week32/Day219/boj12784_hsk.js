fc4611321a85398a99286f8f70ad158b4c2ebcd9
sukyeongh <hsk10271@gmail.com>
2021-08-03 11:36:13 +0900

    Day219 : Day219(인하니카 공화국) 풀이

---
+++ Study5 - Tree/Week32/Day219/boj12784_hsk.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+7 6
+1 2 1
+2 3 4
+2 4 1
+5 1 4
+6 5 1
+7 5 2
+7 6
+1 2 1
+2 3 4
+2 4 1
+5 1 4
+6 5 1
+7 5 5`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getMinNumOfDynamite = (curNode, dynamite) => {
+  visitedNodes[1] = true;
+
+  let minNumOfDynamite = 0;
+
+  for (let node of tree[curNode]) {
+    let nextNode = node.connectedNode;
+    let nextDynamite = node.numOfDynamite;
+
+    if (visitedNodes[nextNode]) continue;
+    visitedNodes[nextNode] = true;
+
+    minNumOfDynamite += getMinNumOfDynamite(nextNode, nextDynamite);
+  }
+
+  if (!minNumOfDynamite) return dynamite === Number.MAX_SAFE_INTEGER ? 0 : dynamite;
+  return Math.min(minNumOfDynamite, dynamite);
+};
+
+const T = parseInt(input());
+let tree = [];
+let visitedNodes = [];
+let result = [];
+
+for (let i = 0; i < T; i++) {
+  const [numOfIslands, numOfBridges] = input().split(' ').map(Number);
+  tree = Array.from(new Array(numOfIslands + 1), () => new Array());
+  visitedNodes = new Array(numOfIslands + 1).fill(false);
+
+  for (let j = 1; j < numOfIslands; j++) {
+    const [nodeA, nodeB, D] = input().split(' ').map(Number);
+
+    tree[nodeA].push({ connectedNode: nodeB, numOfDynamite: D });
+    tree[nodeB].push({ connectedNode: nodeA, numOfDynamite: D });
+  }
+
+  result.push(getMinNumOfDynamite(1, Number.MAX_SAFE_INTEGER));
+}
+console.log(result.join('\n'));
