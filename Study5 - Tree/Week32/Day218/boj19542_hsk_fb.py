24d6a58e720206a95ddb8a0241ff0428801ae4e9
Seungbeen Wee <wsb0722@naver.com>
2021-08-04 21:55:03 +0900

    Update boj19542_hsk_fb.py

--- Study5 - Tree/Week32/Day218/boj19542_hsk_fb.py
+++ Study5 - Tree/Week32/Day218/boj19542_hsk_fb.py
@@ -21,7 +21,7 @@ def getTotalDistanceToMove():
     
     for i in range(1, N + 1):
         ## or 구분 문장 각자에 괄호가 있으면 가독성이 높아질 것 같습니다.
-        ## 음. 이부분에서는 이미 충분히 가독성이 좋다고 생각하기 떄문에 바꾸지 않겠습니다.
+        ## => 음. 이부분에서는 이미 충분히 가독성이 좋다고 생각하기 떄문에 바꾸지 않겠습니다.
         if maxDepthOfNodeList[i] < D or i == S: continue        
         distance += 1
     
@@ -39,7 +39,7 @@ for _ in range(N - 1):
     tree[nodeB].append(nodeA)
 
 ## 세미콜론 있어도 괜찮아요? : 22 괜찮더라도 없으면 아예 없애는 게 깔끔할 것 같아요
-## 본능적으로 적은거지만, 문법 오류는 안납니다(원래는 없는게 맞아요)
+## => 본능적으로 적은거지만, 문법 오류는 안납니다(원래는 없는게 맞아요)
 visitedNodes[S] = True
 calcMaxDepthAbleToDeliver(S)
 print(getTotalDistanceToMove())
