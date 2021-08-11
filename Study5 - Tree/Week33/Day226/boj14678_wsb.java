d2fd39f0420bed1c13f3e966615b6691068a55d2
김진홍 <deepred.kim@gmail.com>
2021-08-11 20:02:16 +0900

    Update boj14678_wsb.java

--- Study5 - Tree/Week33/Day226/boj14678_wsb.java
+++ Study5 - Tree/Week33/Day226/boj14678_wsb.java
@@ -45,6 +45,8 @@ class PlaygroundTree extends Tree {
             break;
         }
 
+        //// 위에껀 시작노드로부터 리프까지의 경우만 체크하는거 같고 밑에껀.. 음.. 코드를 읽어도 알기가 어렵네요. 모든 경우의 수에 대한 거라곤 하는데 정말 모~든 경우의 수를 체크하는거면 위 함수를 실행할 이유도 없었을 것 같고.. 뭐지..
+        //// 코딩 의도에 대한 설명이 좀 더 되어 있으면 좋을 것 같습니다. (의견임)
         fillLastFootArrs(startNode);
         fillLastFootArrsInAllCases(startNode);
 
