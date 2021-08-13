84d2484e42d8dc05cd298d90b81102349f67cada
sukyeongh <hsk10271@gmail.com>
2021-08-13 14:33:49 +0900

    Day229 : Day229(거울, 오! 거울) 풀이

---
+++ Study5 - Tree/Week33/Day229/boj4740_hsk.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `AMBULANCE
+Evian
+madam, i'm adam
+***`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let result = [];
+while (1) {
+  let str = input();
+
+  if (str === '***') break;
+
+  let reversedStr = str.split('').reverse().join('');
+  result.push(reversedStr);
+}
+
+console.log(result.join('\n'));
