0320bb84b8c2c73c785dad39bc5a42093050b975
sukyeongh <hsk10271@gmail.com>
2021-08-14 08:09:51 +0900

    Day230 : Day228 피드백 반영

---
+++ Study5 - Tree/Week33/Day228/boj9489_hsk_fb.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `10 15
+1 3 4 5 8 9 15 30 31 32
+12 9
+3 5 6 8 9 10 13 15 16 22 23 25
+10 4
+1 3 4 5 8 9 15 30 31 32
+0 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+//// group 보단 sibling 이라는 용어를 썼으면 빠르게 이해될 것 같아요. => 오호 그럴 것 같아ㅛ
+const getTargetIdxOnGrouping = (N, targetNum, nodeArr, parents) => {
+  let [targetIdx, sibling] = [0, 0];
+
+  parents[1] = 0;
+
+  for (let i = 1; i <= N; i++) {
+    if (nodeArr[i] === targetNum) targetIdx = i + 1;
+    if (nodeArr[i] !== nodeArr[i - 1] + 1) sibling += 1;
+    parents[i + 1] = sibling;
+  }
+
+  return targetIdx;
+};
+
+const getCousinCnt = (targetIdx, N, parents) => {
+  let cousinCnt = 0;
+
+  for (let i = 1; i <= N; i++) {
+    let isCousin = parents[i] !== parents[targetIdx] && parents[parents[i]] === parents[parents[targetIdx]];
+    if (isCousin) cousinCnt += 1;
+  }
+
+  return cousinCnt;
+};
+
+//// 깔끔
+const result = [];
+while (1) {
+  const [N, targetNum] = input().split(' ').map(Number);
+  const parents = new Array(N + 1).fill(-1);
+  if (N === 0 && targetNum === 0) break;
+
+  const nodeArr = input().split(' ').map(Number);
+  const targetIdx = getTargetIdxOnGrouping(N, targetNum, nodeArr, parents);
+  const cousinCnt = getCousinCnt(targetIdx, N, parents);
+
+  result.push(cousinCnt);
+}
+
+console.log(result.join('\n'));
