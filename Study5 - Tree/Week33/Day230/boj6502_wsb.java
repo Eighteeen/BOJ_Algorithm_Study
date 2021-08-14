22a7240df21ddcfd0be6fe936c9ede5fde36a737
Seungbeen Wee <wsb0722@naver.com>
2021-08-14 23:57:29 +0900

    Solve Day230

---
+++ Study5 - Tree/Week33/Day230/boj6502_wsb.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input;
+        int testCaseNum = 0;
+        while (!(input = br.readLine()).equals("0")) {
+            String[] pizzaTableInfo = input.split(" ");
+            int r = Integer.parseInt(pizzaTableInfo[0]);
+            int w = Integer.parseInt(pizzaTableInfo[1]);
+            int l = Integer.parseInt(pizzaTableInfo[2]);
+
+            sb.append(String.format("Pizza %d ", ++testCaseNum))
+                .append(isRectangleInsideCircle(r, w, l) ? "fits" : "does not fit")
+                .append(" on the table.\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    static boolean isRectangleInsideCircle(int radius, int rectangleWidth, int rectangleHeight) {
+        int diameter = radius * 2;
+        double diagonal = Math.sqrt(Math.pow(rectangleWidth, 2) + Math.pow(rectangleHeight, 2));
+        if (diameter >= diagonal) return true;
+        return false;
+    }
+}
