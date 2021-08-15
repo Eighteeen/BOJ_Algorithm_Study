1ef1cf36d8dae154439ed1b5ba9ff727c5fb726f
김진홍 <deepred.kim@gmail.com>
2021-08-15 22:40:29 +0900

    231일차: 내일의 문제

---
+++ Study5 - Tree/Week33/Day231/boj1135_kjh.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+
+class Main {
+  static Node[] nodes;
+
+  public static void main(String[] args) throws Exception {
+    final int NODE_AMOUNT = Input.nextInt();
+  
+    nodes = new Node[NODE_AMOUNT];
+    nodes[0] = new Node(0);
+    Input.next();
+
+    for (int i = 1; i < NODE_AMOUNT; i++) {
+      int parentIdx = Input.nextInt();
+      nodes[i] = new Node(i);
+      nodes[parentIdx].addChild(nodes[i]);
+    }
+
+    System.out.print(getLeastTimeToCall(nodes[0]));
+  }
+
+  static int getLeastTimeToCall(Node root) {
+    System.out.printf("func(%d);\n", root.number);
+    System.out.println(root.children);
+    int childrenSize = root.children.size();
+    if (childrenSize == 0) {
+      System.out.println("return 0;");
+      return 0;
+    }
+
+    int childLeastTimeToCall = 0;
+    for (Node child : root.children) {
+      childLeastTimeToCall = Math.min(childLeastTimeToCall, getLeastTimeToCall(child));
+    }
+
+    System.out.printf("return (%d + %d)==%d\n", childrenSize, childLeastTimeToCall, childrenSize + childLeastTimeToCall);
+    return 1 + childrenSize + childLeastTimeToCall;
+  }
+}
+
+class Node {
+  public List<Node> children;
+  public int number;
+
+  public Node(int number) {
+    this.number = number;
+    children = new ArrayList<>();
+  }
+
+  public void addChild(Node child) {
+    children.add(child);
+  }
+
+  @Override
+  public String toString() {
+    return "(" + number + ")";
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      String line = br.readLine();
+      if (line.equals("")) {
+        return nextLine();
+      }
+      return line;
+    } catch (Exception e) {
+    }
+
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (!tokenizer.hasMoreTokens()) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static StringTokenizer makeTokens() {
+    StringTokenizer tokens = new StringTokenizer(nextLine(), " ");
+    if (tokens.hasMoreTokens() == false) {
+      makeTokens();
+    }
+    return tokens;
+  }
+}
