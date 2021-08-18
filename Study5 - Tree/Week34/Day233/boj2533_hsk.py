1e1d9d25b43ef5b0899f60ad6746645052d34f16
Seungbeen Wee <wsb0722@naver.com>
2021-08-19 04:09:50 +0900

    Feedback Day233

--- Study5 - Tree/Week34/Day233/boj2533_hsk.py
+++ Study5 - Tree/Week34/Day233/boj2533_hsk.py
@@ -1,4 +1,5 @@
 # nodejs로 stack size exceeded 문제로 실패하여 python으로 풀이
+## 세미콜론을 붙여도 에러는 나지 않지만 파이썬을 사용할 때는 파이썬에서 주로 사용하는 문법으로 사용하면 좋을 것 같습니다.
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 9)
@@ -15,7 +16,7 @@ def accumulateEarlyAdapter(node):
         earlyAdapter[node][1] += earlyAdapter[nextNode][0];
 
 
-
+## 깔끔해요
 N = int(input())
 tree = [[] for _ in range(N + 1)]
 earlyAdapter = [[0,0] for _ in range(N + 1)]
