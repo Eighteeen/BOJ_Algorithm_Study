1bddfaf0fd3d10bed88a05b4c342f3c21bf977d0
sukyeongh <hsk10271@gmail.com>
2021-08-18 18:26:18 +0900

    Day234 : Day234(K진 트리) 풀이

---
+++ Study5 - Tree/Week34/Day234/boj11812_hsk.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const getParentNodeNum = (nodeNum, K) => {
+  return Math.floor((nodeNum + K - 2) / K);
+};
+
+const getDistanceBetweenNodes = (nodeA, nodeB, K) => {
+  let distance = 0;
+
+  while (nodeA !== nodeB) {
+    while (nodeA > nodeB) {
+      distance += 1;
+      nodeA = getParentNodeNum(nodeA, K);
+    }
+    while (nodeB > nodeA) {
+      distance += 1;
+      nodeB = getParentNodeNum(nodeB, K);
+    }
+  }
+
+  return distance;
+};
+
+const input = [];
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const [N, K, Q] = input[0].split(' ').map(Number);
+  const result = [];
+  for (let i = 1; i <= Q; i++) {
+    let [nodeA, nodeB] = input[i].split(' ').map(Number);
+
+    if (K === 1) {
+      result.push(Math.abs(nodeA - nodeB));
+      continue;
+    }
+
+    let distance = getDistanceBetweenNodes(nodeA, nodeB, K);
+    result.push(distance);
+  }
+  console.log(result.join('\n'));
+
+  process.exit();
+});
