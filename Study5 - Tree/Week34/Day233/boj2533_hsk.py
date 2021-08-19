92094ff66372dc1dab845b4b07d53e5133de6dbb
sukyeongh <hsk10271@gmail.com>
2021-08-19 12:58:05 +0900

    Day235 : Day233 피드백 반영

--- Study5 - Tree/Week34/Day233/boj2533_hsk.py
+++ Study5 - Tree/Week34/Day233/boj2533_hsk.py
@@ -1,5 +1,7 @@
 # nodejs로 stack size exceeded 문제로 실패하여 python으로 풀이
 ## 세미콜론을 붙여도 에러는 나지 않지만 파이썬을 사용할 때는 파이썬에서 주로 사용하는 문법으로 사용하면 좋을 것 같습니다.
+## => 아, 자꾸 js에서 복붙하다보니 세미콜론을 까먹네요.
+## => 세미콜론을을 안쓰는게 파이썬 기본 문법이기 떄문에 변경할 경우 꼭 없애겠습니다.
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 9)
@@ -13,8 +15,8 @@ def accumulateEarlyAdapter(node):
 
         ## 요 부분 배워갑니다
         accumulateEarlyAdapter(nextNode)
-        earlyAdapter[node][0] += min(earlyAdapter[nextNode][0], earlyAdapter[nextNode][1]);
-        earlyAdapter[node][1] += earlyAdapter[nextNode][0];
+        earlyAdapter[node][0] += min(earlyAdapter[nextNode][0], earlyAdapter[nextNode][1])
+        earlyAdapter[node][1] += earlyAdapter[nextNode][0]
 
 
 ## 깔끔해요 :22
@@ -29,4 +31,4 @@ for _ in range(N - 1):
     tree[nodeB].append(nodeA)
 
 accumulateEarlyAdapter(1)
-print(min(earlyAdapter[1][0], earlyAdapter[1][1]));
+print(min(earlyAdapter[1][0], earlyAdapter[1][1]))
