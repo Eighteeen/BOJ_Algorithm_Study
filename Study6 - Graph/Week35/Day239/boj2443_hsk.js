ec342f8f9d6170dd10d55d3edf2f7405427f82f1
sukyeongh <hsk10271@gmail.com>
2021-08-23 09:42:54 +0900

    Day239 : Day239(별 찍기 - 6) 풀이

---
+++ Study6 - Graph/Week35/Day239/boj2443_hsk.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `5`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const drawStar = (spaceCnt, starCnt) => {
+  return ' '.repeat(spaceCnt) + '*'.repeat(starCnt);
+};
+
+const N = parseInt(input());
+for (let i = 0; i < N; i++) {
+  console.log(drawStar(i, 2 * (N - i) - 1));
+}
