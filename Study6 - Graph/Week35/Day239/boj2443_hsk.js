b2126f18bcd0138cfc4a11a09c4e469fd80e3ace
Deepred <deepred.kim@gmail.com>
2021-08-24 09:44:18 +0900

    240일차: 어제 피드백

--- Study6 - Graph/Week35/Day239/boj2443_hsk.js
+++ Study6 - Graph/Week35/Day239/boj2443_hsk.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
-const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `5`).split('\n');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `5`
+).split('\n');
 
 const input = (() => {
   let line = 0;
@@ -10,7 +14,7 @@ const drawStar = (spaceCnt, starCnt) => {
   return ' '.repeat(spaceCnt) + '*'.repeat(starCnt);
 };
 
-//// 정말 깔끔하네요
+//// 정말 깔끔하네요 :22
 const N = parseInt(input());
 for (let i = 0; i < N; i++) {
   console.log(drawStar(i, 2 * (N - i) - 1));
