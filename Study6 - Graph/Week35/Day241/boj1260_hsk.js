c7c6fb15f8cd75c9f2d8a2ea8c7fa623314f55a1
sukyeongh <hsk10271@gmail.com>
2021-08-25 19:44:00 +0900

    Day241 : Day241(DFS와 BFS) 풀이

---
+++ Study6 - Graph/Week35/Day241/boj1260_hsk.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `1000 1 1000
+999 1000`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getVerticesByDFS = (startVertex) => {
+  const visitedVertices = new Array(N + 1).fill(false);
+  const stackNodeList = [startVertex];
+  const dfsList = [];
+
+  while (stackNodeList.length) {
+    let vertex = stackNodeList.pop();
+
+    if (visitedVertices[vertex]) continue;
+    visitedVertices[vertex] = true;
+
+    dfsList.push(vertex);
+
+    let sortedVertices = graph[vertex].sort((a, b) => b - a);
+    for (let nextVertex of sortedVertices) {
+      stackNodeList.push(nextVertex);
+    }
+  }
+
+  return dfsList;
+};
+
+const getVerticesByBFS = (startVertex) => {
+  const visitedVertices = new Array(N + 1).fill(false);
+  const queueNodeList = [startVertex];
+  const bfsList = [];
+
+  while (queueNodeList.length) {
+    let vertex = queueNodeList.shift();
+
+    if (visitedVertices[vertex]) continue;
+    visitedVertices[vertex] = true;
+
+    bfsList.push(vertex);
+
+    let sortedVertices = graph[vertex].sort((a, b) => a - b);
+    for (let nextVertex of sortedVertices) {
+      queueNodeList.push(nextVertex);
+    }
+  }
+
+  return bfsList;
+};
+
+const [N, M, V] = input().split(' ').map(Number);
+const graph = Array.from(new Array(N + 1), () => new Array());
+for (let i = 0; i < M; i++) {
+  const [vertexA, vertexB] = input().split(' ').map(Number);
+
+  graph[vertexA].push(vertexB);
+  graph[vertexB].push(vertexA);
+}
+
+console.log(getVerticesByDFS(V).join(' '));
+console.log(getVerticesByBFS(V).join(' '));
