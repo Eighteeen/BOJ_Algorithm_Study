12d0e2a8ff0815194953e950d5a20740dd42aca7
sukyeongh <hsk10271@gmail.com>
2021-08-30 10:26:27 +0900

    Day246 : Day246(저작권)

---
+++ Study6 - Graph/Week36/Day246/boj2914_hsk.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `10 10`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [A, I] = input().split(' ').map(Number);
+console.log(A * (I - 1) + 1);
