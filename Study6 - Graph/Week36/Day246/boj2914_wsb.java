cd3a95d0a64606a606e9bfb4cf4d9ee2cbaf2ae5
sukyeongh <hsk10271@gmail.com>
2021-08-31 22:08:29 +0900

    Revert "Day247 : Day246 피드백"
    This reverts commit 59674e693bed3c1cfb36d5eaf9b5b31d6ba30305.

--- Study6 - Graph/Week36/Day246/boj2914_wsb.java
+++ Study6 - Graph/Week36/Day246/boj2914_wsb.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//// 깔끔.. : 22
+//// 깔끔..
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
