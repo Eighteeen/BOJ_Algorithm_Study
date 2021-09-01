5f2ee098f16f00f86d25e37765a5673b7da6dbfc
Deepred <deepred.kim@gmail.com>
2021-09-01 13:58:00 +0900

    Update boj2914_kjh_fb.py

--- Study6 - Graph/Week36/Day246/boj2914_kjh_fb.py
+++ Study6 - Graph/Week36/Day246/boj2914_kjh_fb.py
@@ -1,7 +1,7 @@
 import math
 
 songAmount, targetAverage = map(int, input().split(' '))
-print(songAmount * (targetAverage - 1) + 1));
+print(songAmount * (targetAverage - 1) + 1);
 
 ## 예전에 푼 것 같지만 조금 어렵게 푸신 것 같네요 ㅎㅎ : 22 문제에 비해 어렵게 푸셨네요.
-## => 당일에 풀었습니다 ㅎ;
+## => 당일에 풀었습니다 ㅎ; 
