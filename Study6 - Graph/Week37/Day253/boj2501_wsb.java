249c3b38c30b0d2901a0fdd6fffa9f0061756806
Seungbeen Wee <wsb0722@naver.com>
2021-09-07 00:41:19 +0900

    Solve Day253

---
+++ Study6 - Graph/Week37/Day253/boj2501_wsb.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] problemInfo = br.readLine().split(" ");
+        int N = Integer.parseInt(problemInfo[0]);
+        int K = Integer.parseInt(problemInfo[1]);
+
+        System.out.println(getKthDivisor(N, K));
+        br.close();
+    }
+
+    static int getKthDivisor(int devidedNum, int K) {
+        int divisorCnt = 0;
+        for (int i = 1; i <= devidedNum; i++) {
+            if (devidedNum % i > 0) continue;
+            if (++divisorCnt == K) return i;
+        }
+        return 0;
+    }
+}
