48dfd27bd799d09e3e173e2b12cabd75eba5a80d
sukyeongh <hsk10271@gmail.com>
2021-09-10 20:55:20 +0900

    Day257 : Day257(삼각형과 세 변) 풀이

---
+++ Study6 - Graph/Week37/Day257/boj5073_hsk.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `7 7 7
+6 5 4
+3 2 5
+6 2 6
+0 0 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+while (1) {
+  let sides = input().split(' ').map(Number);
+  sides.sort((a, b) => a - b);
+
+  let [sideA, sideB, sideC] = sides;
+
+  if (sideA + sideB + sideC === 0) break;
+
+  if (sideC >= sideA + sideB) console.log('Invalid');
+  else {
+    if (sideA === sideB && sideB === sideC) console.log('Equilateral');
+    else if (sideA !== sideB && sideA !== sideC && sideB !== sideC) console.log('Scalene');
+    else console.log('Isosceles');
+  }
+}
