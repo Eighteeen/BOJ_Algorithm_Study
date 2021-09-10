a0ae85edbd98fcb4b25f721c4f26bb2d0f134892
Deepred <deepred.kim@gmail.com>
2021-09-10 09:18:02 +0900

    257일차: 오늘의 문제

---
+++ Study6 - Graph/Week37/Day257/boj5073_kjh.py
@@ -0,0 +1,25 @@
+def getTriangleType(sides):
+    longestSide = max(sides)    
+    otherSides = sum(sides) - longestSide;
+    
+    if longestSide >= otherSides:
+        return "Invalid"
+    
+    sameSideCount = 0
+    for i in range(2):
+    	for j in range(i + 1, 3):
+	        if sides[i] == sides[j]:
+	            sameSideCount += 1
+    
+    if sameSideCount >= 2:
+        return "Equilateral"
+    elif sameSideCount == 1:
+        return "Isosceles"
+    return "Scalene"
+
+while True:
+    sides = list(map(int, input().split()))
+    if sides[0] == 0 and sides[1] == 0 and sides[2] == 0:
+        break;
+    print(getTriangleType(sides))
+    
