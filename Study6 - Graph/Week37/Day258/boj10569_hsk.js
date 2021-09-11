f18cf311c23bdfc8000f8d299fcee2c4cd90885a
sukyeongh <hsk10271@gmail.com>
2021-09-11 20:39:15 +0900

    Day258 : Day258(다면체) 풀이

---
+++ Study6 - Graph/Week37/Day258/boj10569_hsk.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+8 12
+4 6`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const T = parseInt(input());
+for (let i = 0; i < T; i++) {
+  const [V, E] = input().split(' ').map(Number);
+  console.log(2 - V + E);
+}
