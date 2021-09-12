0115314222ab12ecfe64b9a8979aba452bdf2c9f
Seungbeen Wee <wsb0722@naver.com>
2021-09-13 00:04:08 +0900

    Solve Day259

---
+++ Study6 - Graph/Week37/Day259/boj5339_wsb.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\\n")
+            .append("    ( oo|\n")
+            .append("    _\\=/_\n")
+            .append("   /  _  \\\n")
+            .append("  //|/.\\|\\\\\n")
+            .append(" ||  \\ /  ||\n")
+            .append("============\n")
+            .append("|          |\n".repeat(3));
+            
+        System.out.print(sb);
+        br.close();
+    }
+}
