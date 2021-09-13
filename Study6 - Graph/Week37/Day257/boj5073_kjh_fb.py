4d7d39e9ba7f3f66670fcb301de21ee1cc1a88ed
김진홍 <deepred.kim@gmail.com>
2021-09-13 23:40:11 +0900

    259일차: 그제 피드백 반영

---
+++ Study6 - Graph/Week37/Day257/boj5073_kjh_fb.py
@@ -0,0 +1,27 @@
+def getTriangleType(sides):
+    longestSide = max(sides)    
+    otherSides = sum(sides) - longestSide;
+    
+    if longestSide >= otherSides:
+        return "Invalid"
+    
+    sameSideCount = 0
+    for i in range(2):
+    	for j in range(i + 1, 3):
+	        if sides[i] == sides[j]:
+	            sameSideCount += 1
+    
+    if sameSideCount >= 2:
+        return "Equilateral"
+    elif sameSideCount == 1:
+        return "Isosceles"
+    return "Scalene"
+
+## break에 세미콜론은 빼는 것이.. => ㅎㅎ!
+while True:
+    sides = list(map(int, input().split()))
+    if sides[0] == 0 and sides[1] == 0 and sides[2] == 0:
+        break
+    print(getTriangleType(sides))
+
+## 깔끔 : 22
