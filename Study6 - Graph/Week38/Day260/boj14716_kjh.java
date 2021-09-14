1288fb429c50faec26173c761269ccb515e54928
sukyeongh <hsk10271@gmail.com>
2021-09-14 10:47:00 +0900

    Day261 : Day260 피드백

--- Study6 - Graph/Week38/Day260/boj14716_kjh.java
+++ Study6 - Graph/Week38/Day260/boj14716_kjh.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 import java.util.List;
 import java.util.ArrayList;
 
-//// 깔끔
+//// 깔끔 : 22
 class Main {
   static boolean[][] visited;
   static List<Coordinate>[][] adjacencyList;
