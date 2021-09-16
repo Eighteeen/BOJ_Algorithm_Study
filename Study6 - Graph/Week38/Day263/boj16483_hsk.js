1f9b64699df2efddfa050b818c3063f2ec9fd21e
sukyeongh <hsk10271@gmail.com>
2021-09-16 10:45:18 +0900

    Day263 : Day263(접시 안의 원) 풀이

---
+++ Study6 - Graph/Week38/Day263/boj16483_hsk.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `17`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const T = parseInt(input());
+console.log(Math.round(Math.pow(T / 2, 2)));
