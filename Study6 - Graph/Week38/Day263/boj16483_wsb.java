e9355a515439bb4e72c6324f60622bc4f42567d6
Winning-Bean <WinningBean@gmail.com>
2021-09-17 09:07:38 +0900

    Solve Day263

---
+++ Study6 - Graph/Week38/Day263/boj16483_wsb.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        System.out.println(T * T / 4);
+
+        br.close();
+    }
+}
