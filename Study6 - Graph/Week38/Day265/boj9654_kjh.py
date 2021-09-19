09b6fdc6d59e175652adbda7492749f07d0c0a4d
sukyeongh <hsk10271@gmail.com>
2021-09-19 22:33:12 +0900

    Day266 : Day265 피드백

--- Study6 - Graph/Week38/Day265/boj9654_kjh.py
+++ Study6 - Graph/Week38/Day265/boj9654_kjh.py
@@ -3,4 +3,6 @@ N2 Bomber      Heavy Fighter  Limited    21
 J-Type 327     Light Combat   Unlimited  1         
 NX Cruiser     Medium Fighter Limited    18        
 N1 Starfighter Medium Fighter Unlimited  25        
-Royal Cruiser  Light Combat   Limited    4''')
+Royal Cruiser  Light Combat   Limited    4''' )
+
+## ㄲㄲ
