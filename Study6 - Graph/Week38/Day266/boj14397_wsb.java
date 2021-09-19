d1144b7027ea19b66d28912a7751f19b264b9aed
Seungbeen Wee <wsb0722@naver.com>
2021-09-20 06:27:49 +0900

    Solve Day266

---
+++ Study6 - Graph/Week38/Day266/boj14397_wsb.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int mapRow, mapCol;
+    static char WATER_FLAG = '.', LAND_FLAG = '#';
+    static int[] dx = {-1, -1, 0, 1, 1, 0};
+    static int[][] dy = {{-1, 0, 1, 0, -1, -1}, {0, 1, 1, 1, 0, -1}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] mapInfo = br.readLine().split(" ");
+        mapRow = Integer.parseInt(mapInfo[0]);
+        mapCol = Integer.parseInt(mapInfo[1]);
+
+        char[][] mapStates = new char[mapRow][];
+        for (int i = 0; i < mapRow; i++) {
+            mapStates[i] = br.readLine().toCharArray();
+        }
+
+        System.out.println(getLenOfBeach(mapStates));
+        br.close();
+    }
+
+    static int getLenOfBeach(char[][] mapStates) {
+        int len = 0;
+
+        for (int i = 0; i < mapRow; i++) {
+            int dyOrder = i % 2;
+
+            for (int j = 0; j < mapCol; j++) {
+                if (mapStates[i][j] == WATER_FLAG) continue;
+
+                for (int k = 0; k < 6; k++) {
+                    int nx = i + dx[k], ny = j + dy[dyOrder][k];
+                    if (nx < 0 || nx == mapRow || ny < 0 || ny == mapCol) continue;
+                    if (mapStates[nx][ny] == WATER_FLAG) len++;
+                }
+            }
+        }
+
+        return len;
+    }
+}
