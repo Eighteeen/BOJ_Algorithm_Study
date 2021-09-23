98abec28ee31c25451a020ae043379fa0c0c7232
Deepred <deepred.kim@gmail.com>
2021-09-23 10:49:30 +0900

    Update boj5522_kjh.py

--- Study6 - Graph/Week39/Day270/boj5522_kjh.py
+++ Study6 - Graph/Week39/Day270/boj5522_kjh.py
@@ -1,87 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
 
-class Main {
-  public static void main(String[] args) throws Exception {
-    
-  }
-}
+sum = 0
+for i in range(5):
+    sum += int(input())
 
-class Input {
-  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  private static StringTokenizer tokenizer;
-
-  public static String nextLine() {
-    try {
-      String line = br.readLine();
-      if (line.equals("")) {
-        return nextLine();
-      }
-      return line;
-    } catch (Exception e) {
-    }
-
-    return null;
-  }
-
-  public static int nextInt() {
-    String nextString = next();
-    return Integer.parseInt(nextString);
-  }
-
-  public static double nextDouble() {
-    String nextString = next();
-    return Double.parseDouble(nextString);
-  }
-
-  public static long nextLong() {
-    String nextString = next();
-    return Long.parseLong(nextString);
-  }
-
-  public static char nextChar() {
-    String nextString = next();
-    return nextString.charAt(0);
-  }
-
-  public static String next() {
-    makeTokensIfNeed();
-    return tokenizer.nextToken();
-  }
-
-  public static void skipLine() {
-    nextLine();
-  }
-
-  public static void skipLine(int n) {
-    for (int i = 0; i < n; i++) {
-      nextLine();
-    }
-  }
-
-  private static void makeTokensIfNeed() {
-    makeTokensIfNotReadedYet();
-    makeTokensIfHasNoToken();
-  }
-
-  private static void makeTokensIfNotReadedYet() {
-    if (tokenizer == null) {
-      tokenizer = makeTokens();
-    }
-  }
-
-  private static void makeTokensIfHasNoToken() {
-    if (!tokenizer.hasMoreTokens()) {
-      tokenizer = makeTokens();
-    }
-  }
-
-  private static StringTokenizer makeTokens() {
-    StringTokenizer tokens = new StringTokenizer(nextLine(), " ");
-    if (tokens.hasMoreTokens() == false) {
-      makeTokens();
-    }
-    return tokens;
-  }
-}
+print(sum)
