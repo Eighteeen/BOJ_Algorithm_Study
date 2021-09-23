c7e6b50b8136b6501f62d97c1087b6bda3c02085
sukyeongh <hsk10271@gmail.com>
2021-09-23 11:12:38 +0900

    Day270 : Day270(카드 게임) 풀이

---
+++ Study6 - Graph/Week39/Day270/boj5522_hsk.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `1
+2
+3
+4
+5`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  sum += parseInt(input());
+}
+console.log(sum);
