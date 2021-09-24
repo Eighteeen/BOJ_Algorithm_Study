fbaa4ee60aa00f693f653f93d92cb065f5a9b374
Seungbeen Wee <wsb0722@naver.com>
2021-09-25 06:59:25 +0900

    Solve Day271

---
+++ Study6 - Graph/Week39/Day271/boj14248_wsb.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int numOfStones;
+    static int[] jumpDistances;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        numOfStones = Integer.parseInt(br.readLine());
+        jumpDistances = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int s = Integer.parseInt(br.readLine());
+        visited = new boolean[numOfStones];
+
+        System.out.println(getMaxNumOfCanVisit(s - 1));
+        br.close();
+    }
+
+    static int getMaxNumOfCanVisit(int fromIdx) {
+        if (isOutsideStoneBridge(fromIdx) || visited[fromIdx]) return 0;
+        visited[fromIdx] = true;
+
+        int visitCnt = 0;
+
+        int leftJumpStoneIdx = fromIdx - jumpDistances[fromIdx];
+        if (leftJumpStoneIdx >= 0) visitCnt = getMaxNumOfCanVisit(leftJumpStoneIdx);
+
+        int rightJumpStoneIdx = fromIdx + jumpDistances[fromIdx];
+        if (rightJumpStoneIdx < numOfStones) {
+            visited[rightJumpStoneIdx] = false;
+            visitCnt += getMaxNumOfCanVisit(rightJumpStoneIdx);
+        }
+
+        return visitCnt + 1;
+    }
+}
