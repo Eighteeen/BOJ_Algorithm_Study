cc6eb683b5fbe3efa21e24f46e5efb3890f44ed4
sukyeongh <hsk10271@gmail.com>
2021-09-24 19:41:07 +0900

    Day271 : Day270 피드백

--- Study6 - Graph/Week39/Day270/boj5522_kjh.py
+++ Study6 - Graph/Week39/Day270/boj5522_kjh.py
@@ -1,9 +1,11 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 9)
+sys.setrecursionlimit(10 ** 9) ## 이거 지우시는게,,
 
 sum = 0
 for i in range(5):
     sum += int(input())
 
 print(sum)
+
+## 깔끔
