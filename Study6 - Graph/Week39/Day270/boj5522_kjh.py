9f54e2d50b114ff684dd88f6a8be9579b6de00fd
김진홍 <deepred.kim@gmail.com>
2021-09-26 00:37:34 +0900

    272일차: 그제 피드백 반영

--- Study6 - Graph/Week39/Day270/boj5522_kjh.py
+++ Study6 - Graph/Week39/Day270/boj5522_kjh.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 9) ## 이거 지우시는게,, : 22
+sys.setrecursionlimit(10 ** 9) ## 이거 지우시는게,, : 22 => 켱씨 main 갖다쓴거 티내려고 안 지움 ㅎ
 
 sum = 0
 for i in range(5):
