ab75d2d2c5fed7c2c09c1fd978cf8338e307ddea
sukyeongh <hsk10271@gmail.com>
2021-09-26 13:39:43 +0900

    Day273 : Day273(경로 찾기) 풀이

---
+++ Study6 - Graph/Week39/Day273/boj11403_hsk.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `7
+0 0 0 1 0 0 0
+0 0 0 0 0 0 1
+0 0 0 0 0 0 0
+0 0 0 0 1 1 0
+1 0 0 0 0 0 0
+0 0 0 0 0 0 1
+0 0 1 0 0 0 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+const graph = Array.from(new Array(N), () => input().split(' ').map(Number));
+
+for (let k = 0; k < N; k++) {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (graph[i][j]) continue;
+      if (graph[i][k] && graph[k][j]) graph[i][j] = 1;
+    }
+  }
+}
+
+console.log(graph.map((vertices) => vertices.join(' ')).join('\n'));
