8c148d1796f758b31572b4ec62e439bf2d60dad0
Seungbeen Wee <wsb0722@naver.com>
2021-09-27 03:15:09 +0900

    Solve Day273

---
+++ Study6 - Graph/Week39/Day273/boj11403_wsb.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int graphSize;
+    static boolean[][] isExistEdges;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int EXIST_EDGE_FLAG = 1;
+
+        graphSize = Integer.parseInt(br.readLine());
+        isExistEdges = new boolean[graphSize][];
+        for (int i = 0; i < graphSize; i++) {
+            int[] inputArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < graphSize; j++) {
+                isExistEdges[i][j] = (inputArr[j] == EXIST_EDGE_FLAG);
+            }
+        }
+
+        setGraph();
+
+        System.out.println(getStringOfIsExistEdges());
+        br.close();
+    }
+
+    static void setIsExistEdges() {
+        for (int k = 0; k < graphSize; k++) {
+            for (int i = 0; i < graphSize; i++) {
+                for (int j = 0; j < graphSize; j++) {
+                    if (isExistEdges[i][j]) continue;
+                    if (isExistEdges[i][k] && isExistEdges[k][j]) isExistEdges[i][j] = true;
+                }
+            }
+        }
+    }
+
+    static String getStringOfIsExistEdges() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] isExistEdgeLine : isExistEdges) {
+            for (int isExist : isExistEdgeLine) {
+                sb.append(isExist ? 1 : 0).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
