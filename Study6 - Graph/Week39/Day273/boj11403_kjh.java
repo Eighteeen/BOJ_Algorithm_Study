74486b1c16e2656f560a66365610b0d8cc8b8696
sukyeongh <hsk10271@gmail.com>
2021-09-27 16:47:58 +0900

    Day274 : Day273 피드백

--- Study6 - Graph/Week39/Day273/boj11403_kjh.java
+++ Study6 - Graph/Week39/Day273/boj11403_kjh.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//// 깔끔무난
 class Main {
   public static void main(String[] args) throws Exception {
     final int VERTEX_AMOUNT = Input.nextInt();
