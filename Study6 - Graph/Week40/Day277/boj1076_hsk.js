6b9afb2e78dfcdd9ab5b2a448534b98d50b6ccc9
sukyeongh <hsk10271@gmail.com>
2021-09-30 09:22:45 +0900

    Day277 : Day277(저항) 풀이

---
+++ Study6 - Graph/Week40/Day277/boj1076_hsk.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const [colorA, colorB, colorC] = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `white
+white
+white`
+).split('\n');
+
+const registers = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9 };
+
+console.log((registers[colorA] * 10 + registers[colorB]) * Math.pow(10, registers[colorC]));
