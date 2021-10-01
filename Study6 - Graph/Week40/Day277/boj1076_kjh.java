3bd84d6cc8163e879bb7225c924ef269cf0da226
Seungbeen Wee <wsb0722@naver.com>
2021-10-02 02:59:09 +0900

    Feedback Day277

--- Study6 - Graph/Week40/Day277/boj1076_kjh.java
+++ Study6 - Graph/Week40/Day277/boj1076_kjh.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 import java.util.Map;
 import java.util.HashMap;
 
-//// 깔끔
+//// 깔끔 : 22
 class Main {
   public static void main(String[] args) throws Exception {
     Resistances resistances = new Resistances();
