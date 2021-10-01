cfa3684b9fec6ab6773c1b159e4738679fbe118e
sukyeongh <hsk10271@gmail.com>
2021-10-01 13:26:17 +0900

    Day278 : Day278(숨바꼭질) 풀이

---
+++ Study6 - Graph/Week40/Day278/boj6118_hsk.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `6 7
+3 6
+4 3
+3 2
+1 3
+1 2
+2 4
+5 2`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getDistanceFromStartVertex = (startVertex) => {
+  const visited = new Array(N + 1).fill(false);
+  const distance = new Array(N + 1).fill(0);
+  const queue = [startVertex];
+
+  visited[startVertex] = true;
+
+  while (queue.length) {
+    const vertex = queue.shift();
+
+    for (let nextVertex of graph[vertex]) {
+      if (visited[nextVertex]) continue;
+      visited[nextVertex] = true;
+
+      distance[nextVertex] = distance[vertex] + 1;
+      queue.push(nextVertex);
+    }
+  }
+
+  return distance;
+};
+
+const [N, M] = input().split(' ').map(Number);
+const graph = Array.from(new Array(N + 1), () => new Array());
+for (let i = 0; i < M; i++) {
+  const [A, B] = input().split(' ').map(Number);
+
+  graph[A].push(B);
+  graph[B].push(A);
+}
+
+let distanceFromOne = getDistanceFromStartVertex(1);
+
+let maxDistance = Math.max.apply(null, distanceFromOne);
+let maxDistanceVertexNum = distanceFromOne.indexOf(maxDistance);
+let cntOfMaxDistance = distanceFromOne.filter((distance) => distance === maxDistance).length;
+
+console.log(maxDistanceVertexNum, maxDistance, cntOfMaxDistance);
