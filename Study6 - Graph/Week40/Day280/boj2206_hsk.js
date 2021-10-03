9a85c9bc954a0c85eae969a61384de967aa8693b
sukyeongh <hsk10271@gmail.com>
2021-10-03 23:18:45 +0900

    Update boj2206_hsk.js

--- Study6 - Graph/Week40/Day280/boj2206_hsk.js
+++ Study6 - Graph/Week40/Day280/boj2206_hsk.js
@@ -16,25 +16,25 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-const getShortestDistance = (startX, startY, isBroke) => {
+const getShortestDistance = (startX, startY, isBrokeWall) => {
   const shortestDistance = Array.from(new Array(N), () => new Array());
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      shortestDistance[i][j] = new Array(2).fill(isBroke);
+      shortestDistance[i][j] = new Array(2).fill(isBrokeWall);
     }
   }
 
   const dx = [0, 0, 1, -1];
   const dy = [1, -1, 0, 0];
-  const queue = [[startX, startY, isBroke]];
+  const queue = [[startX, startY, isBrokeWall]];
   let queueCursor = 0;
 
-  shortestDistance[startX][startY][isBroke] = 1;
+  shortestDistance[startX][startY][isBrokeWall] = 1;
 
   while (queue.length > queueCursor) {
-    const [x, y, isBroke] = queue[queueCursor++];
+    const [x, y, isBrokeWall] = queue[queueCursor++];
 
-    if (x === N - 1 && y === M - 1) return shortestDistance[x][y][isBroke];
+    if (x === N - 1 && y === M - 1) return shortestDistance[x][y][isBrokeWall];
 
     for (let i = 0; i < 4; i++) {
       let nx = x + dx[i];
@@ -42,16 +42,16 @@ const getShortestDistance = (startX, startY, isBroke) => {
 
       if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-      let moveWithoutBrokeWall = !map[nx][ny] && !shortestDistance[nx][ny][isBroke];
+      let moveWithoutBrokeWall = !map[nx][ny] && !shortestDistance[nx][ny][isBrokeWall];
       if (moveWithoutBrokeWall) {
-        queue.push([nx, ny, isBroke]);
-        shortestDistance[nx][ny][isBroke] = shortestDistance[x][y][isBroke] + 1;
+        queue.push([nx, ny, isBrokeWall]);
+        shortestDistance[nx][ny][isBrokeWall] = shortestDistance[x][y][isBrokeWall] + 1;
       }
 
-      let moveBrokeWall = map[nx][ny] && !isBroke;
+      let moveBrokeWall = map[nx][ny] && !isBrokeWall;
       if (moveBrokeWall) {
-        queue.push([nx, ny, isBroke + 1]);
-        shortestDistance[nx][ny][isBroke + 1] = shortestDistance[x][y][isBroke] + 1;
+        queue.push([nx, ny, isBrokeWall + 1]);
+        shortestDistance[nx][ny][isBrokeWall + 1] = shortestDistance[x][y][isBrokeWall] + 1;
       }
     }
   }
