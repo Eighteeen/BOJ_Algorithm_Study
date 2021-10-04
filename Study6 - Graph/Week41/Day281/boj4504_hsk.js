91cc145d3ce8d49d9c0f3ce3368507e87a3755ce
sukyeongh <hsk10271@gmail.com>
2021-10-04 14:22:20 +0900

    Day281 : Day281 풀이

---
+++ Study6 - Graph/Week41/Day281/boj4504_hsk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const [N, ...nums] = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `3
+1
+7
+99
+321
+777
+0`
+).split('\n');
+
+const isMultiple = nums.map((value) => value % N === 0);
+for (let i = 0; i < isMultiple.length - 1; i++) {
+  console.log(`${nums[i]} is${isMultiple[i] ? '' : ' NOT'} a multiple of ${N}.`);
+}
