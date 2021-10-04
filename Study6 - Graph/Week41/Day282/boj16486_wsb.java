5a76d64cdd63a386b38b5daf0ad3e9f6ecb98c0e
Seungbeen Wee <wsb0722@naver.com>
2021-10-05 00:26:03 +0900

    Solve Day282

---
+++ Study6 - Graph/Week41/Day282/boj16486_wsb.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int d1 = Integer.parseInt(br.readLine());
+        int d2 = Integer.parseInt(br.readLine());
+        final float pi = 3.141592f;
+        
+        int diameter = d2 * 2;
+        float circumference = diameter * pi;
+        System.out.printf("%.6f\n", d1 * 2 + circumference);
+
+        br.close();
+    }
+}
