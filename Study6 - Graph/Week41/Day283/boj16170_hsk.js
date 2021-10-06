cb86d9903435e508a3de6a9d15063343e8a7d467
sukyeongh <hsk10271@gmail.com>
2021-10-06 21:04:09 +0900

    Day283 : Day283(오늘의 날짜는?) 풀이

---
+++ Study6 - Graph/Week41/Day283/boj16170_hsk.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+
+console.log(year);
+console.log(month);
+console.log(day);
