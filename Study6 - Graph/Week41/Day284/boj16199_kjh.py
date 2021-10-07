32c6ef5775aa031fe5130687ee5177752c7af8cc
Deepred <deepred.kim@gmail.com>
2021-10-07 09:41:45 +0900

    284일차: 오늘의 문제

---
+++ Study6 - Graph/Week41/Day284/boj16199_kjh.py
@@ -0,0 +1,13 @@
+birthday_year, birthday_month, birthday_day = map(int, input().split(' '))
+now_year, now_month, now_day = map(int, input().split(' '))
+
+man_age = now_year - birthday_year - 1
+if now_month > birthday_month or (now_month == birthday_month and now_day >= birthday_day):
+    man_age += 1
+
+seneun_age = now_year - birthday_year + 1
+yeon_age = now_year - birthday_year
+
+print(man_age)
+print(seneun_age)
+print(yeon_age)
