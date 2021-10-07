c7ddce7d6687ac104f0d04f8df9fe4b0ab084a1f
sukyeongh <hsk10271@gmail.com>
2021-10-07 14:16:46 +0900

    Day284 : Day284(나이 계산하기) 풀이

---
+++ Study6 - Graph/Week41/Day284/boj16199_hsk.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2005 1 2
+2007 1 1`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getManAge = () => {
+  let manAge = 0;
+
+  if (BIRTH_MONTH < STANDARD_MONTH) {
+    manAge = age;
+  } else if (BIRTH_MONTH === STANDARD_MONTH) {
+    if (BIRTH_DAY <= STANDARD_DAY) {
+      manAge = age;
+    } else {
+      manAge = age - 1;
+    }
+  } else {
+    manAge = age - 1;
+  }
+
+  return manAge;
+};
+
+const [BIRTH_YEAR, BIRTH_MONTH, BIRTH_DAY] = input().split(' ').map(Number);
+const [STANDARD_YEAR, STANDARD_MONTH, STANDARD_DAY] = input().split(' ').map(Number);
+const age = STANDARD_YEAR - BIRTH_YEAR;
+
+const [manAge, countAge, yearAge] = [getManAge(), age + 1, age];
+console.log(manAge + '\n' + countAge + '\n' + yearAge);
