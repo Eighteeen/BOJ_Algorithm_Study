5de5541fd76dabeac27995bc42ff5d25c4d756c8
김진홍 <deepred.kim@gmail.com>
2021-10-10 04:08:50 +0900

    286일차: 그제 피드백 반영

--- Study6 - Graph/Week41/Day284/boj16199_kjh.py
+++ Study6 - Graph/Week41/Day284/boj16199_kjh.py
@@ -2,6 +2,8 @@ birthday_year, birthday_month, birthday_day = map(int, input().split(' '))
 now_year, now_month, now_day = map(int, input().split(' '))
 
 ## now_year - birthday_year 을 변수로 저장해서 써서 연산을 줄이는 것도 좋을 것 같습니다.
+## => man, seneun, yeon 순서대로 초기화하고 man, seneun, yeon 순서대로 출력하는 이 단정함이 저는 왠지모르게 좋아서 이대로 냅둘게요!
+## => 피드백 고마워요!
 man_age = now_year - birthday_year - 1
 if now_month > birthday_month or (now_month == birthday_month and now_day >= birthday_day):
     man_age += 1
