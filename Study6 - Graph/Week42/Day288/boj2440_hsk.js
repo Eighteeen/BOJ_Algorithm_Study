87485cfde3668d2771e96265fb432d8223a9d63b
sukyeongh <hsk10271@gmail.com>
2021-10-11 08:18:50 +0900

    Day288 : Day288(별 찍기 -3) 풀이

---
+++ Study6 - Graph/Week42/Day288/boj2440_hsk.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `5`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const drawStar = (starCnt) => {
+  return '*'.repeat(starCnt);
+};
+
+const N = parseInt(input());
+for (let i = N; i > 0; i--) {
+  console.log(drawStar(i));
+}
