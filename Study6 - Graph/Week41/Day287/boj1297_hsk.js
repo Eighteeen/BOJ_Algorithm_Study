07744e52b7c0b61043f9cef39b60c566da425ad0
sukyeongh <hsk10271@gmail.com>
2021-10-10 15:44:23 +0900

    Day287 : Day287(TV 크기) 풀이

---
+++ Study6 - Graph/Week41/Day287/boj1297_hsk.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `52 9 16`).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [D_LENGTH, H_RATIO, W_RATIO] = input().split(' ').map(Number);
+
+const D_RATIO = Math.sqrt(H_RATIO * H_RATIO + W_RATIO * W_RATIO);
+const x = (W_RATIO * D_LENGTH) / D_RATIO;
+const y = (H_RATIO * D_LENGTH) / D_RATIO;
+
+console.log(Math.floor(y), Math.floor(x));
