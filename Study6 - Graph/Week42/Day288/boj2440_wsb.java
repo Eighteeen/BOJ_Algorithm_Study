c2bb8e6266d6c730359de33ec2a405e45705a903
Seungbeen Wee <wsb0722@naver.com>
2021-10-11 03:09:35 +0900

    Solve Day288

---
+++ Study6 - Graph/Week42/Day288/boj2440_wsb.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append("*".repeat(N - i)).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
