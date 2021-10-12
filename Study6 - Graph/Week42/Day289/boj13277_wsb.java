349f2f6ac657c8e09838337c0aff04ff04c2128c
Seungbeen Wee <wsb0722@naver.com>
2021-10-12 23:21:37 +0900

    Solve Day289

---
+++ Study6 - Graph/Week42/Day289/boj13277_wsb.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numInfo = br.readLine().split(" ");
+        BigInteger num1 = new BigInteger(numInfo[0]);
+        BigInteger num2 = new BigInteger(numInfo[1]);
+
+        System.out.println(num1.multiply(num2));
+        br.close();
+    }
+}
