6116fa6831d186f35d16da508813d30b478e8906
Seungbeen Wee <wsb0722@naver.com>
2021-10-14 01:41:01 +0900

    Solve Day290

---
+++ Study6 - Graph/Week42/Day290/boj14215_wsb.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numInfo = br.readLine().split(" ");
+        int a = Integer.parseInt(numInfo[0]);
+        int b = Integer.parseInt(numInfo[1]);
+        int c = Integer.parseInt(numInfo[2]);
+
+        int perimeter = a + b + c;
+        int longestLength = Math.max(a, Math.max(b, c));
+        int perimeterMinusLongestLength = perimeter - longestLength;
+
+        while (perimeterMinusLongestLength <= longestLength) {
+            longestLength--;
+            perimeter--;
+        }
+
+        System.out.println(perimeter);
+        br.close();
+    }
+}
