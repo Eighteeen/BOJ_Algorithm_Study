31096f9dcb6c9e99770d6bbd6b81483642ef614d
Deepred <deepred.kim@gmail.com>
2021-10-14 11:53:28 +0900

    291일차: 오늘의 문제

---
+++ Study6 - Graph/Week42/Day291/boj7510_kjh.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for i in range(T):
+  print("Scenario #%d:" % (i + 1))
+  lengthsOfSides = list(map(int, input().split(' ')))
+  lengthsOfSides.sort()
+
+  if lengthsOfSides[2] ** 2 == lengthsOfSides[0] ** 2 + lengthsOfSides[1] ** 2:
+    print("yes")
+  else:
+    print("no")
+  
+  print()
