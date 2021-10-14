8dbb0083e1258af391d42853f215bde8ccd8179b
sukyeongh <hsk10271@gmail.com>
2021-10-14 09:51:46 +0900

    Day290 피드백

--- Study6 - Graph/Week42/Day290/boj14215_kjh.java
+++ Study6 - Graph/Week42/Day290/boj14215_kjh.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 
+//// 깔끔
 class Main {
   public static void main(String[] args) throws Exception {
     int[] lengthsOfSides = new int[3];
