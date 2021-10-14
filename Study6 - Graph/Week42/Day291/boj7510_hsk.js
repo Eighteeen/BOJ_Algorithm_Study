87d8a9bed9100a0eb13a109d887d78234f1f1b54
sukyeongh <hsk10271@gmail.com>
2021-10-14 09:52:01 +0900

    Day291(고급 수학) 풀이

---
+++ Study6 - Graph/Week42/Day291/boj7510_hsk.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+36 77 85
+40 55 69`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const TEST_CASE = parseInt(input());
+for (let i = 1; i <= TEST_CASE; i++) {
+  const sides = input().split(' ').map(Number);
+  sides.sort((a, b) => a - b);
+
+  const [sideA, sideB, hypotenuse] = sides;
+
+  let isrightTriangle = Math.pow(hypotenuse, 2) === Math.pow(sideA, 2) + Math.pow(sideB, 2);
+
+  console.log(`Scenario #${i}:`);
+  isrightTriangle ? console.log('yes\n') : console.log('no\n');
+}
