78d7c93c56f8f1d0b9da414dcb336584349ae104
Seungbeen Wee <wsb0722@naver.com>
2021-10-16 01:11:01 +0900

    Solve Day292

---
+++ Study6 - Graph/Week42/Day292/boj2052_wsb.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigDecimal baseNum = BigDecimal.valueOf(2);
+        int N = Integer.parseInt(br.readLine());
+        BigDecimal result = baseNum;
+
+        while (N-- > 1) {
+            result = result.multiply(baseNum);
+        }
+        result = BigDecimal.valueOf(1).divide(result);
+
+        System.out.println(result.toPlainString());
+        br.close();
+    }
+}
