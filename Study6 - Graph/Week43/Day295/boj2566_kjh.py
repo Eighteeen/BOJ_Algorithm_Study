c1f60430d88edbe9d21bd599c6ba3689ad648900
Deepred <deepred.kim@gmail.com>
2021-10-18 10:59:04 +0900

    295일차: 오늘의 문제

---
+++ Study6 - Graph/Week43/Day295/boj2566_kjh.py
@@ -0,0 +1,14 @@
+max_value = 0
+max_row = 0
+max_col = 0
+
+for i in range(9):
+  nums = list(map(int, input().split(' ')))
+  for j in range(9):
+    if nums[j] > max_value:
+      max_value = nums[j]
+      max_row = i + 1
+      max_col = j + 1
+
+print(max_value)
+print(max_row, max_col)
