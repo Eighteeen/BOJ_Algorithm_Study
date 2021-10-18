5783a507c1c2d735bf13cb0812930879f5ed998c
sukyeongh <hsk10271@gmail.com>
2021-10-18 16:09:28 +0900

    Day294 피드백

--- Study6 - Graph/Week42/Day294/boj14928_wsb.java
+++ Study6 - Graph/Week42/Day294/boj14928_wsb.java
@@ -2,14 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//// 끔
+//// 끔  : 깔
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[] numCharArr = br.readLine().toCharArray();
         int remain = 0;
-        //// 올 BigInteger 안 쓰는거 굿
+        //// 올 BigInteger 안 쓰는거 굿 : 22 굳굳굳
         for (char n : numCharArr) {
             remain = (remain * 10 + (n - '0')) % 20000303;
         }
