818e57b871a7dab6dcbb3f417c6753fcab7c41f3
sukyeongh <hsk10271@gmail.com>
2021-10-19 11:01:33 +0900

    Day295 피드백

--- Study6 - Graph/Week43/Day295/boj2566_wsb.java
+++ Study6 - Graph/Week43/Day295/boj2566_wsb.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//// 끔깔
+//// 끔깔 : 22
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
