e1c0ec3eeaf5d0f1afd54a3eb3cc9cd7704e1c4f
sukyeongh <hsk10271@gmail.com>
2021-10-20 12:48:05 +0900

    Day297(Site Score) 풀이

---
+++ Study6 - Graph/Week43/Day297/boj20254_hsk.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [UR, TR, UO, TO] = (
+  process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `1 10 100 1000`
+).split(' ');
+
+console.log(56 * UR + 24 * TR + 14 * UO + 6 * TO);
