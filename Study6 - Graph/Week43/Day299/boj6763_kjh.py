154ae67d1aea7df7b6ec316d3c523a42344ed6b7
Deepred <deepred.kim@gmail.com>
2021-10-21 14:11:28 +0900

    Update boj6763_kjh.py

--- Study6 - Graph/Week43/Day299/boj6763_kjh.py
+++ Study6 - Graph/Week43/Day299/boj6763_kjh.py
@@ -8,9 +8,9 @@ if exceeded_speed <= 0:
   quit()
 
 fine = 0
-if exceeded_speed >= 1 and exceeded_speed <= 20:
+if 1 <= exceeded_speed <= 20:
   fine = 100
-elif exceeded_speed >= 21 and exceeded_speed <= 30:
+elif 21 <= exceeded_speed <= 30:
   fine = 270
 else:
   fine = 500
