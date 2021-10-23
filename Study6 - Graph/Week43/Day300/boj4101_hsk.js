f8c6c86f612e44fc0d5a0a0ae2907b9042b10716
sukyeongh <hsk10271@gmail.com>
2021-10-23 20:23:21 +0900

    Day300(크냐?) 풀이

---
+++ Study6 - Graph/Week43/Day300/boj4101_hsk.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `1 19
+4 4
+23 14
+0 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+while (1) {
+  const [numA, numB] = input().split(' ').map(Number);
+  if (numA === 0 && numB === 0) break;
+
+  console.log(numA > numB ? 'Yes' : 'No');
+}
