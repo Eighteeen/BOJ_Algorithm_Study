df07b25ed79480bf7d95ee33e59c1824007ca342
Seungbeen Wee <wsb0722@naver.com>
2021-10-25 21:58:55 +0900

    Day302
    - Reflect Feedback Day300
    - Feedback Day301
    - Solve Day302

---
+++ Study6 - Graph/Week44/Day302/boj9610_wsb.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] coordinatesCounts = new int[5];
+        final int AXIS = 0, Q1 = 1, Q2 = 2, Q3 = 3, Q4 = 4;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] coordinates = br.readLine().split(" ");
+            int x = Integer.parseInt(coordinates[0]);
+            int y = Integer.parseInt(coordinates[1]);
+
+            if (x == 0 || y == 0) coordinatesCounts[AXIS]++;
+            else if (x > 0) {
+                if (y > 0) coordinatesCounts[Q1]++;
+                else coordinatesCounts[Q4]++;
+            } else {
+                if (y > 0) coordinatesCounts[Q2]++;
+                else coordinatesCounts[Q3]++;
+            }
+        }
+
+        sb.append(String.format("Q1: %d\n", coordinatesCounts[Q1]))
+            .append(String.format("Q2: %d\n", coordinatesCounts[Q2]))
+            .append(String.format("Q3: %d\n", coordinatesCounts[Q3]))
+            .append(String.format("Q4: %d\n", coordinatesCounts[Q4]))
+            .append(String.format("AXIS: %d\n", coordinatesCounts[AXIS]))
+
+        System.out.print(sb);
+        br.close();
+    }
+}
