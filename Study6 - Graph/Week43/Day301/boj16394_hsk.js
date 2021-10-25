7453f626a99fcff2276121d78b41b889cb2716a4
Deepred <deepred.kim@gmail.com>
2021-10-25 10:38:19 +0900

    302일차: 어제 피드백

--- Study6 - Graph/Week43/Day301/boj16394_hsk.js
+++ Study6 - Graph/Week43/Day301/boj16394_hsk.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const YEAR = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `2018`).split('\n');
 
 console.log(YEAR - 1946);
+//// ㄱㄱ ㄱㄱ
