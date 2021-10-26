d1edbb932a2162ada20d803c1e7e1dc48c18ea5b
김진홍 <deepred.kim@gmail.com>
2021-10-27 00:09:46 +0900

    303일차: 오늘의 문제

---
+++ Study6 - Graph/Week44/Day303/boj10817_kjh.py
@@ -0,0 +1,8 @@
+a, b, c = input().split()
+a, b, c = int(a), int(b), int(c)
+if (a >= b and a <= c) or (a >= c and a <= b):
+    print(a)
+elif (b >= a and b <= c) or (b >= c and b <= a):
+    print(b)
+elif (c >= a and c <= b) or (c >= b and c <= a):
+    print(c)
