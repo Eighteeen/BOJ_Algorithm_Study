f0eefecfdf4f41cf41ab436d3a921dbd2a0e3eb9
Deepred <deepred.kim@gmail.com>
2021-10-27 09:33:17 +0900

    304일차: 오늘의 문제

---
+++ Study6 - Graph/Week44/Day304/boj2985_kjh.py
@@ -0,0 +1,19 @@
+def get_operator(operand1, operand2, result):
+  if operand1 + operand2 == result:
+    return '+'
+  if operand1 - operand2 == result:
+    return '-'
+  if operand1 * operand2 == result:
+    return '*'
+  if operand1 / operand2 == result:
+    return '/'
+  return None
+
+a, b, c = map(int, input().split(' '))
+
+operator = get_operator(a, b, c)
+if type(operator) is str:
+  print('{}{}{}={}'.format(a, operator, b, c))
+else:
+  operator = get_operator(b, c, a)
+  print('{}={}{}{}'.format(a, b, operator, c))
