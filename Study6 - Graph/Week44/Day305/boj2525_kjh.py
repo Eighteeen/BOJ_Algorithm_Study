ed281226d6509caff302c37a599efa613fa039a7
Deepred <deepred.kim@gmail.com>
2021-10-28 10:12:38 +0900

    305일차: 오늘의 문제

---
+++ Study6 - Graph/Week44/Day305/boj2525_kjh.py
@@ -0,0 +1,8 @@
+hour, minute = map(int, input().split(' '))
+required_minutes = int(input())
+
+hour += (minute + required_minutes) // 60
+minute = (minute + required_minutes) % 60
+hour = hour % 24
+
+print(hour, minute)
