f9baf468c1d45446dee8e9d09f0d04c10916539b
sukyeongh <hsk10271@gmail.com>
2021-10-30 08:38:04 +0900

    Day307(그대로 출력하기 2) 풀이

---
+++ Study6 - Graph/Week44/Day307/boj11719_hsk.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input =
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `    Hello
+
+Baekjoon     
+   Online Judge    `;
+
+console.log(input);
