dfe91ba6c929d14d75a951eb8aa79dbeef0c769c
sukyeongh <hsk10271@gmail.com>
2021-11-02 15:10:33 +0900

    Day310(팩토리얼) 풀이

---
+++ Study6 - Graph/Week45/Day310/boj10872_hsk.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const N = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `10`).split('\n');
+
+const factorial = (num) => {
+  if (num <= 1) return 1;
+
+  return factorial(num - 1) * num;
+};
+
+console.log(factorial(N));
