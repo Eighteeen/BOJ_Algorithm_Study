12efc0d8ca445055fbd596e69b92f706d5154e60
sukyeongh <hsk10271@gmail.com>
2021-11-04 15:36:22 +0900

    Day312(그대로 출력하기) 풀이

---
+++ Study6 - Graph/Week45/Day312/boj11718_hsk.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input =
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `Hello
+Baekjoon
+Online Judge`;
+
+console.log(input);
