b9a03bd65d3cbc94a28b352cdf0a57e5d4ccc285
Seungbeen Wee <wsb0722@naver.com>
2021-11-06 09:48:43 +0900

    Update boj3182_wsb.java

--- Study6 - Graph/Week45/Day313/boj3182_wsb.java
+++ Study6 - Graph/Week45/Day313/boj3182_wsb.java
@@ -7,7 +7,6 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
