cd515e8a33f8baced6cdb96061f461d481d293dc
Seungbeen Wee <wsb0722@naver.com>
2021-11-06 09:47:31 +0900

    Solve Day313

---
+++ Study6 - Graph/Week45/Day313/boj3182_wsb.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static int[] answers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        answers = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            answers[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(getMaxMeetNum());
+        br.close();
+    }
+
+    static int getMaxMeetNum() {
+        int maxMeetNum = 0;
+        int maxMeet = 0;
+
+        int len = answers.length;
+        for (int i = 1; i < len; i++) {
+            boolean[] isVisited = new boolean[len];
+            int current = i;
+            int currentMeet = 0;
+
+            while (!isVisited[current]) {
+                isVisited[current] = true;
+                current = answers[current];
+                currentMeet++;
+            }
+
+            if (maxMeet < currentMeet) {
+                maxMeet = currentMeet;
+                maxMeetNum = i;
+            }
+        }
+
+        return maxMeetNum;
+    }
+}
