9d4d7d1e316f2f8c8c812bf6c88648eed9708e8d
Seungbeen Wee <wsb0722@naver.com>
2021-11-09 04:47:21 +0900

    Solve Day316

---
+++ Study6 - Graph/Week46/Day316/boj14562_wsb.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int C = Integer.parseInt(br.readLine());
+        while (C-- > 0) {
+            String[] scoresInfo = br.readLine().split(" ");
+            int S = Integer.parseInt(scoresInfo[0]);
+            int T = Integer.parseInt(scoresInfo[1]);
+
+            sb.append(getMinKickCntOfSameScores(S, T)).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    static int getMinKickCntOfSameScores(int myScore, int otherScore) {
+        if (myScore == otherScore) return 0;
+
+        return 1 + Math.min(getMinKickCntOfSameScores(myScore * 2, otherScore + 3),
+            getMinKickCntOfSameScores(myScore + 1, otherScore));
+    }
+}
