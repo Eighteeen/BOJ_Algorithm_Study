4892cd356568dbccd68196fd5908955b15b26081
Deepred <deepred.kim@gmail.com>
2021-11-09 09:32:38 +0900

    317일차: 오늘의 문제

---
+++ Study6 - Graph/Week46/Day317/boj2997_kjh.py
@@ -0,0 +1,11 @@
+a, b, c = sorted(map(int, input().split(' ')))
+
+frontDiff = b-a;
+backDiff = c-b;
+
+if frontDiff == backDiff:
+  print(c + frontDiff)
+elif frontDiff > backDiff:
+  print(a + backDiff)
+elif frontDiff < backDiff:
+  print(b + frontDiff)
