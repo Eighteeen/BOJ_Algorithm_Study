5aa4d09ca349b09db8fcf6750600ce1f0e045c98
sukyeongh <hsk10271@gmail.com>
2021-11-09 13:36:04 +0900

    Day317(네 번째 수)

---
+++ Study6 - Graph/Week46/Day317/boj2997_hsk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const nums = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `10 1 4`)
+  .split(' ')
+  .map(Number);
+
+nums.sort((a, b) => a - b);
+
+const diffPair1 = nums[1] - nums[0];
+const diffPair2 = nums[2] - nums[1];
+
+if (diffPair1 > diffPair2) {
+  console.log(nums[0] + diffPair2);
+} else if (diffPair1 < diffPair2) {
+  console.log(nums[1] + diffPair1);
+} else {
+  console.log(nums[2] + diffPair1);
+}
