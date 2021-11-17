1e10835d7720c3e2ddde698a7bb3c1b055fbc130
Deepred <deepred.kim@gmail.com>
2021-11-17 11:01:26 +0900

    325일차: 어제 피드백

--- Study6 - Graph/Week47/Day324/boj2442_hsk.js
+++ Study6 - Graph/Week47/Day324/boj2442_hsk.js
@@ -4,3 +4,4 @@ const N = Number(process.platform === 'linux' ? fs.readFileSync('/dev/stdin').to
 for (let i = 1; i <= N; i++) {
   console.log(' '.repeat(N - i) + '*'.repeat(2 * i - 1));
 }
+//// 깔
--- Study6 - Graph/Week47/Day324/boj2442_wsb.java
+++ Study6 - Graph/Week47/Day324/boj2442_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// 끔
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
