23068a87dbeb6813bab914e9b9fd430b73da43e2
sukyeongh <hsk10271@gmail.com>
2021-11-19 14:37:53 +0900

    Day327(A → B) 풀이

---
+++ Study6 - Graph/Week47/Day327/boj16953_hsk.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const [A, B] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `100 40021`)
+  .split(' ')
+  .map(Number);
+
+const getLeastCntOfOperation = (numOfStart, numOfTarget) => {
+  const queue = [[numOfStart, 0]];
+
+  while (queue.length) {
+    const [num, cntOfOperation] = queue.shift();
+
+    if (num === numOfTarget) return cntOfOperation + 1;
+
+    for (let i = 0; i < 2; i++) {
+      let nextNum = 0;
+
+      if (i === 0) nextNum = 2 * num;
+      else nextNum = 10 * num + 1;
+
+      if (nextNum <= numOfTarget) {
+        queue.push([nextNum, cntOfOperation + 1]);
+      }
+    }
+  }
+  return -1;
+};
+
+console.log(getLeastCntOfOperation(A, B));
