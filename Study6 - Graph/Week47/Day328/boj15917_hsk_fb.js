3a60f2e705a31587326aea2c442aaa073fa7a4a2
sukyeongh <hsk10271@gmail.com>
2021-11-22 00:31:08 +0900

    RE 피드백 반영
    ,,,피드백 반영 써놓고 반영 안해놔서 다시해여,,,

--- Study6 - Graph/Week47/Day328/boj15917_hsk_fb.js
+++ Study6 - Graph/Week47/Day328/boj15917_hsk_fb.js
@@ -22,7 +22,7 @@ const input = (() => {
 
 //// 자바스크립트 스타일 가이드에 따르면 함수 첫글자는 소문자 : 22
 //// => 잉 대문자로 썻네여,, 왜그랬지
-const CanExpressedAsPower = (num) => {
+const canExpressedAsPower = (num) => {
   //// 조건문 그대로 return해도 돼용
   //// => 저는 이게 편해서 그대로 할게욧
   if ((num & -num) === num) return true;
@@ -33,7 +33,7 @@ const Q = Number(input());
 const result = [];
 for (let i = 0; i < Q; i++) {
   const a = Number(input());
-  result.push(CanExpressedAsPower(a) ? 1 : 0);
+  result.push(canExpressedAsPower(a) ? 1 : 0);
 }
 console.log(result.join('\n'));
 //// 깔끔
