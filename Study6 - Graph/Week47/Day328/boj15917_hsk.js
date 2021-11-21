0ceab57e497b65a8222ba2401d917d993217348a
김진홍 <deepred.kim@gmail.com>
2021-11-21 19:09:01 +0900

    329일차: 어제 피드백

--- Study6 - Graph/Week47/Day328/boj15917_hsk.js
+++ Study6 - Graph/Week47/Day328/boj15917_hsk.js
@@ -20,7 +20,9 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+//// 자바스크립트 스타일 가이드에 따르면 함수 첫글자는 소문자
 const CanExpressedAsPower = (num) => {
+  //// 조건문 그대로 return해도 돼용
   if ((num & -num) === num) return true;
   return false;
 };
