b31f5b999a705f6483e9d577c15c57fae8d49240
김진홍 <deepred.kim@gmail.com>
2021-11-23 15:47:32 +0900

    331일차: 오늘의 문제

--- Study6 - Graph/Week48/Day331/boj2845_kjh.py
+++ Study6 - Graph/Week48/Day331/boj2845_kjh.py
@@ -1,6 +1,6 @@
 people_per_square_meter, area = map(int, input().split(' '))
-number_of_people = people_per_square_meter * area
+people = people_per_square_meter * area
 
-articles_with_number_of_people = map(int, input().split(' '))
-for article in articles_with_number_of_people:
-  
+people_articles = map(int, input().split(' '))
+for article in people_articles:
+  print(article - people, end=' ')
