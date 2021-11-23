ba83d4d59e70aa22190f93b776f5c16442822530
sukyeongh <hsk10271@gmail.com>
2021-11-24 06:02:08 +0900

    Day332(달달함이 넘쳐흘러) 풀이

---
+++ Study6 - Graph/Week48/Day332/boj17256_hsk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `15 8 15
+22 8 22`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [ax, ay, az] = input().split(' ').map(Number);
+const [cx, cy, cz] = input().split(' ').map(Number);
+
+console.log(cx - az, parseInt(cy / ay), cz - ax);
