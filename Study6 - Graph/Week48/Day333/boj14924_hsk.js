dd1a8ccd9742aee38149bb7be3b02c43ca91f077
sukyeongh <hsk10271@gmail.com>
2021-11-25 17:36:56 +0900

    Day333(폰 노이만과 파리) 풀이

---
+++ Study6 - Graph/Week48/Day333/boj14924_hsk.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [S, T, D] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `50 75 200`)
+  .split(' ')
+  .map(Number);
+
+console.log((D / (S * 2)) * T);
