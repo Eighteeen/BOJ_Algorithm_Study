0611a494a3a19da59c0615bbcf4774a0b7273f1c
Deepred <deepred.kim@gmail.com>
2021-11-26 10:36:52 +0900

    334일차: 오늘의 문제

---
+++ Study6 - Graph/Week48/Day334/boj1837_kjh.py
@@ -0,0 +1,13 @@
+password, prime_min_limit = map(int, input().split(' '))
+
+min_prime = 0
+
+for i in range(2, prime_min_limit):
+  if password % i == 0:
+    min_prime = i
+    break
+
+if min_prime == 0:
+  print("GOOD")
+else:
+  print("BAD", min_prime)
