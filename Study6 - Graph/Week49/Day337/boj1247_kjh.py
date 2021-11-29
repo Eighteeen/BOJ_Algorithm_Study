08712b4007f8778e0f285f74165783ed3e561aa9
Deepred <deepred.kim@gmail.com>
2021-11-29 12:11:58 +0900

    337일차: 오늘의 문제

---
+++ Study6 - Graph/Week49/Day337/boj1247_kjh.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(3):
+  N = int(input())
+
+  number = 0
+  for j in range(N):
+    number += int(input())
+  
+  if number == 0:
+    print(0)
+  elif number < 0:
+    print('-')
+  else:
+    print('+')
