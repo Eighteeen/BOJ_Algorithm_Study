0b1f3be4e94842d8512d854219d06c0c07450be4
Seungbeen Wee <wsb0722@naver.com>
2021-11-30 02:15:59 +0900

    Solve Day337

---
+++ Study6 - Graph/Week49/Day337/boj1247_wsb.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            int N = Integer.parseInt(br.readLine());
+            BigInteger sum = BigInteger.ZERO;
+            for (int j = 0; j < N; j++) {
+                sum = sum.add(new BigInteger(br.readLine()));
+            }
+            
+            if (sum.equals(BigInteger.ZERO)) {
+                sb.append("0\n");
+            }
+            else if (sum.compareTo(BigInteger.ZERO) > 0) {
+                sb.append("+\n");
+            }
+            else {
+                sb.append("-\n");
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
