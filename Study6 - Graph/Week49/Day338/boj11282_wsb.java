9d6574a85a69dd00a0e377beac9cf24d17e0a59c
Seungbeen Wee <wsb0722@naver.com>
2021-12-01 00:07:49 +0900

    Update boj11282_wsb.java

--- Study6 - Graph/Week49/Day338/boj11282_wsb.java
+++ Study6 - Graph/Week49/Day338/boj11282_wsb.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println((char) 44031 + Integer.parseInt(br.readLine()));
+        System.out.println((char) (44031 + Integer.parseInt(br.readLine())));
         br.close();
     }
 }
