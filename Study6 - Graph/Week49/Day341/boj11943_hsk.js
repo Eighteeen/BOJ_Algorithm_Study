4e7639289c318d10bac1dc40003f5f7f4b6cdb3a
sukyeongh <hsk10271@gmail.com>
2021-12-03 20:18:13 +0900

    Day341(파일 옮기기) 풀이

---
+++ Study6 - Graph/Week49/Day341/boj11943_hsk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `1 2
+3 4`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [A, B] = input().split(' ').map(Number);
+const [C, D] = input().split(' ').map(Number);
+
+console.log(Math.min(A + D, B + C));
