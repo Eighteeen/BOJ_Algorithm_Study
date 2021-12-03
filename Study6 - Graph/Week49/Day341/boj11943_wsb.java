7a04597828022a5aa6515768e93bb21a69f59dd7
Seungbeen Wee <wsb0722@naver.com>
2021-12-04 00:05:39 +0900

    Solve Day341

---
+++ Study6 - Graph/Week49/Day341/boj11943_wsb.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] basketInfo1 = br.readLine().split(" ");
+        String[] basketInfo2 = br.readLine().split(" ");
+
+        int appleCntOfBasket1 = Integer.parseInt(basketInfo1[0]);
+        int orangeCntOfBasket1 = Integer.parseInt(basketInfo1[1]);
+        int appleCntOfBasket2 = Integer.parseInt(basketInfo2[0]);
+        int orangeCntOfBasket2 = Integer.parseInt(basketInfo2[1]);
+
+        System.out.println(Math.min(appleCntOfBasket1 + orangeCntOfBasket2, appleCntOfBasket2 + orangeCntOfBasket1));
+        br.close();
+    }
+}
