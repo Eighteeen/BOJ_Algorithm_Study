b4e15dfde459af5574400f58eb2952e72e0f2e98
김진홍 <deepred.kim@gmail.com>
2021-12-04 13:23:10 +0900

    342일차: 그제 피드백 반영

---
+++ Study6 - Graph/Week49/Day340/boj1075_kjh_fb.py
@@ -0,0 +1,14 @@
+N = int(input())
+F = int(input())
+
+begin_num = (N // 100) * 100
+end_num = begin_num + 99
+
+for num in range(begin_num, end_num+1):
+  if num % F == 0:
+    print("{:02d}".format(num % 100))
+    break
+  
+## ㄲㄲ : 22
+## 세미콜론 빼는 거 어때요
+## => 헐 몰랐어요 ㅋㅋ
