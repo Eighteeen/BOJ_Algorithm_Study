45feb11ce97ad2a66788b29782bb47c514ff7fbc
김진홍 <deepred.kim@gmail.com>
2021-12-04 13:22:01 +0900

    342일차: 오늘의 문제

---
+++ Study6 - Graph/Week49/Day342/boj17009_kjh.py
@@ -0,0 +1,12 @@
+score = [0, 0]
+
+for i in range(2):
+  for j in range(3):
+    score[i] += int(input()) * (3-j)
+
+if score[0] > score[1]:
+  print('A')
+elif score[0] < score[1]:
+  print('B')
+else:
+  print('T')
