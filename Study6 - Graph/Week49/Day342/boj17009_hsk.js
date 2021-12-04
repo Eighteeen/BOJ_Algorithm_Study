ad07db0cd16341b391333ceb87861cb814f44e52
sukyeongh <hsk10271@gmail.com>
2021-12-04 21:34:40 +0900

    Day342(Winning Score) 풀이

---
+++ Study6 - Graph/Week49/Day342/boj17009_hsk.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `7
+3
+0
+6
+4
+1`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let score = { apple: 0, banana: 0 };
+for (let i = 3; i > 0; i--) {
+  score.apple += parseInt(input()) * i;
+}
+for (let i = 3; i > 0; i--) {
+  score.banana += parseInt(input()) * i;
+}
+
+if (score.apple > score.banana) {
+  console.log('A');
+} else if (score.apple < score.banana) {
+  console.log('B');
+} else {
+  console.log('T');
+}
