dd5e5e041376b642df4b219e4c8ac47bcdfd38b9
sukyeongh <hsk10271@gmail.com>
2021-12-05 19:44:41 +0900

    Day343(플러그) 풀이

---
+++ Study6 - Graph/Week49/Day343/boj2010_hsk.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `2
+5
+8`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = parseInt(input());
+let totalOfComputer = 0;
+for (let i = 0; i < N; i++) {
+  totalOfComputer += parseInt(input());
+}
+console.log(totalOfComputer - (N - 1));
