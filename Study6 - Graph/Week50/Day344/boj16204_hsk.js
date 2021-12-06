c2e226b25c89a3ca3c6323531325bb6d769058cf
sukyeongh <hsk10271@gmail.com>
2021-12-06 16:48:50 +0900

    Day344(카드 뽑기) 문제

---
+++ Study6 - Graph/Week50/Day344/boj16204_hsk.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [N, M, K] = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `4 3 2`)
+  .split(' ')
+  .map(Number);
+
+console.log(Math.min(M, K) + Math.min(N - M, N - K));
