d691857664dedb15dd09071e48320394170e26e7
Seungbeen Wee <wsb0722@naver.com>
2021-12-08 00:00:36 +0900

    Solve Day345

---
+++ Study6 - Graph/Week50/Day345/boj21300_wsb.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] bottles = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int result = 0;
+        for (int bottle : bottles) {
+            result += bottle * 5;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
