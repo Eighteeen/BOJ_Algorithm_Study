ac045037775218ce26215d3e921bfbaa61189ecb
sukyeongh <hsk10271@gmail.com>
2021-12-08 14:55:48 +0900

    Day346(하얀 칸) 풀이

---
+++ Study6 - Graph/Week50/Day346/boj1100_hsk.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const board = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `........
+..F.....
+.....F..
+.....F..
+........
+........
+.......F
+.F......`
+)
+  .split('\n')
+  .map((value) => value.split(''));
+
+let horseCntOnWhite = 0;
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    if ((i + j) % 2 === 0 && board[i][j] === 'F') horseCntOnWhite++;
+  }
+}
+
+console.log(horseCntOnWhite);
