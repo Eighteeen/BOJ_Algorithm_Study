5ff61ebc5c1491e9dfb538cab77a063379f59313
sukyeongh <hsk10271@gmail.com>
2021-12-09 21:52:34 +0900

    Day347(체스판 조각) 풀이

---
+++ Study6 - Graph/Week50/Day347/boj3004_hsk.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const N = Number((process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `1`).split('\n'));
+
+let row = parseInt(N / 2) + 1;
+let col = N - row + 2;
+
+console.log(row * col);
