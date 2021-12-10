fc3182f4cd2eaca1f1f2637ac531c173bfd708d9
sukyeongh <hsk10271@gmail.com>
2021-12-10 16:06:26 +0900

    Day348(베시와 데이지) 풀이

---
+++ Study6 - Graph/Week50/Day348/boj16431_hsk.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `4 5
+5 4
+4 4`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [Br, Bc] = input().split(' ').map(Number);
+const [Dr, Dc] = input().split(' ').map(Number);
+const [Jr, Jc] = input().split(' ').map(Number);
+
+const distanceOfB = Math.max(Math.abs(Jr - Br), Math.abs(Jc - Bc));
+const distanceOfD = Math.abs(Jr - Dr) + Math.abs(Jc - Dc);
+
+let result = 'tie';
+if (distanceOfB < distanceOfD) {
+  result = 'bessie';
+} else if (distanceOfB > distanceOfD) {
+  result = 'daisy';
+}
+console.log(result);
