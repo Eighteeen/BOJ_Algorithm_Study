8f45d6df6b0ba7e604b947a26b9f525707c9e604
김진홍 <deepred.kim@gmail.com>
2021-12-12 22:02:32 +0900

    350일차: 어제 피드백

--- Study6 - Graph/Week50/Day349/boj1212_hsk.js
+++ Study6 - Graph/Week50/Day349/boj1212_hsk.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const num = process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `314`;
 
 console.log(parseInt(num, 8).toString(2));
+//// ㄲㄲ
--- Study6 - Graph/Week50/Day349/boj1212_wsb.java
+++ Study6 - Graph/Week50/Day349/boj1212_wsb.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//// BigInteger를 쓰지않겠다는 의지 리스펙
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
