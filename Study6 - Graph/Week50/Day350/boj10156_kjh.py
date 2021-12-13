510a3cf34dff9b0476a2b98de626f99d209ecf70
sukyeongh <hsk10271@gmail.com>
2021-12-13 22:09:29 +0900

    Day350

--- Study6 - Graph/Week50/Day350/boj10156_kjh.py
+++ Study6 - Graph/Week50/Day350/boj10156_kjh.py
@@ -4,3 +4,5 @@ money_to_buy = PRICE * QUANTITY
 money_need_more = max(0, money_to_buy - MONEY_HAVE)
 
 print(money_need_more)
+
+## ㄲㄲ
---
+++ Study6 - Graph/Week51/Day351/boj9493_hsk.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `21 70 80
+26 65 80
+0 0 0`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const matchDigits = (result, digit) => {
+  return new Array(Math.max(digit - result.toString().length, 0) + 1).join('0') + result;
+};
+
+const [HOUR_TO_SECONDS, MINUTE_TO_SECONDS] = [3600, 60];
+while (1) {
+  const [M, A, B] = input().split(' ').map(Number);
+  if (M === 0 && A === 0 && B === 0) break;
+
+  const durationOfA = (M / A) * HOUR_TO_SECONDS;
+  const durationOfB = (M / B) * HOUR_TO_SECONDS;
+  const diff = Math.round(Math.abs(durationOfA - durationOfB));
+
+  console.log(
+    `${Math.floor(diff / HOUR_TO_SECONDS, 2)}:${matchDigits(
+      Math.floor((diff % HOUR_TO_SECONDS) / MINUTE_TO_SECONDS),
+      2
+    )}:${matchDigits((diff % HOUR_TO_SECONDS) % MINUTE_TO_SECONDS, 2)}`
+  );
+}
