12e1b6d0ed2e570cc5ba6fe3123d7167c5c6797f
Winning-Bean <WinningBean@gmail.com>
2021-12-13 09:03:04 +0900

    Solve Day350

---
+++ Study6 - Graph/Week50/Day350/boj10156_wsb.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] dongsuInfo = br.readLine().split(" ");
+        int K = Integer.parseInt(dongsuInfo[0]);
+        int N = Integer.parseInt(dongsuInfo[1]);
+        int M = Integer.parseInt(dongsuInfo[2]);
+
+        int excessMoney = K * N - M;
+        if (excessMoney < 0) {
+            excessMoney = 0;
+        }
+
+        System.out.println(excessMoney);
+        br.close();
+    }
+}
