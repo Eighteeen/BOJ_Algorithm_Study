89dfcd6a4c6227acc91d5bb644dc07b21b1dd721
Deepred <deepred.kim@gmail.com>
2021-12-14 09:27:37 +0900

    352일차: 오늘의 문제

---
+++ Study6 - Graph/Week51/Day352/boj2480_kjh.py
@@ -0,0 +1,17 @@
+dices = list(map(int, input().split()))
+
+prize_level = 1
+prize_dice = max(dices)
+
+for i in range(3):
+  for j in range(i + 1, 3):
+    if dices[i] == dices[j]:
+      prize_dice = dices[i]
+      prize_level += 1
+
+if prize_level == 1:
+  print(prize_dice * 100)
+elif prize_level == 2:
+  print(1000 + prize_dice * 100)
+else:
+  print(10000 + prize_dice * 1000)
