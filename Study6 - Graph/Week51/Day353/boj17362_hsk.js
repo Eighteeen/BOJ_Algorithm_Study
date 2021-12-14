ed24a5b73a82cc016283a9bb25afa6c1d2b242bc
sukyeongh <hsk10271@gmail.com>
2021-12-15 01:22:03 +0900

    Day353(수학은 체육과목 입니다 2) 풀이

---
+++ Study6 - Graph/Week51/Day353/boj17362_hsk.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let N = Number(process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `1000`);
+
+let result = 0;
+N = N % 8;
+
+if (N === 1) result = 1;
+else if (N === 2 || N === 0) result = 2;
+else if (N === 3 || N === 7) result = 3;
+else if (N === 4 || N === 6) result = 4;
+else result = 5;
+
+console.log(result);
