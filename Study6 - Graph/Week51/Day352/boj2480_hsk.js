0bf20b961bee839885eb34693daff7b438180055
sukyeongh <hsk10271@gmail.com>
2021-12-14 15:21:40 +0900

    Day352(주사위 세개) 풀이

---
+++ Study6 - Graph/Week51/Day352/boj2480_hsk.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const dice = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString().trim() : `6 2 5`)
+  .split(' ')
+  .map(Number);
+
+const calcPrizeMoney = (value1, value2, value3) => {
+  if (value1 === value2 && value1 === value3 && value2 === value3) {
+    return 10000 + value1 * 1000;
+  } else if (value1 === value2) {
+    return 1000 + 100 * value1;
+  } else if (value2 === value3) {
+    return 1000 + 100 * value2;
+  } else if (value3 === value1) {
+    return 1000 + 100 * value3;
+  } else {
+    return Math.max(value1, value2, value3) * 100;
+  }
+};
+
+console.log(calcPrizeMoney(...dice));
