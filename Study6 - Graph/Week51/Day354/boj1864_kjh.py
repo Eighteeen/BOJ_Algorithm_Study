97e7d0ff55b825da79aa342d5f556e3c5dcadfe7
Deepred <deepred.kim@gmail.com>
2021-12-15 10:02:20 +0900

    354일차: 오늘의 문제

---
+++ Study6 - Graph/Week51/Day354/boj1864_kjh.py
@@ -0,0 +1,23 @@
+octopus_digit = {
+  '-': 0,
+  '\\': 1,
+  '(': 2,
+  '@': 3,
+  '?': 4,
+  '>': 5,
+  '&': 6,
+  '%': 7,
+  '/': -1
+}
+
+while True:
+  octopus_number = input()
+  if octopus_number == '#':
+    break
+
+  decimal_number = 0
+  for i in range(len(octopus_number)):
+    number = octopus_number[-i-1]
+    decimal_number += (8 ** i) * octopus_digit[number]
+
+  print(decimal_number)
