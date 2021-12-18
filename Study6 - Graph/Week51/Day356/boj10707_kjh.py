43dd2f610ff7310552faeb6cd021f628e3b7388f
김진홍 <deepred.kim@gmail.com>
2021-12-18 21:13:29 +0900

    356일차: 오늘의 문제

---
+++ Study6 - Graph/Week51/Day356/boj10707_kjh.py
@@ -0,0 +1,13 @@
+X_FARE_PER_LITER = int(input())
+Y_BASIC_FARE = int(input())
+Y_BASIC_LITERS = int(input())
+Y_FARE_PER_LITER = int(input())
+USED_LITERS = int(input())
+
+x_fare = X_FARE_PER_LITER * USED_LITERS
+y_fare = Y_BASIC_FARE
+
+if USED_LITERS > Y_BASIC_LITERS:
+  y_fare += (USED_LITERS - Y_BASIC_LITERS) * Y_FARE_PER_LITER
+
+print(min(x_fare, y_fare))
