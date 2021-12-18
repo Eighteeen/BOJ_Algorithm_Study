1da3c12bfd705832c3ab9acf05459cd3d09c7c7f
sukyeongh <hsk10271@gmail.com>
2021-12-18 18:49:05 +0900

    Day356(수도요금) 풀이

---
+++ Study6 - Graph/Week51/Day356/boj10707_hsk.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const [xFee, yBaseFee, yUsage, yFee, P] = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `8
+300
+100
+10
+250`
+)
+  .split('\n')
+  .map(Number);
+
+const waterFeeOfX = xFee * P;
+const waterFeeOfY = yUsage < P ? yBaseFee + yFee * (P - yUsage) : yBaseFee;
+
+console.log(Math.min(waterFeeOfX, waterFeeOfY));
