27f0f27c0fe60e679dc420a328c91c01ed71b405
sukyeongh <hsk10271@gmail.com>
2021-12-21 20:34:53 +0900

    Day359(여러분의 다리가 되어 드리겠습니다!) 풀이

---
+++ Study6 - Graph/Week52/Day359/boj17352_hsk.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString().trim()
+    : `5
+1 2
+2 3
+4 5`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getNotVisitedNumOfIsland = (startVertex) => {
+  const visitedIslands = new Array(N + 1).fill(false);
+  const queue = [startVertex];
+  let queueCursor = 0;
+
+  visitedIslands[startVertex] = true;
+
+  while (queue.length > queueCursor) {
+    const island = queue[queueCursor++];
+
+    for (let nextIsland of graphOfIslands[island]) {
+      if (visitedIslands[nextIsland]) continue;
+
+      visitedIslands[nextIsland] = true;
+      queue.push(nextIsland);
+    }
+  }
+
+  let notVisitedNumOfIsland = visitedIslands.slice(1).findIndex((visited) => !visited) + 1;
+  return notVisitedNumOfIsland;
+};
+
+const N = parseInt(input());
+const graphOfIslands = Array.from(Array(N + 1), () => new Array());
+for (let i = 0; i < N - 2; i++) {
+  const [islandA, islandB] = input().split(' ').map(Number);
+  graphOfIslands[islandA].push(islandB);
+  graphOfIslands[islandB].push(islandA);
+}
+
+console.log(1, getNotVisitedNumOfIsland(1));
