236c8857bd5f23441d2c629dd9cde9cf93cc469b
김진홍 <deepred.kim@gmail.com>
2021-12-28 02:38:25 +0900

    365일차: 오늘의 문제

---
+++ Study6 - Graph/Week53/Day365/boj16432_kjh.java
@@ -0,0 +1,137 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+  static int[] choosingCakes;
+  static boolean[][] visited; // 몇번째 날, 몇번째 떡
+  static int[] chosenCakes;
+  
+  public static void main(String[] args) throws Exception {
+    final int LAST_DAY = Input.nextInt();
+
+    List<Integer>[] dailyCakes = new ArrayList[LAST_DAY + 1];
+    for (int i = 1; i <= LAST_DAY; i++) {
+      dailyCakes[i] = new ArrayList<>();
+
+      int cakes = Input.nextInt(); 
+      for (int j = 0; j < cakes; j++) {
+        int cake = Input.nextInt();
+        dailyCakes[i].add(cake);
+      }
+    }
+    
+    choosingCakes = new int[LAST_DAY + 1];
+    visited = new boolean[LAST_DAY + 2][10];
+    chosenCakes = null;
+
+    chooseCakes(dailyCakes, 0, 1, LAST_DAY);
+
+    if (chosenCakes == null) {
+      System.out.print(-1);
+      return;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= LAST_DAY; i++) {
+      sb.append(chosenCakes[i]).append('\n');
+    }
+
+    System.out.print(sb);
+  }
+
+  static void chooseCakes(List<Integer>[] dailyCakes, int prevCake, int day, int lastDay) {
+    if (day == lastDay + 1) {
+      chosenCakes = Arrays.copyOf(choosingCakes, choosingCakes.length);
+      return;
+    }
+
+    for (int cake : dailyCakes[day]) {
+      if (visited[day + 1][cake] || cake == prevCake) {
+        continue;
+      }
+      visited[day + 1][cake] = true;
+
+      choosingCakes[day] = cake;
+      chooseCakes(dailyCakes, cake, day + 1, lastDay);
+    }
+  }
+}
+
+class Input {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokenizer;
+
+  public static String nextLine() {
+    try {
+      String line = br.readLine();
+      if (line.equals("")) {
+        return nextLine();
+      }
+      return line;
+    } catch (Exception e) {
+    }
+
+    return null;
+  }
+
+  public static int nextInt() {
+    String nextString = next();
+    return Integer.parseInt(nextString);
+  }
+
+  public static double nextDouble() {
+    String nextString = next();
+    return Double.parseDouble(nextString);
+  }
+
+  public static long nextLong() {
+    String nextString = next();
+    return Long.parseLong(nextString);
+  }
+
+  public static char nextChar() {
+    String nextString = next();
+    return nextString.charAt(0);
+  }
+
+  public static String next() {
+    makeTokensIfNeed();
+    return tokenizer.nextToken();
+  }
+
+  public static void skipLine() {
+    nextLine();
+  }
+
+  public static void skipLine(int n) {
+    for (int i = 0; i < n; i++) {
+      nextLine();
+    }
+  }
+
+  private static void makeTokensIfNeed() {
+    makeTokensIfNotReadedYet();
+    makeTokensIfHasNoToken();
+  }
+
+  private static void makeTokensIfNotReadedYet() {
+    if (tokenizer == null) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static void makeTokensIfHasNoToken() {
+    if (!tokenizer.hasMoreTokens()) {
+      tokenizer = makeTokens();
+    }
+  }
+
+  private static StringTokenizer makeTokens() {
+    StringTokenizer tokens = new StringTokenizer(nextLine(), " ");
+    if (tokens.hasMoreTokens() == false) {
+      makeTokens();
+    }
+    return tokens;
+  }
+}
