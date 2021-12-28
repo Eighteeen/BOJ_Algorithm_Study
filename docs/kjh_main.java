ea6aa55c8c211f8416c245ffc508ff40d8c22524
김진홍 <deepred.kim@gmail.com>
2021-12-28 18:44:55 +0900

    Update kjh_main.java

--- docs/kjh_main.java
+++ docs/kjh_main.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main {
   public static void main(String[] args) throws Exception {
